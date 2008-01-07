ba6073b0be3295d34f193b2bfe88090366fc9c83
cdunn <cdunn@localhost>
2008-01-07 11:40:01 +0000

    [r90] [trunk/MobileMRSNoPolish] Cleaned up

--- src/org/dimagi/view/ChatFormView.java
+++ src/org/dimagi/view/ChatFormView.java
@@ -22,23 +22,16 @@ import org.celllife.clforms.MVCComponent;
 public class ChatFormView extends MVCComponent implements FormView {
 
 	private Controller controller;
-	//private static Command saveAndReloadCommand;
 	private static Command prevCommand = new Command("Prev", Command.ITEM, 3);
 	private static Command nextCommand = new Command("Next", Command.ITEM, 3);
 	private static Command exitCommand = new Command("Exit", Command.EXIT, 3);
 	private static Displayable screen = null;
-	private Prompt p;
 	private ChatScreenForm chatScreenForm = new ChatScreenForm();
 	
-	/**
-	 * Creates a new DForm.
-	 */
 	public ChatFormView() {
-		System.out.println("ChatterboxPromptScreen2()");
 		screen = chatScreenForm;
 		screen.addCommand(nextCommand);
 		screen.addCommand(prevCommand);
-//		screen.addCommand(saveAndReloadCommand);
 		screen.addCommand(exitCommand);
 	}
 
@@ -47,13 +40,8 @@ public class ChatFormView extends MVCComponent implements FormView {
 	}
 	
 	public void commandAction(Command command, Displayable s) {
-  		System.out.println("ChatterboxPromptScreen2.commandAction()");
+  	
 		try {
-//			if (command == saveAndReloadCommand) {
-//		  		System.out.println("ChatterboxPromptScreen2.saveAndReload()");
-//				controller.processEvent(new ResponseEvent(ResponseEvent.SAVE_AND_RELOAD, -1));
-//			}
-//			else 
 			if (command == List.SELECT_COMMAND){
 		  		System.out.println("ChatterboxPromptScreen2.select()");
 				controller.processEvent(new ResponseEvent(ResponseEvent.GOTO,((List)screen).getSelectedIndex()));
@@ -77,68 +65,22 @@ public class ChatFormView extends MVCComponent implements FormView {
 	}
 
 	// Initialize. If a data member is not backed by RMS, make sure
-	// it is uninitilzed (null) before you put in values.
+	// it is uninitialized (null) before you put in values.
 	protected void initModel() throws Exception {
 		
 	}
 
-	protected void createView() {
-  		System.out.println("ChatterboxPromptScreen2.createView()");
-  		
-  	
-  		
-//	    saveAndReloadCommand = new Command("SAVE&Reload", Command.ITEM, 3);
-//		nextCommand = new Command("Next", Command.ITEM, 3);
-//		exitCommand = new Command("Exit", Command.EXIT, 3);
+	protected void createView() {}
 
-//		screen = chatScreenForm;
-//		
-//		form = controller.getForm();
-//		screen = new List("FORM: "+form.getName(),List.IMPLICIT);
-//		form.calculateRelevantAll();
-//		for(int i = 0; i<form.getPrompts().size(); i++){
-//			
-//			if(((Prompt)form.getPrompt(i)).isRelevant()){
-//				int type = ((Prompt)form.getPrompts().elementAt(i)).getReturnType();
-//				String temp= J2MEUtil.getStringValue(((Prompt)form.getPrompts().elementAt(i)).getValue(),type);
-//				//((List)screen ).append(((Prompt)form.getPrompts().elementAt(i)).getLongText()+temp,null);
-//				// short text + value
-//				((List)screen ).append(((Prompt)form.getPrompts().elementAt(i)).getShortText()+" => " +temp,null);
-//				// Long text + value on new line indent
-//				//((List)screen ).append(((Prompt)form.getPrompts().elementAt(i)).getLongText()+"\n A:"+temp,null);
-//			}
-//			
-//		}
-		
-//		screen.addCommand(nextCommand);
-//		screen.addCommand(saveAndReloadCommand);
-//		screen.addCommand(exitCommand);
-
-	}
-
-	protected void updateView() throws Exception {
-  		System.out.println("ChatterboxPromptScreen2.updateView()");
-		createView();
-	}
+	protected void updateView() throws Exception {}
 	
 
   	public void registerController(Controller controller) {
-  		System.out.println("ChatterboxPromptScreen2.registerController()");
   		this.controller = controller;		
 	}
 
 	public void showPrompt(Prompt prompt) {
-		System.out.println("ChatterboxPromptScreen2.showPrompt(Prompt)");
-		this.p = prompt;
-		try{
-			createView();
-			showScreen();
-		}catch(Exception e){
-			System.out.println("something wrong in CREAT VIEW\n "+e.getMessage());
-			e.printStackTrace();			
-		}
-		//repaint();
+		showScreen();
 	}
-	
 
 }
