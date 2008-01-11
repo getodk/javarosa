1c55a1b28beb53cb7b49ddcc49f3af72f24a1a3c
csims <csims@localhost>
2008-01-11 10:58:05 +0000

    [r103] [trunk/MobileMRSNoPolish] Test case should now fail. Seeing how this is handled by ccnet

--- test/org/dimagi/ExampleTestCase.java
+++ test/org/dimagi/ExampleTestCase.java
@@ -5,7 +5,7 @@ import junit.framework.TestCase;
 public class ExampleTestCase extends TestCase {
     
     public void testAdd() {
-        assertTrue("Addition is broken!",2==1+1);
+        assertTrue("Addition is broken!",2==1+1+1);
     }
     public void testMul() {
         assertTrue("Multiplication is broken!",2*2==4);
