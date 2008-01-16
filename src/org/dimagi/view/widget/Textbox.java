7346ccf890fcb8bd1bdc250a80af20c7038f1efc
cdunn <cdunn@localhost>
2008-01-16 20:27:55 +0000

    [r118] [trunk/MobileMRSNoPolish] Add JavaDoc comments

--- src/org/dimagi/view/widget/Textbox.java
+++ src/org/dimagi/view/widget/Textbox.java
@@ -1,20 +1,24 @@
 package org.dimagi.view.widget;
 
-
 import javax.microedition.lcdui.Font;
 import javax.microedition.lcdui.Graphics;
-import org.dimagi.chatscreen.Constants;
 
 import org.dimagi.utils.ViewUtils;
 import org.dimagi.view.Widget;
 
+/**
+* A custom widget for accepting free text input.
+*  
+* @author cdunn
+* @date Jan-16-2008
+*/
 public class Textbox extends Widget {
 
-	int fontHeight;
-	String str = "";
+	private int fontHeight;
+	private String input = "";
 	
 	/**
-	 * Creates a new textbox widget
+	 * Creates a new custom textbox widget
 	 */
 	public Textbox() {
 	}
@@ -27,27 +31,48 @@ public class Textbox extends Widget {
 		this.setHeight(fontHeight * 2);
 	}
 	
+	/**
+	 * Sets the width and the height of the widget
+	 *  
+	 *  @param width	width of the widget
+	 *  @param height	height of the widget 	
+	 */	
 	public void sizeWidget(int width, int height) {
 		this.setWidth(width);
 		this.setHeight(height);
 	}
 	
+	/**
+	 * Draws the widget in the inactive state
+	 * 
+	 * @param g 	Graphics context
+	 */		
 	public void drawInactiveWidget(Graphics g) {
 		int xBufferSize = this.getWidth()/10;
 		int yBufferSize = this.getHeight()/10;
 		g.setColor(ViewUtils.BLACK);
 		Font theFont = Font.getDefaultFont();
-		g.drawString(str, getWidth()-20, yBufferSize, g.TOP | g.RIGHT);
+		g.drawString(input, getWidth()-20, yBufferSize, g.TOP | g.RIGHT);
 	}
-	
+
+	/**
+	 * Draws the widget in the active state
+	 * 
+ 	 * @param g 	Graphics context
+	 */		
 	public void drawActiveWidget(Graphics g) {
 		int xBufferSize = this.getWidth()/10;
 		int yBufferSize = this.getHeight()/10;
 		g.setColor(ViewUtils.BLACK);
 		g.drawRect(0, yBufferSize, getWidth()-xBufferSize, fontHeight);
-		g.drawString(str, 5, yBufferSize, g.TOP | g.LEFT);
+		g.drawString(input, 5, yBufferSize, g.TOP | g.LEFT);
 	}
 	
+	/**
+	 * Draws the widget
+	 *
+	 * @param g 	Graphics context
+	 */			
 	protected void drawInternal(Graphics g) {
 		if ( this.isActiveWidget() ) {
 			drawActiveWidget(g);
@@ -56,16 +81,21 @@ public class Textbox extends Widget {
 		}
 	}
 	
+	/**
+	 * Process input from keyPress event.
+	 * 
+	 * @param keyCode	keyCode entered by the user
+	 */
 	public void keyPressed(int keyCode) {
 		// without this if statement the keyPress event is propagated to all textbox widgets 
 		if (this.isActiveWidget() ) {
 			if (keyCode >= 0) {
-				str += (char)keyCode;
+				input += (char)keyCode;
 			} else if (keyCode == -11) { // delete key
-				str = str.substring(0, str.length()-1); // remove last character
+				input = input.substring(0, input.length()-1); // remove last character
 			}
 			refresh();
-			this.setShortAnswer(str);
+			this.setShortAnswer(input);
 		}
 	}
 	
