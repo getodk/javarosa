ab1808e683f6d3ea0c4024108d5faeefa21ed450
csims <csims@localhost>
2008-06-24 15:04:51 +0000

    [r403] [branches/dev] The Settings screen no longer references doubles. The jury-rigged system looks correct to me, but I could be wrong.

--- JavaRosa/src/org/javarosa/properties/view/PropertiesScreen.java
+++ JavaRosa/src/org/javarosa/properties/view/PropertiesScreen.java
@@ -161,11 +161,38 @@ public class PropertiesScreen extends FramedForm implements ItemStateListener {
         if (bytes == -1)
             return "error";
 
-        double kb = bytes / 1024.0 + 0.05; // 0.05 for rounding
-        String str = Double.toString(kb);
-        str = str.substring(0, str.indexOf(".") + 2); // 1 decimal place
+        String kbytes = "";
+        //Check for overflow
+        if(bytes*10 < bytes) {
+        	//Handle this case with low precision
+        	int kb  = bytes / 1024;
+        	kbytes = String.valueOf(kb);
+        }
+        else if(bytes*100 < bytes){
+        	int kb = (bytes*10)/1024;
+        	kbytes = String.valueOf(kb);
+        	if(kbytes.length() >= 2) {
+        		kbytes = kbytes.substring(0,kbytes.length()-2) + "." + kbytes.charAt(kbytes.length()-1);
+        	}
+        	else {
+        		kbytes = "0." + kbytes;
+        	}
+        }
+        else {
+        	int kb = (bytes*100)/1024;
+        	kbytes = String.valueOf(kb);
+        	if(kbytes.length() >= 3) {
+        		kbytes = kbytes.substring(0,kbytes.length()-3) + "." + kbytes.substring(kbytes.length()-2, kbytes.length()-1);
+        	}
+        	else if(kbytes.length() >= 2) {
+        		kbytes = "0." + kbytes;
+        	}
+        	else {
+        		kbytes = "0.0" + kbytes;
+        	}
+        }
 
-        return str + " KB";
+        return kbytes + " KB";
     }
 
     public void itemStateChanged(Item item) {
