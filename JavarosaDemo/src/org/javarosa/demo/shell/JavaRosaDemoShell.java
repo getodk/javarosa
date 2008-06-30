ad08b8e216671d3152e15286a160eb5560ac48a2
csims <ctsims@users.noreply.github.com>
2008-06-30 18:39:32 +0000

    [r432] [branches/dev] beginnings of a demo project to sandbox the new refactored api in.

---
+++ JavarosaDemo/src/org/javarosa/demo/shell/JavaRosaDemoShell.java
@@ -0,0 +1,43 @@
+package org.javarosa.demo.shell;
+
+import org.javarosa.core.api.IModule;
+import org.javarosa.core.api.IShell;
+import org.javarosa.demo.module.FormListModule;
+import org.javarosa.demo.module.SplashScreenModule;
+
+/**
+ * This is the shell for the JavaRosa demo that handles switching all of the views
+ * @author Brian DeRenzi
+ *
+ */
+public class JavaRosaDemoShell implements IShell {
+	// List of views that are used by this shell
+	FormListModule formModule = null;
+	SplashScreenModule splashScreen = null;
+
+	public JavaRosaDemoShell() {
+
+	}
+
+	public void ExitShell() {
+
+	}
+
+	public void Run() {
+		this.splashScreen = new SplashScreenModule(this, "/splash.gif");
+		this.formModule = new FormListModule(this,"Forms List");
+		
+		this.splashScreen.start();
+	//	switchView(ViewTypes.FORM_LIST);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.shell.IShell#moduleCompeleted(org.javarosa.module.IModule)
+	 */
+	public void moduleCompleted(IModule module, Hashtable context) {
+		// TODO Auto-generated method stub
+		if( module == this.splashScreen )
+			this.formModule.start();
+	}
+
+}
