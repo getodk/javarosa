0f9812329f77fc889655eaebd2589f4a3b2b8d2c
csims <csims@localhost>
2008-07-03 20:04:23 +0000

    [r463] [branches/dev] Initialization code moved here

--- JavarosaDemo/src/org/javarosa/demo/shell/JavaRosaDemoShell.java
+++ JavarosaDemo/src/org/javarosa/demo/shell/JavaRosaDemoShell.java
@@ -4,13 +4,17 @@ import java.util.Hashtable;
 
 import javax.microedition.lcdui.Displayable;
 
-import org.javarosa.core.JavaRosaPlatform;;
+import org.javarosa.core.Context;
+import org.javarosa.core.JavaRosaPlatform;
 import org.javarosa.core.api.Constants;
 import org.javarosa.core.api.IModule;
 import org.javarosa.core.api.IShell;
+import org.javarosa.core.model.storage.FormDataRMSUtility;
+import org.javarosa.core.model.storage.FormDefRMSUtility;
 import org.javarosa.core.util.WorkflowStack;
 import org.javarosa.demo.module.FormListModule;
 import org.javarosa.demo.module.SplashScreenModule;
+import org.javarosa.xform.util.XFormUtils;
 
 /**
  * This is the shell for the JavaRosa demo that handles switching all of the views
@@ -24,13 +28,13 @@ public class JavaRosaDemoShell implements IShell {
 	
 	WorkflowStack stack;
 	
-	Hashtable context;
+	Context context;
 	
 	IModule currentModule;
 
 	public JavaRosaDemoShell() {
 		stack = new WorkflowStack(); 
-		context = new Hashtable();
+		context = new Context();
 	}
 
 	public void exitShell() {
@@ -38,23 +42,43 @@ public class JavaRosaDemoShell implements IShell {
 	}
 	
 	public void run() {
+		init();
 		this.splashScreen = new SplashScreenModule(this, "/splash.gif");
 		this.formModule = new FormListModule(this,"Forms List");
 		
-		this.splashScreen.start();
+		this.splashScreen.start(context);
 		currentModule = splashScreen;
 	//	switchView(ViewTypes.FORM_LIST);
+	}
+	
+	private void init() {
+
+		FormDataRMSUtility formData = new FormDataRMSUtility("FormDataRMS");
+		FormDefRMSUtility formDef = new FormDefRMSUtility("FormDefRMS");
+
+		// For now let's add the dummy form.
+		if (formDef.getNumberOfRecords() == 0) {
+			formDef.writeToRMS(XFormUtils
+					.getFormFromResource("/hmis-a_draft.xhtml"));
+			formDef.writeToRMS(XFormUtils
+					.getFormFromResource("/hmis-b_draft.xhtml"));
+			formDef.writeToRMS(XFormUtils
+					.getFormFromResource("/shortform.xhtml"));
+		}
+		JavaRosaPlatform.instance().getStorageManager().getRMSStorageProvider()
+				.registerRMSUtility(formData);
+		JavaRosaPlatform.instance().getStorageManager().getRMSStorageProvider()
+				.registerRMSUtility(formDef);
 	}
 	
 	private void workflow(IModule lastModule, String cmd, Hashtable returnVals) {
 		//TODO: parse any returnvals into context
 		if(stack.size() != 0) {
-			stack.pop().resume();
+			stack.pop().resume(context);
 		}
 		// TODO Auto-generated method stub
 		if( lastModule == this.splashScreen ) {
-			this.formModule.setContext(this.context);
-			this.formModule.start();
+			this.formModule.start(context);
 			currentModule = formModule;
 		}
 	}
@@ -63,9 +87,9 @@ public class JavaRosaDemoShell implements IShell {
 	 * @see org.javarosa.shell.IShell#moduleCompeleted(org.javarosa.module.IModule)
 	 */
 	public void returnFromModule(IModule module, String returnCode, Hashtable returnVals) {
-		Hashtable moduleContext = module.halt();
+		module.halt();
 		if(returnCode != Constants.MODULE_COMPLETE) {
-			stack.push(module,moduleContext);
+			stack.push(module,module.getContext());
 		}
 		workflow(module, returnCode, returnVals);
 	}
