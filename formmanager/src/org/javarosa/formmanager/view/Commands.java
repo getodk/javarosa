101d7794cf9d025b4d9a09c8f3f3bccf06891a91
csims <csims@localhost>
2008-07-07 14:44:55 +0000

    [r468] [branches/dev] Initial FormList view and module placed in here from Demo.

---
+++ formmanager/src/org/javarosa/formmanager/view/Commands.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+package org.javarosa.formmanager.view;
+
+/**
+ * @author Brian DeRenzi
+ *
+ */
+public class Commands {
+	public static final String CMD_NONE = "none";
+	public static final String CMD_SELECT_XFORM = "select_xform";
+}
