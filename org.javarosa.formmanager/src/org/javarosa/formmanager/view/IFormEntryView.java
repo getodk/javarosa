76a8e5ade7b5d709101fd1336783a8ca0d62df70
csims <ctsims@users.noreply.github.com>
2008-07-14 18:22:47 +0000

    [r578] [branches/dev] Merged in code

--- org.javarosa.formmanager/src/org/javarosa/formmanager/view/IFormEntryView.java
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/view/IFormEntryView.java
@@ -1,7 +1,17 @@
 package org.javarosa.formmanager.view;
 
+/**
+ * 
+ * A view for displaying a form to the user, and allowing them to fill
+ * out values. 
+ * 
+ * @author Drew Roos
+ *
+ */
 public interface IFormEntryView {
 	public void destroy ();
 	
 	public void formComplete ();
+	
+	public void show();
 }
