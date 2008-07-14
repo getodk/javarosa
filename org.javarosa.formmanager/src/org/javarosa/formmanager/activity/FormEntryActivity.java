2a35a04b42159628282e8c8c45266e4cb1fedac7
csims <csims@localhost>
2008-07-14 20:14:46 +0000

    [r580] [branches/dev] Added an error when FormEntry fails to obtain a form, set the model to actually accept a FormDef.

--- org.javarosa.formmanager/src/org/javarosa/formmanager/activity/FormEntryActivity.java
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/activity/FormEntryActivity.java
@@ -1,14 +1,21 @@
 package org.javarosa.formmanager.activity;
 
+import java.io.IOException;
 import java.util.Enumeration;
 import java.util.Vector;
 
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Form;
 
 import org.javarosa.core.Context;
+import org.javarosa.core.JavaRosaServiceProvider;
 import org.javarosa.core.api.Constants;
 import org.javarosa.core.api.IActivity;
 import org.javarosa.core.api.IShell;
+import org.javarosa.core.model.FormDef;
+import org.javarosa.core.model.storage.FormDefRMSUtility;
 import org.javarosa.formmanager.controller.FormEntryController;
 import org.javarosa.formmanager.model.FormEntryModel;
 import org.javarosa.formmanager.model.IControllerListener;
@@ -27,10 +34,13 @@ public class FormEntryActivity implements IActivity, IControllerListener {
 	private FormEntryModel model;
 
 	/** Current running context **/
-	FormEntryContext context;
+	private FormEntryContext context;
 	
 	/** The parent shell **/
-	IShell parent;
+	private IShell parent;
+	
+	/** Loading error string **/
+	private final static String LOAD_ERROR = "Deepest Apologies. The form could not be loaded.";
 
 	public FormEntryActivity(IShell parent) {
 		this.parent = parent;
@@ -49,25 +59,47 @@ public class FormEntryActivity implements IActivity, IControllerListener {
 	}
 	
 	public void start (Context context) {
-		this.context = (FormEntryContext)context;
-		
-		//load form
-		//parse form
-		//pre-process form
-		
+		FormDef theForm = null;
 		if(context.getClass() == FormEntryContext.class) {
-			context = (FormEntryContext)context;
-			//Set the form definition here
+			this.context = (FormEntryContext)context;			
+			
+			//TODO: Are we going to make this non-RMS dependant any any point?
+			FormDefRMSUtility utility = (FormDefRMSUtility)JavaRosaServiceProvider.instance().getStorageManager().getRMSStorageProvider().getUtility(FormDefRMSUtility.getUtilityName());
+			theForm = new FormDef();
+			try {
+				utility.retrieveFromRMS(this.context.getFormID(),theForm);
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			}
+			catch (IllegalAccessException e) {
+				e.printStackTrace();
+			}
+			catch (InstantiationException e) {
+				e.printStackTrace();
+			}
 		}
-		
-		model = new FormEntryModel(/* form data */);
-		controller = new FormEntryController(model, this);
-		view = new Chatterbox("Chatterbox", model, controller); //shouldn't reference this directly
-		
-		controller.setView(view);
+		if (theForm != null) {
 
-		//We need to figure out how to identify the View that should be used here.
-		//Probably with the properties
+			// load form
+			// parse form
+			// pre-process form
+
+			model = new FormEntryModel(theForm);
+			controller = new FormEntryController(model, this);
+			view = new Chatterbox("Chatterbox", model, controller); // shouldn't
+																	// reference
+																	// this
+																	// directly
+
+			controller.setView(view);
+
+			// We need to figure out how to identify the View that should be
+			// used here.
+			// Probably with the properties
+		} else {
+			displayError(LOAD_ERROR);
+		}
 	}
 	
 	
@@ -86,5 +118,12 @@ public class FormEntryActivity implements IActivity, IControllerListener {
 	public void setView(Displayable view) {
 		parent.setDisplay(this, view);
 		
+	}
+	
+	private void displayError(String errorMsg) {
+		Alert alert = new Alert("Form Entry Error");
+		alert.setType(AlertType.ERROR);
+		alert.setString(errorMsg);
+		setView(alert);
 	}
 }
