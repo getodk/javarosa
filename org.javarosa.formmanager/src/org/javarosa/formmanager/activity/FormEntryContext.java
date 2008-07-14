1469b01c06457f66c8f2c688d1c0e485822aab95
Drew Roos <mrgriscom@users.noreply.github.com>
2008-07-14 17:50:08 +0000

    [r577] [branches/dev] [no message]

---
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/activity/FormEntryContext.java
@@ -0,0 +1,24 @@
+package org.javarosa.formmanager.activity;
+
+import org.javarosa.core.*;
+
+public class FormEntryContext extends Context {
+	public static final String FORM_ID = "FORM_ID";
+	public static final String INSTANCE_ID = "INSTANCE_ID";
+	
+	public int getFormID () {
+		return ((Integer)getElement(FORM_ID)).intValue();
+	}
+	
+	public void setFormID (int formID) {
+		setElement(FORM_ID, new Integer(formID));
+	}
+
+	public int getInstanceID () {
+		return ((Integer)getElement(INSTANCE_ID)).intValue();
+	}
+	
+	public void setInstanceID (int instanceID) {
+		setElement(INSTANCE_ID, new Integer(instanceID));
+	}
+}
