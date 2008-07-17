7c62a7a33a6eeecbac1268fd0d0bfe13f985a226
csims <csims@localhost>
2008-07-17 16:15:02 +0000

    [r590] [branches/dev] Broken, but compiling

--- org.javarosa.xform/src/org/javarosa/model/xform/XPathReference.java
+++ org.javarosa.xform/src/org/javarosa/model/xform/XPathReference.java
@@ -26,4 +26,16 @@ public class XPathReference implements IDataReference {
 	public void setReference (Object o) {
 		xpath = (String)o;
 	}
+	public boolean referenceMatches(IDataReference reference) {
+		if(reference.getClass() != XPathReference.class) {
+			return getReference().equals(reference);
+		}
+		else {
+			//Put xpath specific reference matching here;
+			XPathReference extReference = (XPathReference)reference;
+			//This does the same thing as above for now, eventually
+			//we should fill in * references and stuff
+			return extReference.xpath.equals(xpath);
+		}
+	}
 }
