80726de22619b7585f95115b83993553c9a0b964
csims <csims@localhost>
2008-07-17 20:27:43 +0000

    [r593] [branches/dev] Added the XFormAnswerDataSerializer

---
+++ org.javarosa.xform/src/org/javarosa/xform/util/XFormAnswerDataSerializer.java
@@ -0,0 +1,124 @@
+package org.javarosa.xform.util;
+
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.javarosa.core.model.IAnswerDataSerializer;
+import org.javarosa.core.model.utils.DateUtils;
+import org.javarosa.formmanager.model.temp.QuestionData;
+import org.javarosa.formmanager.model.temp.DateData;
+import org.javarosa.formmanager.model.temp.SelectMultiData;
+import org.javarosa.formmanager.model.temp.SelectOneData;
+import org.javarosa.formmanager.model.temp.Selection;
+import org.javarosa.formmanager.model.temp.StringData;
+
+/**
+ * The XFormAnswerDataSerializer taks in AnswerData objects, and provides
+ * an XForms compliant (String or Element) representation of that AnswerData.
+ * 
+ * By default, this serializer can properly operate on StringData, DateData
+ * SelectMultiData, and SelectOneData AnswerData objects. This list can be
+ * extended by registering appropriate XForm serializing AnswerDataSerializers
+ * with this class.
+ * 
+ * @author Clayton Sims
+ *
+ */
+public class XFormAnswerDataSerializer implements IAnswerDataSerializer {
+	
+	Vector additionalSerializers = new Vector();
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.javarosa.core.model.IAnswerDataSerializer#registerAnswerSerializer(org.javarosa.core.model.IAnswerDataSerializer)
+	 */
+	public void registerAnswerSerializer(IAnswerDataSerializer ads) {
+		additionalSerializers.addElement(ads);
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.javarosa.core.model.IAnswerDataSerializer#canSerialize(org.javarosa.formmanager.model.temp.QuestionData)
+	 */
+	public boolean canSerialize(QuestionData data) {
+		if (data.getClass() == StringData.class
+				|| data.getClass() == DateData.class
+				|| data.getClass() == SelectMultiData.class
+				|| data.getClass() == SelectOneData.class) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	/**
+	 * @param data The AnswerDataObject to be serialized
+	 * @return A String which contains the given answer
+	 */
+	public Object serializeAnswerData(StringData data) {
+		return (String)data.getValue();
+	}
+	
+	/**
+	 * @param data The AnswerDataObject to be serialized
+	 * @return A String which contains a date in xsd:date
+	 * formatting
+	 */
+	public Object serializeAnswerData(DateData data) {
+		return DateUtils.getXMLStringValue((Date)data.getValue());
+	}
+	
+	/**
+	 * @param data The AnswerDataObject to be serialized
+	 * @return A string containing the xforms compliant format
+	 * for a <select> tag, a string containing a list of answers
+	 * separated by space characters.
+	 */
+	public Object serializeAnswerData(SelectMultiData data) {
+		Vector selections = (Vector)data.getValue();
+		Enumeration en = selections.elements();
+		String selectString = "";
+		
+		while(en.hasMoreElements()) {
+			Selection selection = (Selection)en.nextElement();
+			selectString = selectString + " " + selection.getText();
+		}
+		//As Crazy, and stupid, as it sounds, this is the XForms specification
+		//for storing multiple selections.	
+		return selectString;
+	}
+	
+	/**
+	 * @param data The AnswerDataObject to be serialized
+	 * @return A String which contains the value of a selection
+	 */
+	public Object serializeAnswerData(SelectOneData data) {
+		return ((Selection)data.getValue()).getText();
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.javarosa.core.model.IAnswerDataSerializer#serializeAnswerData(org.javarosa.formmanager.model.temp.QuestionData)
+	 */
+	public Object serializeAnswerData(QuestionData data) {
+		if(data.getClass() == StringData.class) {
+			return serializeAnswerData((StringData)data);
+		} else if(data.getClass() == DateData.class) {
+			return serializeAnswerData((DateData)data);
+		} else if(data.getClass() == SelectMultiData.class) {
+			return serializeAnswerData((SelectMultiData)data);
+		} else if(data.getClass() == SelectOneData.class) {
+			return serializeAnswerData((SelectOneData)data);
+		} else {
+			Enumeration en = additionalSerializers.elements();
+			while(en.hasMoreElements()) {
+				IAnswerDataSerializer serializer = (IAnswerDataSerializer)en.nextElement();
+				if(serializer.canSerialize(data)) {
+					return serializer.serializeAnswerData(data);
+				}
+			}
+		}
+		return null;
+	}
+}
