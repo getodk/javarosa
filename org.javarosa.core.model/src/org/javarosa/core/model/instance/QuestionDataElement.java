a2639dc1ca9239f557b858c656f618a6c79e117f
csims <csims@localhost>
2008-07-18 16:53:08 +0000

    [r609] [branches/dev] Serialization logic now makes sense/should work.

--- org.javarosa.core.model/src/org/javarosa/core/model/instance/QuestionDataElement.java
+++ org.javarosa.core.model/src/org/javarosa/core/model/instance/QuestionDataElement.java
@@ -11,136 +11,167 @@ import org.javarosa.core.model.utils.ExternalizableHelper;
 import org.javarosa.core.services.storage.utilities.UnavailableExternalizerException;
 
 /**
- * QuestionDataElement is a TreeElement of a DataModelTree that is a leaf
- * which contains the answer to a QuestionDef. 
+ * QuestionDataElement is a TreeElement of a DataModelTree that is a leaf which
+ * contains the answer to a QuestionDef.
  * 
  * In an XML Analogy, this represents a terminal element in the XML tree.
  * 
  * @author Clayton Sims
- *
+ * 
  */
-public class QuestionDataElement extends TreeElement{
+public class QuestionDataElement extends TreeElement {
 
 	/** The actual question data value */
 	private AnswerData value;
-	
+
 	/** A Binding for the Question Definition */
 	private IDataReference reference;
+
+	/**
+	 * Creates a new, blank, QuestionDataElement;
+	 */
+	public QuestionDataElement() {
+	}
 	
 	/**
-	 * Creates a new QuestionDataElement for the question defined
-	 * by the name and reference provided
+	 * Creates a new QuestionDataElement for the question defined by the name
+	 * and reference provided
 	 * 
-	 * @param name The name of this TreeElement
-	 * @param reference The reference for Question Definitions
+	 * @param name
+	 *            The name of this TreeElement
+	 * @param reference
+	 *            The reference for Question Definitions
 	 */
 	public QuestionDataElement(String name, IDataReference reference) {
 		this.name = name;
 		this.reference = reference;
 	}
-	
+
 	/**
-	 * Creates a new QuestionDataElement for the question defined
-	 * by the name and reference provided, and sets its value to that 
-	 * provided.
-	 *  
-	 * @param name The name of this TreeElement
-	 * @param reference The reference for Question Definitions
-	 * @param value The value for this Question Definition
+	 * Creates a new QuestionDataElement for the question defined by the name
+	 * and reference provided, and sets its value to that provided.
+	 * 
+	 * @param name
+	 *            The name of this TreeElement
+	 * @param reference
+	 *            The reference for Question Definitions
+	 * @param value
+	 *            The value for this Question Definition
 	 */
-	public QuestionDataElement(String name, IDataReference reference, AnswerData value) {
+	public QuestionDataElement(String name, IDataReference reference,
+			AnswerData value) {
 		this(name, reference);
 		this.value = value;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.model.TreeElement#getName()
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * @return The value for the question defined by IBinding
 	 */
-	public AnswerData getValue(){ 
+	public AnswerData getValue() {
 		return value;
 	}
-	
+
 	/**
 	 * Sets the value for the question defined by IBinding
-	 * @param value The question's answer value
+	 * 
+	 * @param value
+	 *            The question's answer value
 	 */
 	public void setValue(AnswerData value) {
 		this.value = value;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.model.TreeElement#isLeaf()
 	 */
 	public boolean isLeaf() {
 		return true;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.model.TreeElement#setRoot(org.javarosa.core.model.TreeElement)
 	 */
 	protected void setRoot(TreeElement root) {
 		this.root = root;
 	}
-	
+
 	/*
 	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.model.TreeElement#contains(org.javarosa.core.model.TreeElement)
 	 */
 	public boolean contains(TreeElement element) {
-		if(this == element) {
+		if (this == element) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	
+
 	public boolean matchesReference(IDataReference reference) {
-		if(this.reference == null) {
+		if (this.reference == null) {
 			return false;
-		}
-		else {
+		} else {
 			return this.reference.referenceMatches(reference);
 		}
 	}
-	
+
 	/**
 	 * @return a string representing the value of this question's answer
 	 */
 	public String createStringValue() {
 		return value.toString();
 	}
+
 	/**
 	 * Visitor pattern acceptance method.
-	 * @param visitor The visitor traveling this tree
+	 * 
+	 * @param visitor
+	 *            The visitor traveling this tree
 	 */
 	public void accept(ITreeVisitor visitor) {
 		visitor.visit(this);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.services.storage.utilities.Externalizable#readExternal(java.io.DataInputStream)
 	 */
 	public void readExternal(DataInputStream in) throws IOException,
-			InstantiationException, IllegalAccessException, UnavailableExternalizerException {
+			InstantiationException, IllegalAccessException,
+			UnavailableExternalizerException {
 		this.name = ExternalizableHelper.readUTF(in);
+		if (reference == null) {
+			throw new UnavailableExternalizerException(
+					"Attempt to resolve serialization for a DataModelTree failed because there was no reference " +
+					"template available to deserialize the stored reference");
+		}
 		reference.readExternal(in);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.services.storage.utilities.Externalizable#writeExternal(java.io.DataOutputStream)
 	 */
 	public void writeExternal(DataOutputStream out) throws IOException {
-		// TODO Auto-generated method stub
+		//This flag is in place to determine whether a Data element is a Group or a Data
+		//True for groups, false for DataElements
+		ExternalizableHelper.writeBoolean(out, false);
 		ExternalizableHelper.writeUTF(out, this.name);
 		reference.writeExternal(out);
 	}
