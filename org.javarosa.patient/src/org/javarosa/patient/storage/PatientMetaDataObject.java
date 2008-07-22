f73191f35bb0f3fd5f23a20a449787aa2206f8c9
csims <csims@localhost>
2008-07-22 19:01:00 +0000

    [r664] [branches/dev] Simple Patient API basis

---
+++ org.javarosa.patient/src/org/javarosa/patient/storage/PatientMetaDataObject.java
@@ -0,0 +1,93 @@
+package org.javarosa.patient.storage;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.javarosa.core.services.storage.utilities.MetaDataObject;
+import org.javarosa.patient.model.Patient;
+
+public class PatientMetaDataObject extends MetaDataObject {
+
+    private String patientName = ""; //the name of the FormData being referenced
+	private int patientIdReference;
+
+    
+    public String toString(){
+    	return new String (super.toString()+" name: "+this.patientName);
+    }
+    
+    /**
+     * Creates an Empty meta data object
+     */
+    public PatientMetaDataObject()
+    {
+        
+    }
+    
+    /**
+     * Creates a meta data object for the patient data object given.
+     * 
+     * @param form The patient whose meta data this object will become
+     */
+    public PatientMetaDataObject(Patient patient)
+    {
+        this.patientName = patient.getFullName();
+        this.patientIdReference = patient.getRecordId();
+        
+    }
+    
+    /**
+     * @param name Sets the name for the form this meta data object represents
+     */
+    public void setName(String name)
+    {
+        this.patientName = name;
+    }
+    
+    /**
+     * @return the name of the form represented by this meta data
+     */
+    public String getName()
+    {
+       return this.patientName; 
+    }
+    
+    /**
+     * @return the RMS Storage Id for the form this meta data represents
+     */
+    public int getPatientIdReference() {
+    	return patientIdReference;
+    }
+    
+    /* (non-Javadoc)
+     * @see org.javarosa.clforms.storage.MetaDataObject#readExternal(java.io.DataInputStream)
+     */
+    public void readExternal(DataInputStream in) throws IOException
+    {
+    	super.readExternal(in);
+        this.patientName = in.readUTF();
+        this.patientIdReference = in.readInt();
+    }
+
+   
+    /* (non-Javadoc)
+     * @see org.javarosa.clforms.storage.MetaDataObject#writeExternal(java.io.DataOutputStream)
+     */
+    public void writeExternal(DataOutputStream out) throws IOException
+    {
+    	super.writeExternal(out);
+    	out.writeUTF(this.getName());
+    	out.writeInt(this.getPatientIdReference());
+    }
+
+    /*
+     * (non-Javadoc)
+     * @see org.javarosa.core.services.storage.utilities.MetaDataObject#setMetaDataParameters(java.lang.Object)
+     */
+    public void setMetaDataParameters(Object object)
+    {
+        Patient patient = (Patient)object;
+        this.setName(patient.getFullName());
+    }
+}
