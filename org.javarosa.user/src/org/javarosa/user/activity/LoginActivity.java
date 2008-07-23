b1191c4e32e4dfce69bcff074c1a5c436c9ee901
kieran <kieran@localhost>
2008-07-23 18:08:40 +0000

    [r676] [branches/dev] first commit

---
+++ org.javarosa.user/src/org/javarosa/user/activity/LoginActivity.java
@@ -0,0 +1,125 @@
+/**
+ *
+ */
+package org.javarosa.user.activity;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.ItemCommandListener;
+
+import org.javarosa.core.Context;
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.api.Constants;
+import org.javarosa.core.api.IActivity;
+import org.javarosa.core.api.IShell;
+import org.javarosa.user.view.LoginForm;
+
+/**
+ * @author Kieran Sharpey - Schafer
+ *
+ */
+public class LoginActivity implements IActivity, CommandListener, ItemCommandListener {
+
+	private LoginForm loginScreen = null;
+	private IShell parent = null;
+	public static final String COMMAND_KEY = "command";
+	public static final String USER = "user";
+
+	Context context;
+
+	public LoginActivity(IShell p, String title) {
+		this.parent = p;
+		this.loginScreen = new LoginForm(this, title);
+		this.loginScreen.setCommandListener(this);
+		this.loginScreen.loginButton.setItemCommandListener(this);       // set item command listener
+	}
+
+	public void start(Context context) {
+		parent.setDisplay(this, this.loginScreen);
+		this.context = context;
+	}
+
+	public void commandAction(Command c, Item item) {
+		if (c == this.loginScreen.nextItemCommand) {
+			System.out.println("login pressed");
+			if (loginScreen.validateUser()) {
+				javax.microedition.lcdui.Alert success = loginScreen
+						.successfulLogin();
+				parent.setDisplay(this, success);
+				Hashtable returnArgs = new Hashtable();
+				returnArgs.put(COMMAND_KEY, Commands.CMD_LOGIN_SUCCESS);
+				returnArgs.put(USER, loginScreen.getLoggedInUser());
+				parent.returnFromActivity(this, Constants.ACTIVITY_COMPLETE,
+						returnArgs);
+			} else {
+				// /display an error that login failed and return to login
+				// screen
+				javax.microedition.lcdui.Alert error = loginScreen.tryAgain();
+				parent.setDisplay(this, error);
+				parent.setDisplay(this, this.loginScreen);
+
+			}
+		}
+	}
+
+	public void viewCompleted(Hashtable returnvals, int view_ID) {
+	}
+
+	public void contextChanged(Context context) {
+		Vector contextChanges = this.context.mergeInContext(context);
+
+		Enumeration en = contextChanges.elements();
+		while (en.hasMoreElements()) {
+			String changedValue = (String) en.nextElement();
+			if (changedValue == Constants.USER_KEY) {
+				// update username somewhere
+			}
+		}
+	}
+
+	public void halt() {
+
+	}
+
+	public void resume(Context context) {
+		this.contextChanged(context);
+		// Possibly want to check for new/updated forms
+		JavaRosaServiceProvider.instance().showView(this.loginScreen);
+	}
+
+	public void destroy() {
+
+	}
+
+	public void commandAction(Command c, Displayable d) {
+		if (c == loginScreen.CMD_CANCEL_LOGIN) {
+			parent.exitShell();
+		} else if (c == loginScreen.CMD_LOGIN_LOGIN) {
+			System.out.println("login pressed");
+			if (loginScreen.validateUser()) {
+				javax.microedition.lcdui.Alert success = loginScreen
+						.successfulLogin();
+				parent.setDisplay(this, success);
+				Hashtable returnArgs = new Hashtable();
+				returnArgs.put(COMMAND_KEY, Commands.CMD_LOGIN_SUCCESS);
+				returnArgs.put(USER, loginScreen.getLoggedInUser());
+				parent.returnFromActivity(this, Constants.ACTIVITY_COMPLETE,
+						returnArgs);
+			} else {
+				// /display an error that login failed and return to login
+				// screen
+				javax.microedition.lcdui.Alert error = loginScreen.tryAgain();
+				parent.setDisplay(this, error);
+				parent.setDisplay(this, this.loginScreen);
+
+			}
+		}
+		// #endif
+	}
+}
---
+++ org.javarosa.user/src/org/javarosa/user/model/User.java
@@ -0,0 +1,121 @@
+package org.javarosa.user.model;
+
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.javarosa.core.services.storage.utilities.IDRecordable;
+import org.javarosa.core.services.storage.utilities.Externalizable;
+
+//import javax.microedition.rms.RecordStoreException;
+
+
+
+public class User implements Externalizable, IDRecordable
+{
+	//USERTYPEs
+	public static final String ADMINUSER = "admin";
+	public static final String USERTYPE1 = "TLP";
+	public static final String STANDARD = "standard";
+
+	private int recordId = 0; //this objects ID in the RMS
+	private String username;
+	private String password;
+	private String userType;
+
+	private int [] formsApplied;
+
+
+	public User ()
+	{
+		userType = STANDARD;
+	}
+
+	public void setType (String typeIn)
+	{
+		userType = typeIn;
+	}
+
+	public String getType()
+	{
+		return userType;
+	}
+	public User(String name, String passw)
+	{
+		username = name;
+		password = passw;
+		userType =  STANDARD;
+	}
+	public User(String name, String passw, String isAAdmin)
+	{
+		username = name;
+		password = passw;
+		if (isAAdmin.equals( ADMINUSER))
+		{
+			this.setType( ADMINUSER);
+		}
+		else if (isAAdmin.equals( STANDARD))
+		{
+			this.setType( STANDARD);
+		}
+		else if (isAAdmin.equals( USERTYPE1))
+			this.setType( USERTYPE1);
+		else
+			System.out.println("while creating user, an invalid isAAdmin variable was passed in: options are \"STANDARD\" or \"ADMINSUER\" or \"USERTYPE1\"");
+	}
+
+	///fetch the value for the default user and password from the RMS
+	public void readExternal(DataInputStream in) throws IOException {
+		try {
+			this.username = in.readUTF();
+			this.password = in.readUTF();
+			this.userType = in.readUTF();
+		}
+		catch (IOException ioe)
+		{
+			ioe.printStackTrace();
+			System.out.println(ioe);
+		}
+	}
+
+	public boolean isAdminUser()
+	{
+		if (userType.equals( ADMINUSER))
+		return true;
+		else return false;
+	}
+
+	public void writeExternal(DataOutputStream out) throws IOException {
+		try {
+			out.writeUTF(this.username);
+	        out.writeUTF(this.password);
+	        out.writeUTF(this.userType);
+
+		} catch (IOException e) {
+			throw new RuntimeException(e.getMessage()); //$NON-NLS-1$
+		}
+
+	}
+
+	public String getUsername()
+	{
+		return username;
+	}
+
+	public String getPassword()
+	{
+		return password;
+	}
+
+	public void setRecordId(int recordId)
+	{
+
+		this.recordId = recordId;
+	}
+
+	public int getRecordId() {
+		return recordId;
+	}
+
+}
