9581f3f3d2186c2181685dbf25890851a2164e51
Drew Roos <mrgriscom@users.noreply.github.com>
2008-07-24 20:07:48 +0000

    [r705] [branches/dev] support for saving form

--- org.javarosa.formmanager/src/org/javarosa/formmanager/utility/FormEntryModelListener.java
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/utility/FormEntryModelListener.java
@@ -2,4 +2,5 @@ package org.javarosa.formmanager.utility;
 
 public interface FormEntryModelListener {
 	void questionIndexChanged (int questionIndex);
+	void saveStateChanged (int instanceID, boolean dirty);
 }
