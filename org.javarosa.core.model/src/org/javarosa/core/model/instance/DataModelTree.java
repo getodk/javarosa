00e7832cb96134bd6c92396f82d564ccd12121b5
Drew Roos <mrgriscom@users.noreply.github.com>
2008-07-25 17:58:22 +0000

    [r716] [branches/dev] fixed bug where couldn't find data nodes that occurred after the first nested element

--- org.javarosa.core.model/src/org/javarosa/core/model/instance/DataModelTree.java
+++ org.javarosa.core.model/src/org/javarosa/core/model/instance/DataModelTree.java
@@ -133,19 +133,23 @@ public class DataModelTree implements IFormDataModel {
 	 */
 	private QuestionDataElement resolveReference(IDataReference binding, QuestionDataGroup group) {
 		//TODO: We can do this much more intelligently given that we know the format of the bindings
-		Enumeration en = group.getChildren().elements();
-		while(en.hasMoreElements()) {
+		QuestionDataElement target = null;
+		
+		Enumeration en = group.getChildren().elements();		
+		while(target == null && en.hasMoreElements()) {
 			TreeElement dme = (TreeElement)en.nextElement();
+			
 			if(!dme.isLeaf()) {
-				return resolveReference(binding, (QuestionDataGroup)dme);
+				target = resolveReference(binding, (QuestionDataGroup)dme);
 			} else {
 				if ((dme instanceof QuestionDataElement)
 						&& ((QuestionDataElement) dme).matchesReference(binding)) {
-					return (QuestionDataElement) dme;
+					target = (QuestionDataElement) dme;
 				}
 			}
 		}
-		return null;
+
+		return target;
 	}
 	
 	/**
