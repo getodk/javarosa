68e077c686464f0607da26396a322bcaa04b4853
droos <droos@localhost>
2008-07-29 00:30:24 +0000

    [r734] [branches/dev] don't let the the StringData object contain nulls or empty strings. The entire StringData object should be null in these cases

--- org.javarosa.core.model/src/org/javarosa/core/model/data/StringData.java
+++ org.javarosa.core.model/src/org/javarosa/core/model/data/StringData.java
@@ -4,6 +4,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
+import org.javarosa.core.model.utils.ExternalizableHelper;
 import org.javarosa.core.services.storage.utilities.UnavailableExternalizerException;
 
 public class StringData implements IAnswerData {
@@ -20,11 +21,10 @@ public class StringData implements IAnswerData {
 	public StringData (String s) {
 		setValue(s);
 	}
-	
+
+	//string should not be null or empty; the entire StringData reference should be null in this case
 	public void setValue (Object o) {
 		s = (String)o;
-		if (s == null)
-			s = "";
 	}
 	
 	public Object getValue () {
@@ -40,14 +40,13 @@ public class StringData implements IAnswerData {
 	public void readExternal(DataInputStream in) throws IOException,
 			InstantiationException, IllegalAccessException,
 			UnavailableExternalizerException {
-		this.setValue(in.readUTF());
-		
+		this.setValue(ExternalizableHelper.readUTF(in));		
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.services.storage.utilities.Externalizable#writeExternal(java.io.DataOutputStream)
 	 */
 	public void writeExternal(DataOutputStream out) throws IOException {
-		out.writeUTF(this.s);
+		ExternalizableHelper.writeUTF(out, this.s);
 	}
 }
