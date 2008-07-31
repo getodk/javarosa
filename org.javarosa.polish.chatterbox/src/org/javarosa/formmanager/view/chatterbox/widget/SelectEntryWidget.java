fd23e8e949774f2c3b4d22f7fcbf0e68f843bbd5
csims <ctsims@users.noreply.github.com>
2008-07-31 19:17:50 +0000

    [r793] [branches/dev] Finally! Bug resolution for problems caused by 'up' and 'down' keypresses not being correctly handled by choicegroups implemented.
    Also, implemented a slightly better fix for scrolling to items, but causes a slight oddness in the scrolling flow.

--- org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/SelectEntryWidget.java
+++ org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/SelectEntryWidget.java
@@ -3,6 +3,7 @@ package org.javarosa.formmanager.view.chatterbox.widget;
 import org.javarosa.core.model.QuestionDef;
 
 import de.enough.polish.ui.ChoiceGroup;
+import de.enough.polish.ui.Container;
 import de.enough.polish.ui.Item;
 
 public abstract class SelectEntryWidget extends ExpandedWidget {
@@ -15,7 +16,21 @@ public abstract class SelectEntryWidget extends ExpandedWidget {
 	
 	protected Item getEntryWidget (QuestionDef question) {
 		this.question = question;
-		ChoiceGroup cg = new ChoiceGroup("", style);
+		
+		ChoiceGroup cg = new ChoiceGroup("", style) {
+			public int getRelativeScrollYOffset() {
+				if (!this.enableScrolling && this.parent instanceof Container) {
+					return ((Container)this.parent).getScrollYOffset() + this.relativeY + this.parent.relativeY;
+				}
+				int offset = this.targetYOffset;
+				//#ifdef polish.css.scroll-mode
+					if (!this.scrollSmooth) {
+						offset = this.yOffset;
+					}
+				//#endif
+				return offset;
+			}
+		};
 		
 		for (int i = 0; i < question.getSelectItems().size(); i++)
 			cg.append("", null);
