7b88b283a0f76e7ad8168d7dc23d1148c5fa37e1
Drew Roos <mrgriscom@users.noreply.github.com>
2008-08-01 17:54:54 +0000

    [r815] [branches/dev] fix internationalization bug where it would wipe out the data you had already entered into a text field

--- org.javarosa.core.model/src/org/javarosa/core/model/QuestionStateListener.java
+++ org.javarosa.core.model/src/org/javarosa/core/model/QuestionStateListener.java
@@ -8,10 +8,9 @@ public interface QuestionStateListener {
 	static final int CHANGE_INIT = 0x00;
 	static final int CHANGE_DATA = 0x01;
 	static final int CHANGE_LOCALE = 0x02;
-	static final int CHANGE_RELEVANCY = 0x04;
-	static final int CHANGE_ENABLED = 0x08;
-	static final int CHANGE_VISIBLE = 0x0A;
-	static final int CHANGE_OTHER = 0x0B;
+	static final int CHANGE_ENABLED = 0x04;
+	static final int CHANGE_VISIBLE = 0x08;
+	static final int CHANGE_OTHER = 0x10;
 
 	void questionStateChanged (QuestionDef question, int changeFlags);
 }
