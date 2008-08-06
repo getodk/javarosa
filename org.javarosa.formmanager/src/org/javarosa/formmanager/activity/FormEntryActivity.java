7c7adf0917c3ada79d1a2ec820d662978098c7c2
alfred <alfred@localhost>
2008-08-06 09:25:21 +0000

    [r851] [branches/dev] pass context to preload handler

--- org.javarosa.formmanager/src/org/javarosa/formmanager/activity/FormEntryActivity.java
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/activity/FormEntryActivity.java
@@ -21,7 +21,12 @@ import org.javarosa.core.model.IFormDataModel;
 import org.javarosa.core.model.instance.DataModelTree;
 import org.javarosa.core.model.storage.DataModelTreeRMSUtility;
 import org.javarosa.core.model.storage.FormDefRMSUtility;
+<<<<<<< .mine
+import org.javarosa.core.model.utils.ContextPreloadHandler;
 import org.javarosa.core.model.utils.IPreloadHandler;
+=======
+import org.javarosa.core.model.utils.IPreloadHandler;
+>>>>>>> .r850
 import org.javarosa.core.util.UnavailableExternalizerException;
 import org.javarosa.formmanager.controller.FormEntryController;
 import org.javarosa.formmanager.controller.IControllerHost;
@@ -33,35 +38,38 @@ public class FormEntryActivity implements IActivity, IControllerHost, CommandLis
 
 	/** Alert if the form cannot load **/
 	private Alert alert;
-	
+
 	/** View for entering data **/
 	private IFormEntryView view;
-	
+
 	/** View's controller **/
 	private FormEntryController controller;
-	
+
 	/** The form that is to be displayed to the user, and its values **/
 	private FormEntryModel model;
 
 	/** Current running context **/
 	private FormEntryContext context;
-	
+
 	/** The parent shell **/
 	private IShell parent;
-	
+
 	private IFormEntryViewFactory viewFactory;
-	
+
 	/** Loading error string **/
 	private final static String LOAD_ERROR = "Deepest Apologies. The form could not be loaded.";
+
+	//the context preload handler
+	private ContextPreloadHandler contextHandler;
 
 	public FormEntryActivity(IShell parent, IFormEntryViewFactory viewFactory) {
 		this.parent = parent;
 		this.viewFactory = viewFactory;
 	}
-	
+
 	public void contextChanged(Context context) {
 		Vector contextChanges = this.context.mergeInContext(context);
-		
+
 		Enumeration en = contextChanges.elements();
 		while(en.hasMoreElements()) {
 			String changedValue = (String)en.nextElement();
@@ -70,21 +78,21 @@ public class FormEntryActivity implements IActivity, IControllerHost, CommandLis
 			}
 		}
 	}
-	
+
 	public void start (Context context) {
 		FormDef theForm = null;
 		int instanceID = -1;
-		
+
 		if(context instanceof FormEntryContext) {
-			this.context = (FormEntryContext)context;			
-			instanceID = this.context.getInstanceID();			
-			
+			this.context = (FormEntryContext)context;
+			instanceID = this.context.getInstanceID();
+
 			//TODO: Are we going to make this non-RMS dependant any any point?
 			FormDefRMSUtility formUtil = (FormDefRMSUtility)JavaRosaServiceProvider.instance().getStorageManager().getRMSStorageProvider().getUtility(FormDefRMSUtility.getUtilityName());  //whoa!
 			theForm = new FormDef();
 			try {
 				formUtil.retrieveFromRMS(this.context.getFormID(), theForm);
-				
+
 				if (instanceID != -1) {
 					DataModelTreeRMSUtility modelUtil = (DataModelTreeRMSUtility)JavaRosaServiceProvider.instance().getStorageManager().getRMSStorageProvider().getUtility(DataModelTreeRMSUtility.getUtilityName());
 					IFormDataModel theModel = new DataModelTree();
@@ -115,61 +123,64 @@ public class FormEntryActivity implements IActivity, IControllerHost, CommandLis
 					}
 					
 				}
+				//set handler for preload context
+				contextHandler = new ContextPreloadHandler(context);
+				theForm.getPreloader().addPreloadHandler("context",contextHandler);
 				theForm.preloadModel();
 			}
 			if (theForm.getLocalizer() != null && theForm.getLocalizer().getLocale() == null) {
 				theForm.getLocalizer().setToDefault();
 			}
-			
+
 			model = new FormEntryModel(theForm, instanceID);
 			controller = new FormEntryController(model, this);
 			view = viewFactory.getFormEntryView("chatterbox", model, controller);
 			view.setContext(this.context);
 			controller.setView(view);
-			
+
 			view.show();
 		} else {
 			displayError(LOAD_ERROR);
 		}
 	}
-	
-	
+
+
 	public void halt () {
 		//need to do anything?
 		System.out.println("whoa, nelly! we're halting!");
 	}
-	
+
 	public void resume (Context globalContext) {
 		view.show();
 	}
-	
+
 	public void destroy () {
-		
+
 	}
-	
+
 	public void setDisplay (Displayable d) {
 		parent.setDisplay(this, d);
 	}
-	
+
 	public void controllerReturn (String status) {
 		if ("exit".equals(status)) {
 			Hashtable returnArgs = new Hashtable();
-			
+
 			returnArgs.put("INSTANCE_ID", new Integer(model.getInstanceID()));
 			returnArgs.put("DATA_MODEL", model.getForm().getDataModel());
 			returnArgs.put("FORM_COMPLETE", new Boolean(model.isFormComplete()));
 			returnArgs.put("QUIT_WITHOUT_SAVING", new Boolean(!model.isSaved()));
-			
+
 			parent.returnFromActivity(this, Constants.ACTIVITY_COMPLETE, returnArgs);
 		}
 	}
-	
+
 	public void commandAction(Command command, Displayable display) {
 		if(display == alert) {
 			parent.returnFromActivity(this, Constants.ACTIVITY_COMPLETE, null);
 		}
 	}
-	
+
 	private void displayError(String errorMsg) {
 		alert = new Alert("Form Entry Error",errorMsg,null,AlertType.ERROR);
 		alert.setTimeout(Alert.FOREVER);
