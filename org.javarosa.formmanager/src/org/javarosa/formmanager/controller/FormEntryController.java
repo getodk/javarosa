8a8330c1044a492634126aa283e2576352b05006
alfred <alfred@localhost>
2008-08-12 09:29:48 +0000

    [r948] [branches/dev] notify users of save actions

--- org.javarosa.formmanager/src/org/javarosa/formmanager/controller/FormEntryController.java
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/controller/FormEntryController.java
@@ -2,6 +2,7 @@ package org.javarosa.formmanager.controller;
 
 import java.util.Date;
 
+import javax.microedition.lcdui.AlertType;
 import javax.microedition.lcdui.Displayable;
 
 import org.javarosa.core.JavaRosaServiceProvider;
@@ -16,20 +17,20 @@ import org.javarosa.formmanager.view.IFormEntryView;
 public class FormEntryController {
 	public static final int QUESTION_OK = 0;
 	public static final int QUESTION_REQUIRED_BUT_EMPTY = 1;
-	
+
 	FormEntryModel model;
 	IFormEntryView view;
 	IControllerHost parent;
-	
+
 	public FormEntryController (FormEntryModel model, IControllerHost parent) {
 		this.model = model;
 		this.parent = parent;
 	}
-	
+
 	public void setView (IFormEntryView view) {
 		this.view = view;
 	}
-	
+
 	public int questionAnswered (QuestionDef question, IAnswerData data) {
 		if (question.isRequired() && data == null) {
 			return QUESTION_REQUIRED_BUT_EMPTY;
@@ -39,7 +40,7 @@ public class FormEntryController {
 			return QUESTION_OK;
 		}
 	}
-	
+
 	public boolean commitAnswer (QuestionDef question, IAnswerData data) {
 		if (data != null || model.getForm().getValue(question) != null) {
 			//we should check if the data to be saved is already the same as the data in the model, but we can't
@@ -50,15 +51,15 @@ public class FormEntryController {
 			return false;
 		}
 	}
-	
+
 	public void stepQuestion (boolean forward) {
 		int inc = (forward ? 1 : -1);
 		int index = model.getQuestionIndex();
-		
+
 		do {
 			index += inc;
 		} while (index >= 0 && index < model.getNumQuestions() && !model.getQuestion(index).isVisible());
-		
+
 		if (index < 0) {
 			//already at the earliest relevant question
 			return;
@@ -66,55 +67,62 @@ public class FormEntryController {
 			model.setFormComplete();
 			return;
 		}
-		
+
 		selectQuestion(index);
 	}
-	
+
 	public void selectQuestion (int questionIndex) {
 		model.setQuestionIndex(questionIndex);
 	}
-	
+
 	//saves model as is; view is responsible for committing any pending data in the current question
 	public void save () {
 		boolean postProcessModified = model.getForm().postProcessModel();
-		
+
 		if (!model.isSaved() || postProcessModified) {
 			FormDef form = model.getForm();
 			DataModelTreeRMSUtility utility = (DataModelTreeRMSUtility)JavaRosaServiceProvider.instance().getStorageManager().getRMSStorageProvider().getUtility(DataModelTreeRMSUtility.getUtilityName());
 			DataModelTree instance = (DataModelTree)form.getDataModel(); //worry about supporting other data model types later
 			int instanceID = model.getInstanceID();
-			
+
 			instance.setName(form.getName());
 	        instance.setFormReferenceId(form.getRecordId());
 	        instance.setDateSaved(new Date());
-			
+
+	        //notify of save details
+	        String info = "Form saved to 'Saved Forms' at "+ instance.getDateSaved().toString();
+	       //System.out.println(info);
+			javax.microedition.lcdui.Alert a = new javax.microedition.lcdui.Alert("Save Successful!", info,null,AlertType.INFO);
+			a.setTimeout(2000);
+			setDisplay(a);
+
 			if(instanceID == -1) {
 				instanceID = utility.writeToRMS(instance);
 			} else {
 				utility.updateToRMS(instanceID, instance);
-			}			
-		
+			}
+
 			model.modelSaved(instanceID);
 		}
 	}
-	
+
 	public void exit () {
 		view.destroy();
 		parent.controllerReturn("exit");
 	}
-	
+
 	public void startOver () {
-		
+
 	}
-	
+
 	public void setLanguage (String language) {
 		model.getForm().getLocalizer().setLocale(language);
 	}
-	
+
 	public void cycleLanguage () {
 		setLanguage(model.getForm().getLocalizer().getNextLocale());
 	}
-	
+
 	public void setDisplay (Displayable d) {
 		parent.setDisplay(d);
 	}
