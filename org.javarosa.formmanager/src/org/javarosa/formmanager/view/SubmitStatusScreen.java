28b5d34041c90bc08176c53dae9090b710a7e755
droos <droos@localhost>
2008-08-12 18:53:08 +0000

    [r951] [branches/dev] Destroying the polling timers for the 'submit status' screen when they are no longer needed.

--- org.javarosa.formmanager/src/org/javarosa/formmanager/view/SubmitStatusScreen.java
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/view/SubmitStatusScreen.java
@@ -1,58 +1,88 @@
 package org.javarosa.formmanager.view;
 
+import java.util.Enumeration;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.Spacer;
 import javax.microedition.lcdui.StringItem;
 
-import org.javarosa.core.services.transport.TransportMessage;
-import java.util.Enumeration;
 import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.services.transport.TransportMessage;
+import org.javarosa.formmanager.activity.FormTransportActivity;
 
 public class SubmitStatusScreen extends Form  {
+	private int modelID;
     private StringItem msg;
     private Command okCommand;
+    private Timer timer;
     int counter = 0;
 
     public static final int REFRESH_INTERVAL = 1000;
     public static final int TIMEOUT = 60000;
 
-	public SubmitStatusScreen () {
+    public static String MSG_SENDING = "Sending...";
+    public static String MSG_SUCCESS = "Form has been submitted successfully! Your reference is: ";
+    public static String MSG_FAILED = "Submission failed! Please try to submit the form again later in 'View Saved'.";
+    public static String MSG_TOO_LONG = "Sending is taking a long time; you may check on the status and/or resend later in 'View Saved'";
+    public static String MSG_UNKNOWN_ERROR = "Unknown sending error; form not sent!";
+    
+	public SubmitStatusScreen (FormTransportActivity fta, int modelID) {
     	//#style submitPopup
 		super("Send Status");
-
+		this.modelID = modelID;
+		setCommandListener(fta);
+		
 		okCommand = new Command("OK", Command.OK, 1);
-		msg = new StringItem(null, "Sending...");
+		msg = new StringItem(null, MSG_SENDING);
 
 		addCommand(okCommand);
 		append(new Spacer(80, 0));
-		append(msg);
+		append(msg);	
+
+		timer = new Timer();
+		timer.schedule(new TimerTask () {
+			public void run () {
+				updateStatus();
+			}
+		}, REFRESH_INTERVAL, REFRESH_INTERVAL);
 	}
+	
+	public void updateStatus () {
+		counter += REFRESH_INTERVAL;
 
-	public String getServerResponse()  {
-		Enumeration messages = JavaRosaServiceProvider.instance().getTransportManager().getMessages();
- 		        String receipt ="";
- 		    //while (messages.hasMoreElements())
- 		    //{
- 		    TransportMessage response = (TransportMessage) messages.nextElement();
- 		     receipt = new String(response.getReplyloadData());
- 		    //}
- 		    return receipt;
- 		    }
-
-	public void updateStatus (int status) {
+		int status = JavaRosaServiceProvider.instance().getTransportManager().getModelDeliveryStatus(modelID, false);
+		if (status != TransportMessage.STATUS_NEW)
+			timer.cancel();
 
 		String message;
-		String s = getServerResponse();
-
 		switch (status) {
-		case TransportMessage.STATUS_NEW:       message = (counter < TIMEOUT ? "Sending..."
-				: "Sending is taking a long time; you may check on the status and/or resend later in 'View Saved'"); break;
-		case TransportMessage.STATUS_DELIVERED: message = ("Form has been submitted successfully! Your reference is:"+s); break;
-		case TransportMessage.STATUS_FAILED:    message = "Submission failed! Please try to submit the form again later in 'View Saved'."; break;
-		default:                                message = "Unknown sending error; form not sent!"; break;
+		case TransportMessage.STATUS_NEW:       message = (counter < TIMEOUT ? MSG_SENDING : MSG_TOO_LONG); break;
+		case TransportMessage.STATUS_DELIVERED: message = MSG_SUCCESS + getServerResponse(); break;
+		case TransportMessage.STATUS_FAILED:    message = MSG_FAILED; break;
+		default:                                message = MSG_UNKNOWN_ERROR; break;
 		}
 
 		msg.setText(message);
 	}
+	
+	public void destroy () {
+		timer.cancel();
+	}
+
+	public String getServerResponse()  {
+		Enumeration messages = JavaRosaServiceProvider.instance().getTransportManager().getMessages();
+		String receipt ="";
+		//while (messages.hasMoreElements())
+		//{
+		TransportMessage response = (TransportMessage) messages.nextElement();
+		receipt = new String(response.getReplyloadData()); //this does not seem terribly robust
+		//}
+		return receipt;
+	}
 }
+
+
+
