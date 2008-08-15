ec8b1f0c3cfa5d622df701cfc2e0433b709428a4
mark <mark@localhost>
2008-08-15 08:55:30 +0000

    [r980] [branches/dev] Changed the design pattern and architecture. Submitting chages

---
+++ org.javarosa.patientselect/src/org/javarosa/patientselect/activity/IActivity.java
@@ -0,0 +1,46 @@
+package org.javarosa.patientselect.activity;
+
+import org.javarosa.core.Context;
+
+public interface IActivity {
+	
+	/**
+	 * Starts the module with the current context.
+	 * 
+	 * @param context The context to run the current module in
+	 */
+	public void start(Context context);
+	
+	/**
+	 * Updates the context for this module
+	 * 
+	 * @param globalContext The context object representing the global state 
+	 * of the app
+	 */
+	public void contextChanged(Context globalContext);
+	
+	/**
+	 * Halts any running execution and returns a context for the current state of
+	 * the module.
+	 * 
+	 * @return The current context of the module 
+	 */
+	public void halt();
+	
+	/**
+	 * Resumes execution of the module after it is halted, generally taking into
+	 * account the state of the context 
+	 */
+	public void resume(Context globalContext);
+	
+	/**
+	 * Cleans up anything that needs to be manually destroyed in a module
+	 */
+	public void destroy();
+	
+	/**
+	 * @return The context under which the activity is operating
+	 */
+	public Context getActivityContext();
+
+}
