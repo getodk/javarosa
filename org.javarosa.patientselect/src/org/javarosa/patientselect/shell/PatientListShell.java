fd2805b4f9a5df0a940b8afebfc99347e19a3b22
mark <mark@localhost>
2008-08-15 08:57:24 +0000

    [r984] [branches/dev] Changed the design pattern and architecture. Submitting chages

---
+++ org.javarosa.patientselect/src/org/javarosa/patientselect/shell/PatientListShell.java
@@ -0,0 +1,73 @@
+package org.javarosa.patientselect.shell;
+
+import java.util.Hashtable;
+import org.javarosa.core.*;
+import org.javarosa.core.api.*;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.lcdui.Displayable;
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.patientselect.activity.PatientListActivity;
+
+public class PatientListShell {
+	
+	 	private MIDlet patientListMidlet;
+		// private ListActivity patientListActivity = null;
+		 private PatientListActivity patientListActivity = null;
+		 private Context patientListContext;
+		 private IActivity currentActivity = null;
+		
+		 String shellName = "PatientListShell";
+		 
+		public PatientListShell()
+		{		
+			patientListContext = new Context();
+			
+			patientListActivity = new PatientListActivity(shellName);
+			
+
+		}
+		
+		public void exitShell(){
+			
+			patientListMidlet.notifyDestroyed();
+			System.out.println("exitCommand");
+		}
+		
+		public void run(){
+			
+			System.out.println("Running midlet in Init<>");
+			patientListActivity.start(patientListContext);
+		}
+		
+	    public void init(){
+	    	
+	    	System.out.println("Init Error!");
+		}
+		
+		public void setMIDlet(MIDlet midlet){
+			
+			this.patientListMidlet = midlet;
+		}
+		
+		public void returnFromActivity(IActivity i, String s, Hashtable h){
+			
+			i.resume(patientListContext);
+		}
+		
+		public boolean setDisplay(IActivity callingActivity, Displayable display)
+		{
+			if(callingActivity == currentActivity) {
+				
+				JavaRosaServiceProvider.instance().getDisplay().setCurrent(display);
+				
+				return true;
+			}
+			else 
+			{
+				System.out.println("Activity: " + callingActivity + " attempted, but failed, to set the display");
+				
+				return false;
+			}
+		}
+
+}
