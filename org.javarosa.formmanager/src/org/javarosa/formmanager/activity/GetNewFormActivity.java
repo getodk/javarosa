a0f32a751afdfd84efd94687ee9564646a878e3a
bderenzi <bderenzi@localhost>
2008-08-15 14:58:20 +0000

    [r988] [branches/dev] Created an empty file for the GetNewFormActivity

---
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/activity/GetNewFormActivity.java
@@ -0,0 +1,69 @@
+/**
+ * 
+ */
+package org.javarosa.formmanager.activity;
+
+import org.javarosa.core.Context;
+import org.javarosa.core.api.IActivity;
+
+/**
+ * @author Brian DeRenzi
+ *
+ */
+public class GetNewFormActivity implements IActivity {
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#contextChanged(org.javarosa.core.Context)
+	 */
+	@Override
+	public void contextChanged(Context globalContext) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#destroy()
+	 */
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#getActivityContext()
+	 */
+	@Override
+	public Context getActivityContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#halt()
+	 */
+	@Override
+	public void halt() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#resume(org.javarosa.core.Context)
+	 */
+	@Override
+	public void resume(Context globalContext) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#start(org.javarosa.core.Context)
+	 */
+	@Override
+	public void start(Context context) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
