0ca4011369642ff112639d2677fbc527cdab18bf
kieran <kieran@localhost>
2008-08-15 11:44:46 +0000

    [r986] [branches/dev] adding first classes of clforms - not yet working.

---
+++ org.javarosa.clforms/src/org/javarosa/demo/midlet/JavaRosaDemoMidlet.java
@@ -0,0 +1,47 @@
+package org.javarosa.demo.midlet;
+
+import java.util.Stack;
+import java.util.Vector;
+
+import javax.microedition.lcdui.Display;
+import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.api.IShell;
+import org.javarosa.demo.shell.JavaRosaDemoShell;
+
+/**
+ * This is the starting point for the JavarosaDemo application
+ * @author Brian DeRenzi
+ *
+ */
+public class JavaRosaDemoMidlet extends MIDlet {
+	IShell shell = null;
+	
+	
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void pauseApp() {
+		// TODO Auto-generated method stub
+
+	}
+
+	protected void startApp() throws MIDletStateChangeException {
+		/*
+		 * Duplicate this class and change the following line to 
+		 * create a custom midlet to launch from 
+		 */
+		shell = new JavaRosaDemoShell();
+
+		// Do NOT edit below
+		JavaRosaServiceProvider.instance().initialize();
+		JavaRosaServiceProvider.instance().setDisplay(Display.getDisplay(this));
+		shell.run();
+		((JavaRosaDemoShell)shell).setMIDlet(this);
+	}
+
+}
