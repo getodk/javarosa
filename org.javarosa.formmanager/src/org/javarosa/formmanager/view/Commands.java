bb203bc7c6b4059eb68335c650c0ce2639143c17
bderenzi <bderenzi@users.noreply.github.com>
2008-08-18 17:09:21 +0000

    [r1000] [branches/dev] Added commands for GetNewForm

--- org.javarosa.formmanager/src/org/javarosa/formmanager/view/Commands.java
+++ org.javarosa.formmanager/src/org/javarosa/formmanager/view/Commands.java
@@ -12,5 +12,6 @@ public class Commands {
 	public static final String CMD_SELECT_XFORM = "select_xform";
 	public static final String CMD_EXIT = "exit";
 	public static final String CMD_VIEW_DATA = "view_data";
+	public static final String CMD_GET_NEW_FORM = "get_new_form";
 	public static final String CMD_SETTINGS = "view_settings";
 }
