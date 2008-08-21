a29100556ef2ede7736bf71dac953d3c9a712ee1
alfred <alfred@localhost>
2008-08-21 08:32:28 +0000

    [r1043] [branches/dev] Added widget for numeric questions, implemented methods for select, time widgets. TODO: implement setHint for all widgets.

--- org.javarosa.clforms/src/org/javarosa/formmanager/view/clforms/widgets/TimeQuestionWidget.java
+++ org.javarosa.clforms/src/org/javarosa/formmanager/view/clforms/widgets/TimeQuestionWidget.java
@@ -1,13 +1,49 @@
 package org.javarosa.formmanager.view.clforms.widgets;
 
+import java.util.Date;
+
+import javax.microedition.lcdui.DateField;
+import javax.microedition.lcdui.Item;
+
+import org.javarosa.core.model.QuestionDef;
 import org.javarosa.formmanager.view.clforms.SingleQuestionScreen;
 
 public class TimeQuestionWidget extends SingleQuestionScreen
 {
+	protected Item entryWidget;
+	protected QuestionDef qdef;
 
 	public TimeQuestionWidget(String formTitle) {
 		super(formTitle);
-		// TODO Auto-generated constructor stub
+		reset();
 	}
 	
+	public Item initWidget(QuestionDef question)
+	{
+		qdef = question;
+		entryWidget = getEntryWidget(qdef);
+		return entryWidget;
+	}
+	
+	protected Item getEntryWidget(QuestionDef prompt)
+	{
+		DateField timePicker = new DateField(prompt.getShortText(), DateField.TIME);
+		//set question 
+		timePicker.setLabel(prompt.getLongText());
+		//check if the field has already been filled in by default value- if so display value
+		if (prompt.getDefaultValue() != null){
+			timePicker.setDate((Date)prompt.getDefaultValue());
+		}
+		else
+		{
+			//need a way to retrieve the date entered for setDate()
+		}
+		return timePicker;
+	}
+	//Utility methods
+	public void reset () 
+	{
+		qdef = null;
+		entryWidget = null;
+	}
 }
