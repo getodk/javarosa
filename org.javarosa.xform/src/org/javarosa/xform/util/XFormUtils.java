d6c52f569bff5c3355f308c93565f981996f0f8c
kieran <kieran@localhost>
2008-09-01 15:20:55 +0000

    [r1085] [branches/dev] add the getformFromSerializedResouce method

--- org.javarosa.xform/src/org/javarosa/xform/util/XFormUtils.java
+++ org.javarosa.xform/src/org/javarosa/xform/util/XFormUtils.java
@@ -1,5 +1,6 @@
 package org.javarosa.xform.util;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -7,11 +8,15 @@ import java.io.InputStreamReader;
 import javax.microedition.io.file.FileConnection;
 
 import org.javarosa.core.model.FormDef;
+import org.javarosa.core.model.QuestionDef;
+import org.javarosa.core.model.instance.DataModelTree;
+import org.javarosa.core.model.utils.PrototypeFactory;
+import org.javarosa.core.util.UnavailableExternalizerException;
 import org.javarosa.xform.parse.XFormParser;
 
 /**
  * Static Utility methods pertaining to XForms.
- *  
+ *
  * @author Clayton Sims
  *
  */
@@ -20,7 +25,7 @@ public class XFormUtils {
 		InputStream is = System.class.getResourceAsStream(resource);
 		return getFormFromInputStream(is);
 	}
-	
+
 	// 18 Aug 2008 added by Brian DeRenzi
 	// #if app.usefileconnections
 	public static FormDef getFormFromFile(FileConnection fc) {
@@ -34,7 +39,7 @@ public class XFormUtils {
 		return XFormUtils.getFormFromInputStream(fis);
 	}
 	// #endif
-	
+
 	private static FormDef getFormFromInputStream(InputStream is) {
 		FormDef returnForm = null;
 		InputStreamReader isr = new InputStreamReader(is);
@@ -49,5 +54,41 @@ public class XFormUtils {
 			e.printStackTrace();
 		}
 		return returnForm;
+	}
+
+	public static FormDef getFormFromSerializedResource(String resource) {
+		FormDef returnForm = new FormDef();
+		QuestionDef handy = new QuestionDef();
+		PrototypeFactory modelFactory = new PrototypeFactory();
+		modelFactory.addNewPrototype(new DataModelTree().getClass().getName(), new DataModelTree().getClass());
+		returnForm.setModelFactory(modelFactory);
+		InputStream is = System.class.getResourceAsStream(resource);
+		try {
+			if(is != null) {
+				DataInputStream dis = new DataInputStream(is);
+				returnForm.readExternal(dis);
+				dis.close();
+				is.close();
+			}else{
+				//#if debug.output==verbose
+				System.out.println("ResourceStream NULL");
+				//#endif
+			}
+		}
+		catch(IOException e) {
+			System.err.println("IO Exception while closing stream.");
+			e.printStackTrace();
+		} catch (InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnavailableExternalizerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return returnForm;
+
 	}
 }
