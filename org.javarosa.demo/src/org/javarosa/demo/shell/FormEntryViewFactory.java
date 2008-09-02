fb164c70d4c03ef1a132b7ddc08cf6b969049086
kieran <kieran@localhost>
2008-09-02 11:17:09 +0000

    [r1091] [branches/dev] Updated the FormEntryActivity to use the appropriate property to determine which FormEntryView to use. CL_forms added to the demo shell's FormEntryViewFactory.

--- org.javarosa.demo/src/org/javarosa/demo/shell/FormEntryViewFactory.java
+++ org.javarosa.demo/src/org/javarosa/demo/shell/FormEntryViewFactory.java
@@ -2,15 +2,20 @@ package org.javarosa.demo.shell;
 
 import org.javarosa.formmanager.controller.FormEntryController;
 import org.javarosa.formmanager.model.FormEntryModel;
+import org.javarosa.formmanager.properties.FormManagerProperties;
 import org.javarosa.formmanager.view.IFormEntryView;
 import org.javarosa.formmanager.view.IFormEntryViewFactory;
 import org.javarosa.formmanager.view.chatterbox.Chatterbox;
+import org.javarosa.formmanager.view.clforms.FormViewScreen;
 
 public class FormEntryViewFactory implements IFormEntryViewFactory {
 	public IFormEntryView getFormEntryView (String viewType, FormEntryModel model, FormEntryController controller) {
-		if ("chatterbox".equals(viewType)) {
-			return new Chatterbox("Chatterbox", model, controller);
-		} else {
+		if (FormManagerProperties.VIEW_CHATTERBOX.equals(viewType)) {
+			return new Chatterbox("Chatterbox STRING", model, controller);
+		} else if (FormManagerProperties.VIEW_CLFORMS.equals(viewType)) {
+			return new FormViewScreen("CL_FORMS STRING", model, controller);
+		}
+		else {
 			return null;
 		}
 	}
