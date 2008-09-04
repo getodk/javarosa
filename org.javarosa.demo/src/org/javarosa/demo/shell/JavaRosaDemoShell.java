fbee08b2f5fed3a3869f562c6a8f36bf70adfb4d
kieran <kieran@localhost>
2008-09-04 13:51:01 +0000

    [r1117] [branches/dev] fix bug for USER type check

--- org.javarosa.demo/src/org/javarosa/demo/shell/JavaRosaDemoShell.java
+++ org.javarosa.demo/src/org/javarosa/demo/shell/JavaRosaDemoShell.java
@@ -154,9 +154,10 @@ public class JavaRosaDemoShell implements IShell {
 			Object returnVal = returnVals.get(LoginActivity.COMMAND_KEY);
 			if (returnVal == "USER_VALIDATED") {
 				User user = (User)returnVals.get(LoginActivity.USER);
-				if (user != null)
+				if (user != null){
 					context.setCurrentUser(user.getUsername());
-
+					context.setElement("USER", user);
+				}
 				launchActivity(new FormListActivity(this, "Forms List"), context);
 			} else if (returnVal == "USER_CANCELLED") {
 				exitShell();
@@ -312,10 +313,10 @@ public class JavaRosaDemoShell implements IShell {
 
 		initProperty("DeviceID", genGUID(25));
 		initProperty(FormManagerProperties.VIEW_TYPE_PROPERTY, FormManagerProperties.VIEW_CHATTERBOX);
-		initProperty(HttpTransportProperties.POST_URL_LIST_PROPERTY, "http://update.cell-life.org/save_dump_org.php");
-		initProperty(HttpTransportProperties.POST_URL_PROPERTY, "http://update.cell-life.org/save_dump_org.php");
-//		initProperty(HttpTransportProperties.POST_URL_LIST_PROPERTY, "http://openrosa.org/testsubmit.html");
-//		initProperty(HttpTransportProperties.POST_URL_PROPERTY, "http://openrosa.org/testsubmit.html");
+		initProperty(HttpTransportProperties.POST_URL_LIST_PROPERTY, "http://dev.cell-life.org/javarosa/web/limesurvey/admin/post2lime.php");
+		initProperty(HttpTransportProperties.POST_URL_PROPERTY, "http://dev.cell-life.org/javarosa/web/limesurvey/admin/post2lime.php");
+//		initProperty(HttpTransportProperties.POST_URL_LIST_PROPERTY, "http://update.cell-life.org/save_dump_org.php");
+//		initProperty(HttpTransportProperties.POST_URL_PROPERTY, "http://update.cell-life.org/save_dump_org.php");
 	}
 
 	//TODO: Put this in a utility method
