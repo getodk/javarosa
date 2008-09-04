011bfc928f611bb0e295ba5162b24d4e7dd8596b
mark <mark@localhost>
2008-09-04 04:33:27 +0000

    [r1109] [branches/dev] [no message]

---
+++ org.javarosa.core.model/src/org/javarosa/core/model/data/StudyData.java
@@ -0,0 +1,123 @@
+package org.javarosa.core.model.data;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Vector;
+
+import org.javarosa.core.model.Constants;
+import org.javarosa.core.model.StudyDef;
+import org.javarosa.core.model.storage.FormDefMetaData;
+import org.javarosa.core.model.utils.ExternalizableHelper;
+import org.javarosa.core.util.Externalizable;
+import org.javarosa.core.util.UnavailableExternalizerException;
+
+/**
+ * This calls encapsulates data collected in all forms of a particular study.
+ * 
+ * @author Mark Gerard
+ *
+ */
+public class StudyData implements Externalizable{
+
+	private byte id = Constants.NULL_ID; //this is just for storage;
+	private StudyDef def;
+	private Vector forms;
+	
+	/** Creates a new study data object. */
+	public StudyData(){
+		super();
+	}
+	
+	/**
+	 * Creates a new study data object form these parameters.
+	 * 
+	 * @param id - the id of the study definition represented by this data.
+	 */
+	public StudyData(byte id) {
+		this();
+		setId(id);
+	}
+	
+	/**
+	 * Creates a new study data object form these parameters.
+	 * 
+	 * @param def - reference to the study definition represented by this data.
+	 */
+	public StudyData(StudyDef def) {
+		this();
+		setDef(def);
+		setId(def.getId());
+	}
+
+	public StudyDef getDef() {
+		return def;
+	}
+
+	public void setDef(StudyDef def) {
+		this.def = def;
+	}
+
+	public byte getId() {
+		return id;
+	}
+
+	public void setId(byte id) {
+		this.id = id;
+	}
+	
+	public Vector getForms() {
+		return forms;
+	}
+
+	public void setForms(Vector forms) {
+		this.forms = forms;
+	}
+	
+	public void addForm(FormDefMetaData formData){
+		if(forms == null)
+			forms = new Vector();
+		forms.addElement(formData);
+	}
+	
+	public void addForms(Vector formList){
+		if(formList != null){
+			if(forms == null)
+				forms = formList;
+			else{
+				for(byte i=0; i<formList.size(); i++ )
+					forms.addElement(formList.elementAt(i));
+			}
+		}
+	}
+
+	/** 
+	 * Reads the study data object from the stream.
+	 * 
+	 * @param dis - the stream to read from.
+	 * @throws IOException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public void readExternal(DataInputStream in) throws IOException,
+			InstantiationException, IllegalAccessException,
+			UnavailableExternalizerException {
+		if(!ExternalizableHelper.isEOF(in)){
+			setId(in.readByte());
+			setForms(ExternalizableHelper.readBig(in,new FormDefMetaData().getClass()));
+		}
+		
+	}
+
+	/** 
+	 * Writes the study collection object to the stream.
+	 * 
+	 * @param dos - the stream to write to.
+	 * @throws IOException
+	 */
+	public void writeExternal(DataOutputStream out) throws IOException {
+		out.writeByte(getId());
+		ExternalizableHelper.writeBig(getForms(), out);
+		
+	}
+}
---
+++ org.javarosa.core.model/src/org/javarosa/core/model/data/StudyDataList.java
@@ -0,0 +1,85 @@
+package org.javarosa.core.model.data;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.util.Vector;
+
+import org.javarosa.core.model.utils.ExternalizableHelper;
+import org.javarosa.core.util.Externalizable;
+import org.javarosa.core.util.UnavailableExternalizerException;
+
+/**
+ * This class holds a collection of study data.
+ * 
+ * @author Mark Gerard
+ *
+ */
+public class StudyDataList implements Externalizable {
+
+	/** Collection of studies. */
+	private Vector studies;
+	
+	/** Constructs a new study data collection. */
+	public StudyDataList(){
+		super();
+	}
+	
+	public StudyDataList(Vector studies){
+		this();
+		setStudies(studies);
+	}
+	
+	public Vector getStudies() {
+		return studies;
+	}
+
+	public void setStudies(Vector studies) {
+		this.studies = studies;
+	}
+	
+	public void addStudy(StudyData studyData){
+		if(studies == null)
+			studies = new Vector();
+		studies.addElement(studyData);
+	}
+	
+	public void addStudies(Vector studyList){
+		if(studyList != null){
+			if(studies == null)
+				studies = studyList;
+			else{
+				for(byte i=0; i<studyList.size(); i++ )
+					studies.addElement(studyList.elementAt(i));
+			}
+		}
+	}
+	
+	/** 
+	 * Reads the study collection object from the stream.
+	 * 
+	 * @param dis - the stream to read from.
+	 * @throws IOException
+	 * @throws InstantiationException
+	 * @throws IllegalAccessException
+	 */
+	public void readExternal(DataInputStream in) throws IOException,
+			InstantiationException, IllegalAccessException,
+			UnavailableExternalizerException {
+		if(!ExternalizableHelper.isEOF(in))
+			setStudies(ExternalizableHelper.readBig(in,new StudyData().getClass()));
+		
+	}
+
+	/** 
+	 * Writes the study collection object to the stream.
+	 * 
+	 * @param dos - the stream to write to.
+	 * @throws IOException
+	 */
+	public void writeExternal(DataOutputStream out) throws IOException {
+		ExternalizableHelper.writeBig(getStudies(), out);
+		
+	}
+
+}
