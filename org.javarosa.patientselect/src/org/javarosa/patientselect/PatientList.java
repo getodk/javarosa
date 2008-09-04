33de27cef08d59130f428a52217555ffd4d948f1
mark <mark@localhost>
2008-09-04 04:34:08 +0000

    [r1110] [branches/dev] [no message]

--- org.javarosa.patientselect/src/org/javarosa/patientselect/PatientList.java
+++ org.javarosa.patientselect/src/org/javarosa/patientselect/PatientList.java
@@ -1,13 +1,29 @@
 package org.javarosa.patientselect;
 
+import java.io.IOException;
+
 import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Image;
 import javax.microedition.midlet.MIDlet;
+import javax.microedition.midlet.MIDletStateChangeException;
+
 import org.javarosa.core.JavaRosaServiceProvider;
 import org.javarosa.patientselect.shell.PatientListShell;
 
-public class PatientList extends MIDlet  {
+import de.enough.polish.ui.Screen;
+import de.enough.polish.ui.splash.ApplicationInitializer;
+import de.enough.polish.ui.splash.InitializerSplashScreen;
+
+public class PatientList extends MIDlet implements ApplicationInitializer
+
+{
 	
 	private PatientListShell patientList = null;
+	
+	private Screen mainScreen;
+	private Display display;
+	
 		
 	public PatientList(){
 		
@@ -18,7 +34,29 @@ public class PatientList extends MIDlet  {
 		System.out.println("Midlet created");
 	}
 	
-	public void startApp(){
+	public void startApp() throws MIDletStateChangeException{
+		
+		this.display = Display.getDisplay(this);
+		
+		if(this.mainScreen != null){
+			this.display.setCurrent(this.mainScreen);
+		}
+		else{
+			try{
+				Image image = Image.createImage("/splash.gif");
+				int backgroundcolor = 0xFFFFFF;
+				String loadMessage = "Loading Patient Activities...";
+				int loadMessageColor = 0xFF0000;
+				
+				InitializerSplashScreen splash = new InitializerSplashScreen(
+						this.display, image, backgroundcolor, loadMessage, loadMessageColor, this);
+				
+				this.display.setCurrent(splash);
+			}
+			catch(IOException excep){
+				throw new MIDletStateChangeException("Unable to load splash creen");
+			}
+		}
 		
 		JavaRosaServiceProvider.instance().initialize();
 		JavaRosaServiceProvider.instance().setDisplay(Display.getDisplay(this));
@@ -36,6 +74,10 @@ public class PatientList extends MIDlet  {
 	public void destroyApp(boolean conditional)
 	{
 		
+	}
+
+	public Displayable initApp() {
+		return this.mainScreen;
 	}
 }
 
