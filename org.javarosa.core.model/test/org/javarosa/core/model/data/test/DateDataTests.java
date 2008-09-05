cae843e19ab0e82c66c388037b118163a3077783
csims <csims@localhost>
2008-09-05 19:55:11 +0000

    [r1131] [branches/dev] Added Date Data object tests

---
+++ org.javarosa.core.model/test/org/javarosa/core/model/data/test/DateDataTests.java
@@ -0,0 +1,98 @@
+package org.javarosa.core.model.data.test;
+
+import j2meunit.framework.Test;
+import j2meunit.framework.TestCase;
+import j2meunit.framework.TestMethod;
+import j2meunit.framework.TestSuite;
+
+import java.util.Date;
+
+import org.javarosa.core.model.data.DateData;
+
+public class DateDataTests extends TestCase{
+	
+	Date today;
+	Date notToday;
+	
+	private static int NUM_TESTS = 3;
+	
+	/* (non-Javadoc)
+	 * @see j2meunit.framework.TestCase#setUp()
+	 */
+	protected void setUp() throws Exception {
+		super.setUp();
+		
+		today = new Date();
+		notToday = new Date(today.getTime() - today.getTime()/2);
+	}
+	
+	public DateDataTests(String name, TestMethod rTestMethod) {
+		super(name, rTestMethod);
+	}
+
+	public DateDataTests(String name) {
+		super(name);
+	}
+
+	public DateDataTests() {
+		super();
+	}	
+
+	public Test suite() {
+		TestSuite aSuite = new TestSuite();
+
+		for (int i = 1; i <= NUM_TESTS; i++) {
+			final int testID = i;
+
+			aSuite.addTest(new DateDataTests("DateData Test " + i, new TestMethod() {
+				public void run (TestCase tc) {
+					((DateDataTests)tc).testMaster(testID);
+				}
+			}));
+		}
+
+		return aSuite;
+	}
+	public void testMaster (int testID) {
+		//System.out.println("running " + testID);
+		
+		switch (testID) {
+		case 1: testGetData(); break;
+		case 2: testSetData(); break;
+		case 3: testDisplay(); break;
+		}
+	}
+	
+	public void testGetData() {
+		DateData data = new DateData(today);
+		assertEquals("DateData's getValue returned an incorrect date", data.getValue(), today);
+		Date temp = new Date(today.getTime());
+		today.setTime(1234);
+		assertEquals("DateData's getValue was mutated incorrectly", data.getValue(), temp);
+		
+		Date rep = (Date)data.getValue();
+		rep.setTime(rep.getTime() - 1000);
+		
+		assertEquals("DateData's getValue was mutated incorrectly", data.getValue(), temp);
+	}
+	public void testSetData() {
+		DateData data = new DateData(notToday);
+		data.setValue(today);
+		
+		assertTrue("DateData did not set value properly. Maintained old value.", !(data.getValue().equals(notToday)));
+		assertEquals("DateData did not properly set value ", data.getValue(), today);
+		
+		data.setValue(notToday);
+		assertTrue("DateData did not set value properly. Maintained old value.", !(data.getValue().equals(today)));
+		assertEquals("DateData did not properly reset value ", data.getValue(), notToday);
+		
+		Date temp = new Date(notToday.getTime());
+		notToday.setTime(notToday.getTime() - 1324);
+		
+		assertEquals("DateData's value was mutated incorrectly", data.getValue(), temp);
+	}
+	
+	public void testDisplay() {
+		// We don't actually want this, because the Date's getDisplayText code should be moved to a library
+	}
+}
