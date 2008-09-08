a7da379d415c043ca00a619f6b9e59df4c9cf530
Drew Roos <mrgriscom@users.noreply.github.com>
2008-09-08 16:43:10 +0000

    [r1162] [branches/dev] commenting/stubbing out non-building projects/classes

--- org.javarosa.patient/src/org/javarosa/patient/activity/PatientSelectActivity.java
+++ org.javarosa.patient/src/org/javarosa/patient/activity/PatientSelectActivity.java
@@ -1,9 +1,9 @@
 package org.javarosa.patient.activity;
 
 import org.javarosa.core.api.IShell;
-import org.javarosa.entitymgr.activity.EntitySelectActivity;
-import org.javarosa.patientmgr.Constants;
-import org.javarosa.patientmgr.model.Patient;
+// import org.javarosa.entitymgr.activity.EntitySelectActivity;
+// import org.javarosa.patientmgr.Constants;
+// import org.javarosa.patientmgr.model.Patient;
 
 
 /**
@@ -12,18 +12,23 @@ import org.javarosa.patientmgr.model.Patient;
  * @author daniel
  *
  */
-public class PatientSelectActivity extends EntitySelectActivity{
-	
-	/** Key for the selected patient. */
-	public static final String KEY_PATIENT = KEY_ENTITY;
-	
-	/**
-	 * Constructs a new patient select activity.
-	 * 
-	 * @param appTitle the title of the midlet.
-	 */
-	public PatientSelectActivity(IShell shell,String appTitle){
-		super(new Patient().getClass(),appTitle,Constants.LABEL_PATIENT_ENTITY,Constants.LABEL_PATIENT_IDENTIFIER, Constants.LABEL_PATIENT_NAME,shell);
-		//displayDetailsScreen(false);
-	}
+
+//public class PatientSelectActivity extends EntitySelectActivity {
+//	
+//	/** Key for the selected patient. */
+//	public static final String KEY_PATIENT = KEY_ENTITY;
+//	
+//	/**
+//	 * Constructs a new patient select activity.
+//	 * 
+//	 * @param appTitle the title of the midlet.
+//	 */
+//	public PatientSelectActivity(IShell shell,String appTitle){
+//		super(new Patient().getClass(),appTitle,Constants.LABEL_PATIENT_ENTITY,Constants.LABEL_PATIENT_IDENTIFIER, Constants.LABEL_PATIENT_NAME,shell);
+//		//displayDetailsScreen(false);
+//	}
+//}
+
+public class PatientSelectActivity {
+	//stub class until entitymgr compiles
 }
