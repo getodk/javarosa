d8cb63bb49a7cf0b24d7b9711494a3c71730e130
csims <csims@localhost>
2008-09-16 17:06:52 +0000

    [r1214] [branches/dev] Fixed broken build.

--- org.javarosa.demo/src/org/javarosa/demo/shell/FormEntryViewFactory.java
+++ org.javarosa.demo/src/org/javarosa/demo/shell/FormEntryViewFactory.java
@@ -6,14 +6,14 @@ import org.javarosa.formmanager.properties.FormManagerProperties;
 import org.javarosa.formmanager.view.IFormEntryView;
 import org.javarosa.formmanager.view.IFormEntryViewFactory;
 import org.javarosa.formmanager.view.chatterbox.Chatterbox;
-import org.javarosa.formmanager.view.clforms.FormViewScreen;
+import org.javarosa.formmanager.view.clforms.FormViewManager;
 
 public class FormEntryViewFactory implements IFormEntryViewFactory {
 	public IFormEntryView getFormEntryView (String viewType, FormEntryModel model, FormEntryController controller) {
 		if (FormManagerProperties.VIEW_CHATTERBOX.equals(viewType)) {
 			return new Chatterbox("Chatterbox STRING", model, controller);
 		} else if (FormManagerProperties.VIEW_CLFORMS.equals(viewType)) {
-			return new FormViewScreen("CL_FORMS STRING", model, controller);
+			return new FormViewManager("CL_FORMS STRING", model, controller);
 		}
 		else {
 			return null;
