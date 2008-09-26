748152bdca9c7159f52e7e8f3783fa45a9421884
csims <csims@localhost>
2008-09-26 20:29:40 +0000

    [r1283] [branches/dev] Added appropriate warnings to the Class declarations in this project.

--- org.javarosa.patient.entry/src/org/javarosa/patient/entry/activity/PatientEntryActivity.java
+++ org.javarosa.patient.entry/src/org/javarosa/patient/entry/activity/PatientEntryActivity.java
@@ -23,6 +23,24 @@ import org.javarosa.patient.model.data.NumericListData;
 import org.javarosa.patient.storage.PatientRMSUtility;
 import org.javarosa.patient.util.DateValueTuple;
 
+/**
+ * NOTICE:
+ * 
+ * 
+ * This class, and the entire org.javarosa.patient.entry Module all only
+ * exist because <group> and <repeat> are not yet available in our XForms 
+ * subset. 
+ * 
+ * THIS MODULE IS FULL OF CODE THAT SHOULD NOT BE EMULATED.
+ * 
+ * Please do not take the code in the org.javarosa.patient.entry project
+ * as an example of a proper use of the JavaRosa code base. It will be removed
+ * as soon as it is possible to represent the logic in the PatientEntryForm in
+ * our XForms subset.
+ * 
+ * @author Clayton Sims
+ *
+ */
 public class PatientEntryActivity implements IActivity {
 
 	public static final String PATIENT_ENTRY_FORM_KEY = "Patient Entry Form";
