f3b84859f4c41c6fa6fb32eb7df0236f7effc8ec
csims <ctsims@users.noreply.github.com>
2008-10-16 21:05:37 +0000

    [r1377] [branches/dev] Added property rules for the reminders module

--- org.javarosa.reminders/src/org/javarosa/reminders/RemindersModule.java
+++ org.javarosa.reminders/src/org/javarosa/reminders/RemindersModule.java
@@ -3,6 +3,8 @@ package org.javarosa.reminders;
 import org.javarosa.core.Context;
 import org.javarosa.core.JavaRosaServiceProvider;
 import org.javarosa.core.api.IModule;
+import org.javarosa.core.util.PropertyUtils;
+import org.javarosa.reminders.properties.ReminderPropertyRules;
 import org.javarosa.reminders.storage.ReminderRMSUtility;
 
 public class RemindersModule implements IModule {
@@ -10,6 +12,9 @@ public class RemindersModule implements IModule {
 	public void registerModule(Context context) {
 		ReminderRMSUtility reminderRms = new ReminderRMSUtility(ReminderRMSUtility.getUtilityName());
 		JavaRosaServiceProvider.instance().getStorageManager().getRMSStorageProvider().registerRMSUtility(reminderRms);
+		
+		JavaRosaServiceProvider.instance().getPropertyManager().addRules(new ReminderPropertyRules());
+		PropertyUtils.initializeProperty(ReminderPropertyRules.REMINDERS_ENABLED_PROPERTY, ReminderPropertyRules.REMINDERS_ENABLED);
 	}
 
 }
