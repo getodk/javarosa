5fe2be583e76c818585128c77dca2295bfff0d93
czue <czue@localhost>
2008-10-24 21:51:35 +0000

    [r1420] [branches/dev] Moved the new model classes for file pointers to the core project and added a widget for the ImageChooser (currently this does nothing).  Also added capability to the XForms parser to handle <upload> tags and select the Image type if the mediatype is set to "image/*"

---
+++ org.javarosa.core.model/src/org/javarosa/core/model/data/PointerAnswerData.java
@@ -0,0 +1,49 @@
+package org.javarosa.core.model.data;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
+
+/**
+ * Answer data representing a pointer object.  The pointer is a reference to some 
+ * other object that it knows how to get out of memory.
+ * 
+ * @author Cory Zue
+ *
+ */
+public class PointerAnswerData implements IAnswerData {
+
+	private IDataPointer data;
+	
+	public PointerAnswerData(IDataPointer data) {
+		this.data = data;
+	}
+		
+	public String getDisplayText() {
+		return data.getDisplayText();
+	}
+
+	public Object getValue() {
+		return data;
+	}
+
+	public void setValue(Object o) {
+		if(o == null) {
+			throw new NullPointerException("Attempt to set an IAnswerData class to null.");
+		}
+		data = ((IDataPointer)o);
+	}
+
+	public void readExternal(DataInputStream in, PrototypeFactory pf)
+			throws IOException, DeserializationException {
+		// TODO Auto-generated method stub
+	}
+
+	public void writeExternal(DataOutputStream out) throws IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
