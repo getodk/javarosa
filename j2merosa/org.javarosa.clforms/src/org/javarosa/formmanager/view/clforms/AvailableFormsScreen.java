e4139bb098707467e8a4dac273fd8417da038a68
alfred <alfred@localhost>
2008-11-19 14:13:01 +0000

    [r1478] [branches/dev] Added get-new-forms functionality. Works with CLForms project.

---
+++ j2merosa/org.javarosa.clforms/src/org/javarosa/formmanager/view/clforms/AvailableFormsScreen.java
@@ -0,0 +1,20 @@
+package org.javarosa.formmanager.view.clforms;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.List;
+
+import org.javarosa.formmanager.activity.DisplayFormsHttpActivity;
+
+public class AvailableFormsScreen extends List{
+	private DisplayFormsHttpActivity parentActivity;
+	
+	public final Command CMD_CANCEL = new Command("Cancel",Command.BACK, 1);
+
+	public AvailableFormsScreen(String label,String[] elements,DisplayFormsHttpActivity parentActivity) {
+		super(label, List.IMPLICIT,elements,null);
+		addCommand(CMD_CANCEL);
+		this.parentActivity = parentActivity;
+		setCommandListener(parentActivity);
+	}
+
+}
---
+++ j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/activity/DisplayFormsHttpActivity.java
@@ -0,0 +1,176 @@
+package org.javarosa.formmanager.activity;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+
+import org.javarosa.core.Context;
+import org.javarosa.core.api.Constants;
+import org.javarosa.core.api.IActivity;
+import org.javarosa.core.api.IShell;
+import org.javarosa.core.api.IView;
+import org.javarosa.core.util.Observable;
+import org.javarosa.core.util.Observer;
+import org.javarosa.formmanager.view.clforms.AvailableFormsScreen;
+import org.kxml2.io.KXmlParser;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+
+import javax.microedition.lcdui.List;
+
+public class DisplayFormsHttpActivity implements IActivity,CommandListener,Observer{
+	private AvailableFormsScreen formList;
+	private ByteArrayInputStream bin;
+
+	private KXmlParser parser = new KXmlParser();
+	private Vector items;
+	private Hashtable formInfo;
+
+	public static final String SELECTED_FORM = "selected_form";
+	public static final String FORM_URL = "selected_form_url";
+
+	private Context context;
+
+	private IShell parent;
+
+
+	public DisplayFormsHttpActivity(IShell parent,Hashtable args) {
+		this.parent = parent;
+
+		init(args);
+	}
+
+	public void init(Hashtable args){
+
+		byte[] data = (byte[])args.get(GetFormListHttpActivity.RETURN_KEY);
+
+		try{
+			bin = new ByteArrayInputStream(data);
+
+			//setup parser
+			parser.setInput(new InputStreamReader(bin));
+
+			items = new Vector();
+			formInfo = new Hashtable();
+
+			processSurveyList(parser, formInfo,items);
+
+			String[] formlist = new String[ items.size() ];
+			items.copyInto( formlist );
+
+			formList = new AvailableFormsScreen("Available Forms",formlist,this);
+
+		}catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+		}
+
+	}
+
+public void processSurveyList(KXmlParser parser, Hashtable formInfo, Vector items) throws XmlPullParserException{
+		
+		try {
+			boolean inItem = false;
+			parser.nextTag();
+			parser.require(XmlPullParser.START_TAG, null, "forms");
+			while( parser.nextTag() != XmlPullParser.END_TAG ){
+				//parser file names
+				parser.require(XmlPullParser.START_TAG, null, null);
+				
+				String name = parser.getName();
+				String url = parser.getAttributeValue(null, "url");
+				String text = parser.nextText();
+System.out.println("<"+name+">"+text+" "+url);				
+				if(name.equals("form"))
+					{
+					inItem = true;
+					items.addElement(text);
+					formInfo.put(text, url);
+					}
+				else
+					inItem = false;
+
+				parser.require(XmlPullParser.END_TAG, null, "form");
+			}
+			parser.require(XmlPullParser.END_TAG, null, "forms");
+			
+			parser.next();
+			parser.require(XmlPullParser.END_DOCUMENT, null, null);
+
+		} catch (IOException e) {
+			// TODO: handle exception
+			System.out.println("XML parser error");
+			e.printStackTrace();
+
+		}
+	}
+	public void contextChanged(Context globalContext) {
+		context.mergeInContext(globalContext);
+
+	}
+
+	public void destroy() {
+		parent.exitShell();
+
+	}
+
+	public Context getActivityContext() {
+
+		return context;
+	}
+
+	public void halt() {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void resume(Context globalContext) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void start(Context context) {
+		this.context = context;
+		System.out.println("Before display form list");
+		parent.setDisplay(this, new IView() {public Object getScreenObject() {return formList;}});
+		System.out.println("After display form List");
+
+	}
+
+	public void commandAction(Command command, Displayable display) {
+		if(display == formList){
+			if (command == formList.CMD_CANCEL) {
+				parent.returnFromActivity(this, Constants.ACTIVITY_CANCEL, null);
+			}if(command == List.SELECT_COMMAND){
+
+				String formName = formList.getString(formList.getSelectedIndex());
+				String formurl = (String) formInfo.get(formName);
+				System.out.println("OTTIMO!!" + formName+" "+formurl);
+				Hashtable returnargs = new Hashtable();
+				returnargs.put(SELECTED_FORM, formName);
+				returnargs.put(FORM_URL, formurl);
+				parent.returnFromActivity(this, Constants.ACTIVITY_COMPLETE, returnargs);
+
+			}
+			//other commands	
+		}
+
+	}
+
+	public void update(Observable observable, Object arg) {
+		// TODO Auto-generated method stub
+
+	}
+	public void setShell(IShell shell) {
+		this.parent = shell;
+
+	}
+
+}
