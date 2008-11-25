79a03fde14ea4ced1f84aed3074aba86cdeaee06
czue <czue@localhost>
2008-11-25 20:40:19 +0000

    [r1487] [branches/dev] Adding camera associated commands

--- j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/Commands.java
+++ j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/Commands.java
@@ -16,4 +16,7 @@ public class Commands {
 	public static final String CMD_DELETE_FORM = "delete_form";
 	public static final String CMD_SETTINGS = "view_settings";
 	public static final String CMD_ADD_USER = "add_user";
+	public static final String CMD_CAPTURE_IMAGE = "capture_image";
+	public static final String CMD_CAMERA = "camera";
+	public static final String CMD_IMAGE_BROWSE = "browse_images";
 }
