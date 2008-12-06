0e3472df5d7c89a22aecad9f9aa54455bc431ebf
bderenzi <bderenzi@users.noreply.github.com>
2008-12-06 12:01:28 +0000

    [r1540] [branches/dev] added chatterbox support for decimal entry.

--- j2merosa/org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/NumericEntryWidget.java
+++ j2merosa/org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/NumericEntryWidget.java
@@ -3,20 +3,38 @@ package org.javarosa.formmanager.view.chatterbox.widget;
 import org.javarosa.core.model.QuestionDef;
 import org.javarosa.core.model.data.IAnswerData;
 import org.javarosa.core.model.data.IntegerData;
-import org.javarosa.core.model.data.StringData;
+import org.javarosa.core.model.data.DecimalData;
 
 import de.enough.polish.ui.Item;
 import de.enough.polish.ui.TextField;
 
-public class NumericEntryWidget extends TextEntryWidget {	
+public class NumericEntryWidget extends TextEntryWidget {
+	// BWD 6.Dec - don't need a whole new widget for floating point
+	private boolean isDecimal = false;
+	
+	public NumericEntryWidget() {
+		super();
+	}
+	
+	public NumericEntryWidget(boolean dec) {
+		super();
+		this.isDecimal = dec;
+	}
+	
 	protected Item getEntryWidget (QuestionDef question) {
 		TextField tf = (TextField)super.getEntryWidget(question);
-	    tf.setConstraints(TextField.NUMERIC);
+		if(this.isDecimal)
+			tf.setConstraints(TextField.DECIMAL);
+		else
+			tf.setConstraints(TextField.NUMERIC);
 		return tf;
 	}
 
 	protected void setWidgetValue (Object o) {
-		super.setWidgetValue(((Integer)o).toString());
+		if(this.isDecimal)
+			super.setWidgetValue(((Double)o).toString());
+		else
+			super.setWidgetValue(((Integer)o).toString());
 	}
 	
 	protected IAnswerData getWidgetValue () {
@@ -25,12 +43,19 @@ public class NumericEntryWidget extends TextEntryWidget {
 		if (s == null || s.equals(""))
 			return null;
 		
+		double d = -999999999;
 		int i = -99999;
 		try {
-			i = Integer.parseInt(s);
+			if(this.isDecimal)
+				d = Double.parseDouble(s);
+			else
+				i = Integer.parseInt(s);
 		} catch (NumberFormatException nfe) {
 			System.err.println("Non-numeric data in numeric entry field!");
 		}
-		return new IntegerData(i);
+		if(this.isDecimal)
+			return new DecimalData(d);
+		else
+			return new IntegerData(i);
 	}
 }
