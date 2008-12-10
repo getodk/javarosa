f6fef88fe29db7221cff249807c022bf9fa7962a
czue <czue@localhost>
2008-12-10 21:50:02 +0000

    [r1555] [branches/dev] First final draft of image capture workflow.  Integrated the image acquisition code back in with the form widget and added the flows and logic to pass the data back and forth.  Added an answer data class to support multiple pointers for multiple image selection.  This is close to being ready for testing and integration.

---
+++ javarosa/org.javarosa.core.model/src/org/javarosa/core/model/data/MultiPointerAnswerData.java
@@ -0,0 +1,56 @@
+package org.javarosa.core.model.data;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
+
+/**
+ * An answer data storing multiple pointers
+ * @author Cory Zue
+ *
+ */
+public class MultiPointerAnswerData implements IAnswerData {
+
+	private IDataPointer[] data;
+	
+	public MultiPointerAnswerData (IDataPointer[] values) {
+		data = values;
+	}
+	
+	public String getDisplayText() {
+		String toReturn = "";
+		for (int i=0; i < data.length; i++) {
+			if (i != 0) { 
+				toReturn += ", ";
+			}
+			toReturn += data[i].getDisplayText();
+		}
+		return toReturn;
+	}
+
+	public Object getValue() {
+		return data;
+	}
+
+	public void setValue(Object o) {
+		if(o == null) {
+			throw new NullPointerException("Attempt to set an IAnswerData class to null.");
+		}
+		data = (IDataPointer[]) o;
+	}
+
+	public void readExternal(DataInputStream in, PrototypeFactory pf)
+			throws IOException, DeserializationException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void writeExternal(DataOutputStream out) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
