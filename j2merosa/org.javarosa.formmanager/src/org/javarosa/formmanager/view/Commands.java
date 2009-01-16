c382f8ca1092ef806dcbf415ef38e4fe88e11e37
csims <csims@localhost>
2009-01-16 15:27:02 +0000

    [r1777] Added the default key for these commands.

--- j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/Commands.java
+++ j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/Commands.java
@@ -8,6 +8,7 @@ package org.javarosa.formmanager.view;
  *
  */
 public class Commands {
+	public static final String COMMAND_KEY = "cmd_ret_key";
 	public static final String CMD_NONE = "none";
 	public static final String CMD_SELECT_XFORM = "select_xform";
 	public static final String CMD_EXIT = "exit";
