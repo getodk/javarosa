4a67925b1e6343532b754cac9add704b2dd7ef35
csims <ctsims@users.noreply.github.com>
2009-01-16 16:19:01 +0000

    [r1780] A bunch of stuff from the last couple of days that got delayed.

--- j2merosa/org.javarosa.workflow/src/org/javarosa/workflow/activity/WorkflowActivity.java
+++ j2merosa/org.javarosa.workflow/src/org/javarosa/workflow/activity/WorkflowActivity.java
@@ -3,7 +3,11 @@
  */
 package org.javarosa.workflow.activity;
 
+import java.util.Enumeration;
 import java.util.Hashtable;
+import java.util.Vector;
+
+import javax.microedition.lcdui.Command;
 
 import org.javarosa.core.Context;
 import org.javarosa.core.api.Constants;
@@ -17,35 +21,43 @@ import org.javarosa.workflow.model.WorkflowAction;
 import org.javarosa.workflow.view.StateLaunchScreen;
 
 /**
- * WorkflowLaunchActivity is responsible for identifying the current state of a workflow
- * and presenting the appropriate User Interface for that state.
+ * WorkflowLaunchActivity is responsible for identifying the current state of a
+ * workflow and presenting the appropriate User Interface for that state.
  * 
  * @author Clayton Sims
- * @date Jan 13, 2009 
- *
+ * @date Jan 13, 2009
+ * 
  */
 public class WorkflowActivity implements IActivity, IWorkflowActionListener {
-	
+
 	/**
 	 * The workflow that will be used in this activity.
 	 */
 	Workflow workflow;
-	
+
 	WorkflowLaunchContext context;
-	
+
 	IShell shell;
-	
+
 	StateLaunchScreen stateScreen;
 
-	/* (non-Javadoc)
-	 * @see org.javarosa.core.api.IActivity#contextChanged(org.javarosa.core.Context)
+	/** Vector<Command> **/
+	Vector commands = new Vector();
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.javarosa.core.api.IActivity#contextChanged(org.javarosa.core.Context)
 	 */
 	public void contextChanged(Context globalContext) {
 		// TODO Auto-generated method stub
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.api.IActivity#destroy()
 	 */
 	public void destroy() {
@@ -53,14 +65,18 @@ public class WorkflowActivity implements IActivity, IWorkflowActionListener {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.api.IActivity#getActivityContext()
 	 */
 	public Context getActivityContext() {
 		return context;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.api.IActivity#halt()
 	 */
 	public void halt() {
@@ -68,43 +84,65 @@ public class WorkflowActivity implements IActivity, IWorkflowActionListener {
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.api.IActivity#resume(org.javarosa.core.Context)
 	 */
 	public void resume(Context globalContext) {
-		// TODO Auto-generated method stub
-
+		shell.setDisplay(this, stateScreen);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.javarosa.core.api.IActivity#setShell(org.javarosa.core.api.IShell)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.javarosa.core.api.IActivity#setShell(org.javarosa.core.api.IShell)
 	 */
 	public void setShell(IShell shell) {
 		this.shell = shell;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.api.IActivity#start(org.javarosa.core.Context)
 	 */
 	public void start(Context context) {
-		if(context instanceof WorkflowLaunchContext) {
-			this.context = (WorkflowLaunchContext)context;
+		if (context instanceof WorkflowLaunchContext) {
+			this.context = (WorkflowLaunchContext) context;
 			this.workflow = this.context.getWorkflow();
 			this.workflow.setDataModel(this.context.getDataModel());
 		}
 		stateScreen = new StateLaunchScreen("Title", this.workflow);
+		Enumeration en = commands.elements();
+		while(en.hasMoreElements()) {
+			stateScreen.addCommand((Command)en.nextElement());
+		}
 		shell.setDisplay(this, stateScreen);
 	}
 
 	public void actionFired(WorkflowAction action) {
 		Hashtable returnArgs = new Hashtable();
 		returnArgs.put(Constants.RETURN_ARG_KEY, action);
-		shell.returnFromActivity(this,Constants.ACTIVITY_SUSPEND, returnArgs);
+		shell.returnFromActivity(this, Constants.ACTIVITY_SUSPEND, returnArgs);
 	}
-	/* (non-Javadoc)
-	 * @see org.javarosa.core.api.IActivity#annotateCommand(org.javarosa.core.api.ICommand)
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see
+	 * org.javarosa.core.api.IActivity#annotateCommand(org.javarosa.core.api
+	 * .ICommand)
 	 */
 	public void annotateCommand(ICommand command) {
-		throw new RuntimeException("The Activity Class " + this.getClass().getName() + " Does Not Yet Implement the annotateCommand Interface Method. Please Implement It.");
+		if (command.getCommand() instanceof Command) {
+			this.commands.addElement(command.getCommand());
+		} else {
+			throw new RuntimeException(
+					"Attempted to annotate a platform invalid command of class "
+							+ command.getCommand().getClass().toString()
+							+ " to the j2me platform Activity WorkflowActivity");
+		}
 	}
 }
