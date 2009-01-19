96f28b9050879866c34f5a0270487b8c0a594a71
bderenzi <bderenzi@localhost>
2009-01-19 07:51:23 +0000

    [r1793] drew says we can emulate the code in patient entry now.

--- j2merosa/org.javarosa.patient.entry/src/org/javarosa/patient/entry/activity/PatientEntryActivity.java
+++ j2merosa/org.javarosa.patient.entry/src/org/javarosa/patient/entry/activity/PatientEntryActivity.java
@@ -31,7 +31,6 @@ import org.javarosa.xform.util.XFormUtils;
  * exist because <group> and <repeat> are not yet available in our XForms 
  * subset. 
  * 
- * THIS MODULE IS FULL OF CODE THAT SHOULD NOT BE EMULATED.
  * 
  * Please do not take the code in the org.javarosa.patient.entry project
  * as an example of a proper use of the JavaRosa code base. It will be removed
