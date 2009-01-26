4030eafb067af2248c17d9e0f6501f4f651b582c
csims <ctsims@users.noreply.github.com>
2009-01-26 08:30:33 +0000

    [r1862] A huge amount of changes to wire up the CommCare UI specifically for the capabilities of persisting referrals, viewing them, selecting patients, registering new patients (once the form becomes available), and handling referrals when complete.

---
+++ j2merosa/org.javarosa.chsreferral/src/org/javarosa/chsreferral/PatientReferralModule.java
@@ -0,0 +1,28 @@
+package org.javarosa.chsreferral;
+
+import org.javarosa.chsreferral.storage.PatientReferralRMSUtility;
+import org.javarosa.core.Context;
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.api.IModule;
+
+/**
+ * 
+ */
+
+/**
+ * @author Clayton Sims
+ * @date Jan 23, 2009 
+ *
+ */
+public class PatientReferralModule implements IModule {
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IModule#registerModule(org.javarosa.core.Context)
+	 */
+	public void registerModule(Context context) {
+		PatientReferralRMSUtility refRms = new PatientReferralRMSUtility(PatientReferralRMSUtility.getUtilityName());
+		JavaRosaServiceProvider.instance().getStorageManager().getRMSStorageProvider().registerRMSUtility(refRms);
+
+	}
+
+}
