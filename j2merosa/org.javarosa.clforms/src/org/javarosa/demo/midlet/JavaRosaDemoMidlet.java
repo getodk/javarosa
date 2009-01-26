4e554102d5763009d2acb7d901b4c1aac5db5748
alfred <alfred@localhost>
2009-01-26 12:46:48 +0000

    [r1863] rename clforms shell file to prevent further confusion

--- j2merosa/org.javarosa.clforms/src/org/javarosa/demo/midlet/JavaRosaDemoMidlet.java
+++ j2merosa/org.javarosa.clforms/src/org/javarosa/demo/midlet/JavaRosaDemoMidlet.java
@@ -6,7 +6,7 @@ import javax.microedition.midlet.MIDletStateChangeException;
 
 import org.javarosa.core.JavaRosaServiceProvider;
 import org.javarosa.core.api.IShell;
-import org.javarosa.demo.shell.JavaRosaDemoShell;
+import org.javarosa.demo.shell.JavaRosaCLFormsShell;
 import org.javarosa.j2me.view.J2MEDisplay;
 
 /**
@@ -33,12 +33,12 @@ public class JavaRosaDemoMidlet extends MIDlet {
 		 * Duplicate this class and change the following line to
 		 * create a custom midlet to launch from
 		 */
-		shell = new JavaRosaDemoShell();
+		shell = new JavaRosaCLFormsShell();
 
 		// Do NOT edit below
 		JavaRosaServiceProvider.instance().initialize();
 		JavaRosaServiceProvider.instance().setDisplay(new J2MEDisplay(Display.getDisplay(this)));
-		((JavaRosaDemoShell)shell).setMIDlet(this);
+		((JavaRosaCLFormsShell)shell).setMIDlet(this);
 		shell.run();
 	}
 
