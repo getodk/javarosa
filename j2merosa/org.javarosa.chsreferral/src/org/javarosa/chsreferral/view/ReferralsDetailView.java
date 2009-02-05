19aa19854826299271382b8e5527a4fc0b64ca13
bderenzi <bderenzi@users.noreply.github.com>
2009-02-05 06:22:50 +0000

    [r1939] put in swahili translations.  still need clayton to do a bit of work here.  used the same commcare.lang.sw build flag.  is annoying and temporary.

--- j2merosa/org.javarosa.chsreferral/src/org/javarosa/chsreferral/view/ReferralsDetailView.java
+++ j2merosa/org.javarosa.chsreferral/src/org/javarosa/chsreferral/view/ReferralsDetailView.java
@@ -23,9 +23,16 @@ public class ReferralsDetailView extends Form implements IView {
 	public void setReferral(PatientReferral ref) {
 		this.ref = ref;
 		//TODO: Get patient info here somehow.
-		this.append("Name: " + ref.getPatientId());
-		this.append("Date of Referral: " + ref.getDateReferred());
-		this.append("Referral Type: " + ref.getType());
+		// Putting it in swahili.
+		//#if commcare.lang.sw
+		this.append("Jina: " + ref.getPatientId()); // Name
+		this.append("Tarehe ya rufaa: " + ref.getDateReferred()); // Date of referral
+		this.append("Aina ya rufaa: " + ref.getType()); // Referral type
+		//#else
+		this.append("Name: " + ref.getPatientId()); // Name
+		this.append("Date of referral: " + ref.getDateReferred()); // Date of referral
+		this.append("Referral type: " + ref.getType()); // Referral type
+		//#endif
 	}
 	
 	public PatientReferral getReferral() {
