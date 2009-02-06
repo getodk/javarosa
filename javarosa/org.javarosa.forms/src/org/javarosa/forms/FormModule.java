c294662636270d7a9464c096cfa7224d14f812ac
csims <ctsims@users.noreply.github.com>
2009-02-06 15:16:52 +0000

    [r1946] structure to dump changes into

---
+++ javarosa/org.javarosa.forms/src/org/javarosa/forms/FormModule.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package org.javarosa.forms;
+
+import org.javarosa.core.Context;
+import org.javarosa.core.api.IModule;
+
+/**
+ * @author Clayton Sims
+ * @date Feb 5, 2009 
+ *
+ */
+public class FormModule implements IModule {
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IModule#registerModule(org.javarosa.core.Context)
+	 */
+	public void registerModule(Context context) {
+	}
+
+}
