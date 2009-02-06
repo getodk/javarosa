690ee57c629c9e4be05fa402bd87b26dc65375fa
csims <csims@localhost>
2009-02-06 18:45:46 +0000

    [r1950] Removed the crippled copy of the Formmanger from the JavaRosa cross-platform libraries. All form code that is cross-platform should now be shifted into the org.javarosa.forms project until it can be reorganized properly.

--- javarosa/org.javarosa.formmanager/src/org/javarosa/formmanager/controller/IControllerHost.java
+++
@@ -1,9 +0,0 @@
-package org.javarosa.formmanager.controller;
-
-import org.javarosa.core.api.IView;
-
-public interface IControllerHost {
-	void setView (IView view);
-	
-	void controllerReturn (String status);
-}
--- javarosa/org.javarosa.formmanager/src/org/javarosa/formmanager/utility/IFormDefRetrievalMethod.java
+++
@@ -1,8 +0,0 @@
-package org.javarosa.formmanager.utility;
-
-import org.javarosa.core.Context;
-import org.javarosa.core.model.FormDef;
-
-public interface IFormDefRetrievalMethod {
-	FormDef retreiveFormDef(Context context);
-}
