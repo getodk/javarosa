c547770de9024578e3e4da6c24e29317932c461c
csims <csims@localhost>
2009-02-16 20:32:50 +0000

    [r1985] More fun with j2me polish! Some serious hackery going on here.

--- j2merosa/org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/SelectEntryWidget.java
+++ j2merosa/org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/SelectEntryWidget.java
@@ -1,11 +1,28 @@
 package org.javarosa.formmanager.view.chatterbox.widget;
 
+import javax.microedition.lcdui.Image;
+
 import org.javarosa.core.model.QuestionDef;
 
 import de.enough.polish.ui.ChoiceGroup;
+import de.enough.polish.ui.ChoiceItem;
 import de.enough.polish.ui.Container;
 import de.enough.polish.ui.Item;
 
+/**
+ * The base widget for multi and single choice selections.
+ * 
+ * NOTE: This class has a number of Hacks that I've made after rooting through
+ * the j2me polish source. If polish is behaving unpredictably, it is possibly because
+ * of conflicting changes in new Polish versions with this code. I have outlined
+ * the latest versions of polish in which the changes appear to work.
+ * 
+ * Questions should be directed to csims@dimagi.com
+ * 
+ * @author Clayton Sims
+ * @date Feb 16, 2009 
+ *
+ */
 public abstract class SelectEntryWidget extends ExpandedWidget {
 	private int style;
 	protected QuestionDef question;
@@ -19,11 +36,36 @@ public abstract class SelectEntryWidget extends ExpandedWidget {
 	protected Item getEntryWidget (QuestionDef question) {
 		this.question = question;
 		
-		//This is a slight UI hack that is in place to make the choicegroup properly
-		//intercept 'up' and 'down' inputs. Essentially Polish is very broken when it comes
-		//to scrolling nested containers, and this function properly takes the parent (Widget) position
-		//into account as well as the choicegroup's
 		ChoiceGroup cg = new ChoiceGroup("", style) {
+			
+			/** Hack #1 **/
+			// j2me polish refuses to produce events in the case where select items
+			// are already selected. This code intercepts key presses that toggle
+			// selection, and clear any existing selection to prevent the supression
+			// of the appropriate commands.
+			//
+			// NOTE: These changes are only necessary for Polish versions > 2.0.5 as far
+			// as I can tell. I have tested them on 2.0.4 and 2.0.7 and the changes
+			// were compatibly with both
+			protected boolean handleKeyReleased(int keyCode, int gameAction) {
+				boolean gameActionIsFire = getScreen().isGameActionFire(
+						keyCode, gameAction);
+				if (gameActionIsFire) {
+					ChoiceItem choiceItem = (ChoiceItem) this.focusedItem;
+					choiceItem.isSelected = false;
+				}
+				return super.handleKeyReleased(keyCode, gameAction);
+			}
+			
+			/** Hack #2 **/
+			//This is a slight UI hack that is in place to make the choicegroup properly
+			//intercept 'up' and 'down' inputs. Essentially Polish is very broken when it comes
+			//to scrolling nested containers, and this function properly takes the parent (Widget) position
+			//into account as well as the choicegroup's
+			//
+			// I have tested this change with Polish 2.0.4 on Nokia Phones and the emulators and it works
+			// correctly. It also works correctly on Polish 2.0.7 on the emulator, but I have not attempted
+			// to use it on a real nokia phone.
 			public int getRelativeScrollYOffset() {
 				if (!this.enableScrolling && this.parent instanceof Container) {
 					
@@ -54,9 +96,9 @@ public abstract class SelectEntryWidget extends ExpandedWidget {
 				return offset;
 			}
 		};
-		
-		for (int i = 0; i < question.getSelectItems().size(); i++)
+		for (int i = 0; i < question.getSelectItems().size(); i++){
 			cg.append("", null);
+		}
 		
 		this.choicegroup = cg;
 		
