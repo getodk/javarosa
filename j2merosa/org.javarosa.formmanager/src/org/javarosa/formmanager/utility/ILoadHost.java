631f4dcd2ecb0ee1f9789b3aee1a7c881d4d076a
csims <csims@localhost>
2009-02-27 15:29:42 +0000

    [r2020] Javadocs

--- j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/utility/ILoadHost.java
+++ j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/utility/ILoadHost.java
@@ -2,6 +2,19 @@ package org.javarosa.formmanager.utility;
 
 import org.javarosa.core.Context;
 
+/**
+ * An interface for specifying actions to occur at the end of a form
+ * entry activity. 
+ * 
+ * @author Clayton Sims
+ * @date Jan 30, 2009 
+ *
+ */
 public interface ILoadHost {
+	
+	/**
+	 * A handler method that will be called upon completion of a form entry activity.
+	 * @param context The context of the form entry activity that preceded this call.
+	 */
 	public void returnFromLoading(Context context);
 }
