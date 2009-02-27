733f635e989a49881ed5889ec229a4d55c0981d0
csims <ctsims@users.noreply.github.com>
2009-02-27 20:56:07 +0000

    [r2026] Server URL property configured

---
+++ j2merosa/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/properties/FeedbackReportProperties.java
@@ -0,0 +1,118 @@
+package org.javarosa.communication.reporting.properties;
+
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.Vector;
+
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.services.properties.IPropertyRules;
+
+public class FeedbackReportProperties implements IPropertyRules {
+	Hashtable rules;
+	Vector readOnlyProperties;
+
+	public final static String FEEDBACK_REPORT_SERVER = "oj_c_fr";
+	
+	/**
+	 * Creates the JavaRosa set of property rules
+	 */
+	public FeedbackReportProperties() {
+		rules = new Hashtable();
+		readOnlyProperties = new Vector();
+
+		// Site-specific Fields
+		rules.put(FEEDBACK_REPORT_SERVER, new Vector());
+		
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.javarosa.properties.IPropertyRules#allowableValues(String)
+	 */
+	public Vector allowableValues(String propertyName) {
+		return (Vector) rules.get(propertyName);
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.javarosa.properties.IPropertyRules#checkValueAllowed(String,
+	 *      String)
+	 */
+	public boolean checkValueAllowed(String propertyName, String potentialValue) {
+		Vector prop = ((Vector) rules.get(propertyName));
+		if (prop.size() != 0) {
+			// Check whether this is a dynamic property
+			if (prop.size() == 1
+					&& checkPropertyAllowed((String) prop.elementAt(0))) {
+				// If so, get its list of available values, and see whether the
+				// potential value is acceptable.
+				return ((Vector) JavaRosaServiceProvider.instance()
+						.getPropertyManager().getProperty(
+								(String) prop.elementAt(0)))
+						.contains(potentialValue);
+			} else {
+				return ((Vector) rules.get(propertyName))
+						.contains(potentialValue);
+			}
+		} else
+			return true;
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.javarosa.properties.IPropertyRules#allowableProperties()
+	 */
+	public Vector allowableProperties() {
+		Vector propList = new Vector();
+		Enumeration iter = rules.keys();
+		while (iter.hasMoreElements()) {
+			propList.addElement(iter.nextElement());
+		}
+		return propList;
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.javarosa.properties.IPropertyRules#checkPropertyAllowed)
+	 */
+	public boolean checkPropertyAllowed(String propertyName) {
+		Enumeration iter = rules.keys();
+		while (iter.hasMoreElements()) {
+			if (propertyName.equals(iter.nextElement())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * (non-Javadoc)
+	 * 
+	 * @see org.javarosa.properties.IPropertyRules#checkPropertyUserReadOnly)
+	 */
+	public boolean checkPropertyUserReadOnly(String propertyName) {
+		return readOnlyProperties.contains(propertyName);
+	}
+
+	public String getHumanReadableDescription(String propertyName) {
+		if(FEEDBACK_REPORT_SERVER.equals(propertyName)) {
+    		return "Feedback Report Server";
+     	}
+    	
+    	return propertyName;
+	}
+
+	public String getHumanReadableValue(String propertyName, String value) {
+		// What's this method for?
+		return value;
+	}
+
+	public void handlePropertyChanges(String propertyName) {
+		// nothing.  
+		// what's this method for?
+	}	
+}
