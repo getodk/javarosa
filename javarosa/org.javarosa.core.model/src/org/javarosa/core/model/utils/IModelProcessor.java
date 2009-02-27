2ae549ab269fb70670abc9b7b9bf3f41f744e29c
csims <ctsims@users.noreply.github.com>
2009-02-27 15:37:51 +0000

    [r2021] Javadocs

--- javarosa/org.javarosa.core.model/src/org/javarosa/core/model/utils/IModelProcessor.java
+++ javarosa/org.javarosa.core.model/src/org/javarosa/core/model/utils/IModelProcessor.java
@@ -7,14 +7,36 @@ import org.javarosa.core.Context;
 import org.javarosa.core.model.instance.DataModelTree;
 
 /**
+ * An interface for classes which are capable of parsing and performing actions
+ * on Data Model objects.
+ * 
  * @author Clayton Sims
  * @date Jan 27, 2009 
  *
  */
 public interface IModelProcessor {
+	
+	/**
+	 * Processes the provided data model.
+	 * 
+	 * @param tree The data model that will be handled.
+	 */
 	public void processModel(DataModelTree tree);
 	
+	/**
+	 * Initializes this model processor with the provided
+	 * context.
+	 * 
+	 * @param context An activity context
+	 */
 	public void initializeContext(Context context);
 	
+	/**
+	 * Loads the provided context model with any information
+	 * obtained from processing the models provided to this
+	 * model processor.
+	 * 
+	 * @param context An activity context
+	 */
 	public void loadProcessedContext(Context context);
 }
