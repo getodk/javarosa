bf6c7cfe7ee223c166aeabdea5be7f6cdd1324ed
csims <csims@localhost>
2009-02-27 21:19:56 +0000

    [r2027] Should be working.

--- j2merosa/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/activity/FeedbackReportActivity.java
+++ j2merosa/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/activity/FeedbackReportActivity.java
@@ -3,80 +3,113 @@
  */
 package org.javarosa.communication.reporting.activity;
 
+import java.util.Hashtable;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+
+import org.javarosa.communication.http.HttpTransportDestination;
+import org.javarosa.communication.http.HttpTransportMethod;
+import org.javarosa.communication.reporting.properties.FeedbackReportProperties;
+import org.javarosa.communication.reporting.view.FeedbackReportScreen;
 import org.javarosa.core.Context;
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.api.Constants;
 import org.javarosa.core.api.IActivity;
 import org.javarosa.core.api.ICommand;
 import org.javarosa.core.api.IShell;
+import org.javarosa.core.services.transport.ByteArrayPayload;
+import org.javarosa.core.services.transport.IDataPayload;
+import org.javarosa.core.services.transport.TransportMessage;
 
 /**
  * @author Clayton Sims
  * @date Feb 27, 2009 
  *
  */
-public class FeedbackReportActivity implements IActivity {
+public class FeedbackReportActivity implements IActivity, CommandListener {
+	
+	FeedbackReportScreen screen;
+	IShell shell;
+	Context context;
+	
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#annotateCommand(org.javarosa.core.api.ICommand)
 	 */
 	public void annotateCommand(ICommand command) {
-		// TODO Auto-generated method stub
-
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#contextChanged(org.javarosa.core.Context)
 	 */
 	public void contextChanged(Context globalContext) {
-		// TODO Auto-generated method stub
-
+		this.context.mergeInContext(globalContext);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#destroy()
 	 */
 	public void destroy() {
-		// TODO Auto-generated method stub
-
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#getActivityContext()
 	 */
 	public Context getActivityContext() {
-		// TODO Auto-generated method stub
-		return null;
+		return context;
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#halt()
 	 */
 	public void halt() {
-		// TODO Auto-generated method stub
-
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#resume(org.javarosa.core.Context)
 	 */
 	public void resume(Context globalContext) {
-		// TODO Auto-generated method stub
-
+		this.contextChanged(globalContext);
+		this.start(globalContext);
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#setShell(org.javarosa.core.api.IShell)
 	 */
 	public void setShell(IShell shell) {
-		// TODO Auto-generated method stub
-
+		this.shell = shell;
 	}
 
 	/* (non-Javadoc)
 	 * @see org.javarosa.core.api.IActivity#start(org.javarosa.core.Context)
 	 */
 	public void start(Context context) {
-		// TODO Auto-generated method stub
+		screen = new FeedbackReportScreen("");
+		screen.setCommandListener(this);
+		shell.setDisplay(this, screen);
+	}
 
+	public void commandAction(Command c, Displayable d) {
+		if(c.equals(FeedbackReportScreen.SEND_REPORT)) {
+			String message = screen.getString();
+			
+			String url = JavaRosaServiceProvider.instance().getPropertyManager().getSingularProperty(FeedbackReportProperties.FEEDBACK_REPORT_SERVER);
+			
+			String id = JavaRosaServiceProvider.instance().getPropertyManager().getSingularProperty("DeviceID");
+			
+			//TODO: For now, only http is supported, so we hack this to switch to that.
+			int httpmethod = (new HttpTransportMethod()).getId();
+			JavaRosaServiceProvider.instance().getTransportManager().setCurrentTransportMethod(httpmethod);
+			
+			TransportMessage tmessage = new TransportMessage(new ByteArrayPayload(message.getBytes(), "Feedback Message",IDataPayload.PAYLOAD_TYPE_TEXT), new HttpTransportDestination(url),id,-1);
+			JavaRosaServiceProvider.instance().getTransportManager().send(tmessage, httpmethod);
+			
+			//TODO: Feedback!
+		} else if (c.equals(FeedbackReportScreen.CANCEL)) {
+			shell.returnFromActivity(this, Constants.ACTIVITY_COMPLETE, new Hashtable());
+		}
 	}
 
 }
--- j2merosa/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/view/FeedbackReportScreen.java
+++ j2merosa/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/view/FeedbackReportScreen.java
@@ -3,17 +3,29 @@
  */
 package org.javarosa.communication.reporting.view;
 
-import javax.microedition.lcdui.Form;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.TextBox;
+import javax.microedition.lcdui.TextField;
+
+import org.javarosa.core.api.IView;
 
 /**
  * @author Clayton Sims
  * @date Feb 27, 2009 
  *
  */
-public class FeedbackReportScreen extends Form {
+public class FeedbackReportScreen extends TextBox implements IView {
+	public static final Command SEND_REPORT = new Command("Send Report", 1, Command.OK);
+	public static final Command CANCEL = new Command("Cancel", 1, Command.CANCEL);
+	
 
 	public FeedbackReportScreen(String title) {
-		super(title);
+		super(title + "\nPlease enter your feedback report.", "", 500, TextField.ANY);
+	}
+
+
+	public Object getScreenObject() {
+		return this;
 	}
 
 }
