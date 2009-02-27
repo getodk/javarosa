a40601a82776af2404d2629f9402bc2141ee209e
csims <ctsims@users.noreply.github.com>
2009-02-27 20:54:27 +0000

    [r2025] Basic structural elements added

---
+++ j2merosa/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/view/FeedbackReportScreen.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.javarosa.communication.reporting.view;
+
+import javax.microedition.lcdui.Form;
+
+/**
+ * @author Clayton Sims
+ * @date Feb 27, 2009 
+ *
+ */
+public class FeedbackReportScreen extends Form {
+
+	public FeedbackReportScreen(String title) {
+		super(title);
+	}
+
+}
