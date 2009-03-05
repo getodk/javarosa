6c224ef118b4daff8315083b14cd911c15de6191
csims <ctsims@users.noreply.github.com>
2009-03-05 16:41:45 +0000

    [r2047] [no message]

---
+++ j2merosa/org.javarosa.user/src/org/javarosa/user/activity/EditUserActivity.java
@@ -0,0 +1,153 @@
+/**
+ * 
+ */
+package org.javarosa.user.activity;
+
+import java.util.Hashtable;
+
+import javax.microedition.lcdui.AlertType;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+
+import org.javarosa.core.Context;
+import org.javarosa.core.api.Constants;
+import org.javarosa.core.api.IActivity;
+import org.javarosa.core.api.ICommand;
+import org.javarosa.core.api.IShell;
+import org.javarosa.core.api.IView;
+import org.javarosa.user.utility.AddUserContext;
+import org.javarosa.user.view.NewUserForm;
+
+/**
+ * @author Clayton Sims
+ * @date Mar 3, 2009 
+ *
+ */
+public class EditUserActivity implements IActivity, CommandListener {
+
+	private IShell parent = null;
+	public final Command CMD_SAVE = new Command("Save", Command.OK, 1);
+	public final Command CMD_CANCEL = new Command("Exit",Command.BACK, 1);
+	public static final String COMMAND_KEY = "command";
+
+	AddUserContext context;
+	NewUserForm addUser = null;
+	
+	public EditUserActivity (IShell p) {
+		this.parent = p;
+
+	}
+	
+	public void contextChanged(Context globalContext) {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+
+	public Context getActivityContext() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	public void halt() {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public void resume(Context globalContext) {
+		// TODO Auto-generated method stub
+
+	}
+
+	
+	public void start(Context context) {
+		// TODO Auto-generated method stub
+		this.context = new AddUserContext(context);
+		//this.newuserForm = new NewUserForm(this, "Login");
+		//this.loginScreen.setCommandListener(this);
+		//this.loginScreen.loginButton.setItemCommandListener(this);       // set item command listener
+		//parent.setDisplay(this, this.loginScreen);
+		
+		//take this out into an activity
+		addUser = new NewUserForm("Add User", this.context.getDecorator());
+		addUser.addCommand(CMD_SAVE);
+		addUser.addCommand(CMD_CANCEL);
+		addUser.setCommandListener(this);
+		parent.setDisplay(this, addUser);
+
+	}
+	
+	public void commandAction(Command c,Displayable d) {
+		
+		if (c == this.CMD_SAVE)
+		{
+			String answer = addUser.readyToSave();
+
+    		if (answer.equals(""))	{///success
+
+    			final javax.microedition.lcdui.Alert successfulNewUser  = new javax.microedition.lcdui.Alert("User added","User added successfully",null,javax.microedition.lcdui.AlertType.CONFIRMATION);
+    			
+    			parent.setDisplay(this, new IView() {public Object getScreenObject() { return successfulNewUser;}});
+    			parent.setDisplay(this, this.addUser);
+    		}
+    		else if (answer.substring(0,10 ).equals("Username ("))///name already taken..
+    		{
+
+    			final javax.microedition.lcdui.Alert nameTakenError  = new javax.microedition.lcdui.Alert("Problem adding User - name taken",
+						answer, null,AlertType.ERROR);
+    			parent.setDisplay(this, new IView() {public Object getScreenObject() { return nameTakenError;}});
+    			parent.setDisplay(this, this.addUser);
+    		}
+    		else if (answer.substring(0,9).equals("Please fi") )
+    		{
+    			System.out.println(answer.substring(9));
+    			final javax.microedition.lcdui.Alert noInputError  = new javax.microedition.lcdui.Alert("Problem adding User - no input",
+						answer, null,AlertType.ERROR);
+    			parent.setDisplay(this, new IView() {public Object getScreenObject() { return noInputError;}});
+    			parent.setDisplay(this, this.addUser);
+    		}
+    		else if (answer.substring(0,9).equals("Please re"))///password error
+    		{
+    			System.out.println(answer.substring(9));
+    			final javax.microedition.lcdui.Alert passwordMismatchError  = new javax.microedition.lcdui.Alert("Problem adding User - passwords don't match",
+						answer, null,AlertType.ERROR);
+    			passwordMismatchError.setTimeout(javax.microedition.lcdui.Alert.FOREVER);
+    			parent.setDisplay(this, new IView() {public Object getScreenObject() { return passwordMismatchError;}});
+    			parent.setDisplay(this, this.addUser);
+
+    		}
+
+    	}	
+		else if (c == this.CMD_CANCEL)
+		{
+			Hashtable returnArgs = new Hashtable();
+			//returnArgs.put(COMMAND_KEY, Commands.CMD_ADD_USER);
+			parent.returnFromActivity(this, Constants.ACTIVITY_COMPLETE, returnArgs );
+		//	parent.setDisplay(this, this.formsList);			
+		}
+	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#setShell(org.javarosa.core.api.IShell)
+	 */
+	public void setShell(IShell shell) {
+		this.parent = shell;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#annotateCommand(org.javarosa.core.api.ICommand)
+	 */
+	public void annotateCommand(ICommand command) {
+		throw new RuntimeException("The Activity Class " + this.getClass().getName() + " Does Not Yet Implement the annotateCommand Interface Method. Please Implement It.");
+	}
+}
---
+++ j2merosa/org.javarosa.user/src/org/javarosa/user/utility/UserEntity.java
@@ -0,0 +1,147 @@
+/**
+ * 
+ */
+package org.javarosa.user.utility;
+
+import java.io.IOException;
+
+import org.javarosa.core.services.storage.utilities.RMSUtility;
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.entity.model.IEntity;
+import org.javarosa.user.model.User;
+
+/**
+ * @author Clayton Sims
+ * @date Mar 5, 2009 
+ *
+ */
+public class UserEntity implements IEntity {
+	
+	protected int recordID;
+	
+	String username;
+	int userid;
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#entityType()
+	 */
+	public String entityType() {
+		// TODO Auto-generated method stub
+		return "User";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#factory(int)
+	 */
+	public IEntity factory(int recordID) {
+		UserEntity entity = new UserEntity();
+		entity.recordID = recordID;
+		return entity;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#fetchRMS(org.javarosa.core.services.storage.utilities.RMSUtility)
+	 */
+	public Object fetchRMS(RMSUtility rmsu) {
+		User user = new User();
+		try {
+			rmsu.retrieveFromRMS(recordID, user);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (DeserializationException e) {
+			e.printStackTrace();
+		}
+		return user;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#getHeaders(boolean)
+	 */
+	public String[] getHeaders(boolean detailed) {
+		if(!detailed) {
+			return new String[]{"Username", "ID"};
+		} else {
+			return new String[]{"Username", "ID", "Type"};
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#getID()
+	 */
+	public String getID() {
+		return String.valueOf(userid);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#getLongFields(java.lang.Object)
+	 */
+	public String[] getLongFields(Object o) {
+		User u = (User)o;
+		String type = "Unknown";
+		if(User.ADMINUSER.equals(u.getUserType())) {
+			type = "Administrator";
+		} else if(User.DEMO_USER.equals(u.getUserType())) {
+			type = "Demo";
+		} else if(User.STANDARD.equals(u.getUserType())) {
+			type = "Standard";
+		}
+		
+		return new String[]{u.getUsername(), String.valueOf(u.getUserID()), type};
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#getName()
+	 */
+	public String getName() {
+		return username;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#getRecordID()
+	 */
+	public int getRecordID() {
+		return recordID;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#getShortFields()
+	 */
+	public String[] getShortFields() {
+		return new String[]{username, getID()};
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#matchID(java.lang.String)
+	 */
+	public boolean matchID(String key) {
+		//TODO: I don't really understand these methods. These should be matching
+		//pretty broadly, but should be reevaluated once the method contract is clear.
+		String[] fields = this.getShortFields();
+		for(int i = 0; i < fields.length; ++i) {
+			if(fields[i].indexOf(key) != -1) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#matchName(java.lang.String)
+	 */
+	public boolean matchName(String key) {
+		
+		//TODO: I don't really understand these methods. These should be matching
+		//pretty broadly, but should be reevaluated once the method contract is clear.
+		return matchID(key);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.patient.select.activity.IEntity#readEntity(java.lang.Object)
+	 */
+	public void readEntity(Object o) {
+		User u = (User)o;
+		this.username = u.getUsername();
+		this.userid = u.getUserID();
+	}
+
+}
