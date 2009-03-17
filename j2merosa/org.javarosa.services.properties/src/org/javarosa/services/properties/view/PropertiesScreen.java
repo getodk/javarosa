dc2bef85a833f28e0fd74419d79607c43c7a6209
droos <droos@localhost>
2009-03-17 21:36:19 +0000

    [r2099] add more verbose RMS info the settings screen (currently J2ME only; does not go through the storage abstraction layer). also, fix bug that was reporting wrong RMS sizes by a factor of 10.

--- j2merosa/org.javarosa.services.properties/src/org/javarosa/services/properties/view/PropertiesScreen.java
+++ j2merosa/org.javarosa.services.properties/src/org/javarosa/services/properties/view/PropertiesScreen.java
@@ -11,6 +11,8 @@ import javax.microedition.lcdui.Form;
 import javax.microedition.lcdui.Item;
 import javax.microedition.lcdui.StringItem;
 import javax.microedition.lcdui.TextField;
+import javax.microedition.rms.RecordStore;
+import javax.microedition.rms.RecordStoreException;
 
 import org.javarosa.core.JavaRosaServiceProvider;
 import org.javarosa.core.api.IView;
@@ -41,7 +43,7 @@ public class PropertiesScreen extends Form implements IView{
         this.propertyManager = propertyManager;
 
         populateProperties();
-        addRMSInfo();
+        addRMSInfoJ2MEOnly();
     }
 
     public void showPropertiesScreen(Display currentDisplay,
@@ -154,6 +156,25 @@ public class PropertiesScreen extends Form implements IView{
 		
 	}
 
+    private void addRMSInfoJ2MEOnly () {
+    	try {
+	    	String[] stores = RecordStore.listRecordStores();
+			for (int i = 0; i < stores.length; i++) {
+				String rmsName = stores[i];
+				RecordStore rms = RecordStore.openRecordStore(rmsName, false);
+				int size = rms.getSize();
+				int avail = rms.getSizeAvailable();
+				int numRecs = rms.getNumRecords();
+				int perRecord = (numRecs == 0 ? -1 : size / numRecs);
+				
+				this.append(new StringItem(null, rmsName));
+				this.append(new StringItem(null, "Used: " + formatBytes(size)));
+				this.append(new StringItem(null, "Records: " + numRecs + (numRecs > 0 ? " (" + formatBytes(perRecord) + " per record)" : "")));
+				this.append(new StringItem(null, "Available: " + formatBytes(avail)));
+			}
+    	} catch (RecordStoreException rse) { }
+    }
+    
     private void addRMSInfo() {
     	Vector stores = JavaRosaServiceProvider.instance().getStorageManager().getRMSStorageProvider().getUtilityNames();
         int consumedSpace[] = new int[stores.size()];
@@ -187,43 +208,56 @@ public class PropertiesScreen extends Form implements IView{
 
 	}
 
-	private String formatBytes(int bytes) {
-        if (bytes == -1)
-            return "error";
-
-        String kbytes = "";
-        //Check for overflow
-        if(bytes*10 < bytes) {
-        	//Handle this case with low precision
-        	int kb  = bytes / 1024;
-        	kbytes = String.valueOf(kb);
-        }
-        else if(bytes*100 < bytes){
-        	int kb = (bytes*10)/1024;
-        	kbytes = String.valueOf(kb);
-        	if(kbytes.length() >= 2) {
-        		kbytes = kbytes.substring(0,kbytes.length()-2) + "." + kbytes.charAt(kbytes.length()-1);
-        	}
-        	else {
-        		kbytes = "0." + kbytes;
-        	}
-        }
-        else {
-        	int kb = (bytes*100)/1024;
-        	kbytes = String.valueOf(kb);
-        	if(kbytes.length() >= 3) {
-        		kbytes = kbytes.substring(0,kbytes.length()-3) + "." + kbytes.substring(kbytes.length()-2, kbytes.length()-1);
-        	}
-        	else if(kbytes.length() >= 2) {
-        		kbytes = "0." + kbytes;
-        	}
-        	else {
-        		kbytes = "0.0" + kbytes;
-        	}
-        }
-
-        return kbytes + " KB";
+    private String formatBytes (int bytes) {   	
+    	int NUM_DIGITS = 2;
+    	
+    	if (bytes <= 0)
+    		return "err";
+    	
+    	double kb = bytes / 1024.;
+    	String str = String.valueOf(kb);
+    	if (str.indexOf(".") != -1)
+    		str = str.substring(0, Math.min(str.indexOf(".") + 1 + NUM_DIGITS, str.length()));
+    	return str + " KB";
     }
+    
+//	private String formatBytes(int bytes) {
+//        if (bytes == -1)
+//            return "error";
+//
+//        String kbytes = "";
+//        //Check for overflow
+//        if(bytes*10 < bytes) {
+//        	//Handle this case with low precision
+//        	int kb  = bytes / 1024;
+//        	kbytes = String.valueOf(kb);
+//        }
+//        else if(bytes*100 < bytes){
+//        	int kb = (bytes*10)/1024;
+//        	kbytes = String.valueOf(kb);
+//        	if(kbytes.length() >= 2) {
+//        		kbytes = kbytes.substring(0,kbytes.length()-2) + "." + kbytes.charAt(kbytes.length()-1);
+//        	}
+//        	else {
+//        		kbytes = "0." + kbytes;
+//        	}
+//        }
+//        else {
+//        	int kb = (bytes*100)/1024;
+//        	kbytes = String.valueOf(kb);
+//        	if(kbytes.length() >= 3) {
+//        		kbytes = kbytes.substring(0,kbytes.length()-3) + "." + kbytes.substring(kbytes.length()-2, kbytes.length()-1);
+//        	}
+//        	else if(kbytes.length() >= 2) {
+//        		kbytes = "0." + kbytes;
+//        	}
+//        	else {
+//        		kbytes = "0.0" + kbytes;
+//        	}
+//        }
+//
+//        return kbytes + " KB";
+//    }
 
     public Hashtable getItemChoices() {
     	return itemChoices;
