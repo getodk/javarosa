0a5b2d84b8835b467baf9ac9c0623b72ac56c3e4
csims <csims@localhost>
2009-04-03 22:08:46 +0000

    [r2192] Fixed bizarre scrolling behavior.

---
+++ j2merosa/org.javarosa.polish.chatterbox/src/de/enough/polish/ui/MoreUIAccess.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package de.enough.polish.ui;
+
+/**
+ * @author Clayton Sims
+ * @date Apr 3, 2009 
+ *
+ */
+public class MoreUIAccess {
+
+	public static boolean isScrollingContainer(Container c) {
+		return c.enableScrolling;
+	}
+}
