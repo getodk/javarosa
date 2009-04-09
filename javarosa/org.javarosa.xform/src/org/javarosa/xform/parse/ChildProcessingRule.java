96e2de082dfe753fe0701c4921d054195e2ec87e
droos <droos@localhost>
2009-04-09 23:51:20 +0000

    [r2219] testing commit access

--- javarosa/org.javarosa.xform/src/org/javarosa/xform/parse/ChildProcessingRule.java
+++ javarosa/org.javarosa.xform/src/org/javarosa/xform/parse/ChildProcessingRule.java
@@ -24,7 +24,7 @@ public class ChildProcessingRule {
 	public static final int MULT_NOT_ALLOWED = 1;
 	public static final int MULT_PROCESS_FIRST_ONLY = 2;
 	public static final int MULT_PROCESS_ALL = 3;
-
+ 
 	public String name;
 	public IElementHandler handler;
 	public boolean required;
