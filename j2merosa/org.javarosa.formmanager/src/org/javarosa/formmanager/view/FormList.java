a974e6f0b4d05b8217aab17d0feb7bb894c21bae
csims <csims@localhost>
2009-05-05 14:54:39 +0000

    [r2284] Java Code now Compiles Cleanly

--- j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/FormList.java
+++ j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/FormList.java
@@ -20,7 +20,7 @@ import org.javarosa.formmanager.activity.FormListActivity;
 import org.javarosa.user.model.User;
 
 //#if javarosa.usepolishlocalisation
-import de.enough.polish.util.Locale;
+//# import de.enough.polish.util.Locale;
 //#endif
 
 /**
@@ -29,19 +29,21 @@ import de.enough.polish.util.Locale;
  */
 public class FormList extends List implements CommandListener, IView {
 	//#if javarosa.usepolishlocalisation
-	private final Command CMD_EXIT = new Command(Locale.get("menu.Exit"), Command.BACK, 2);
-    private final Command CMD_GETNEWFORMS = new Command(Locale.get("menu.GetNewForms"), Command.SCREEN, 2);
+	//# private final Command CMD_EXIT = new Command(Locale.get("menu.Exit"), Command.BACK, 2);
+	//# private final Command CMD_GETNEWFORMS = new Command(Locale.get("menu.GetNewForms"), Command.SCREEN, 2);
     // CZUE added for testing 
-    private final Command CMD_CAMERA = new Command(Locale.get("menu.Camera"), Command.SCREEN, 2);
+	//# private final Command CMD_CAMERA = new Command(Locale.get("menu.Camera"), Command.SCREEN, 2);
     //Added for debugging(Ndubisi)
-    private final Command CMD_RECORDER = new Command(Locale.get("menu.Recorder"), Command.SCREEN, 2);
-    private final Command CMD_IMAGE_BROWSE = new Command(Locale.get("menu.ImageManagement"), Command.SCREEN, 2);
+	//# private final Command CMD_RECORDER = new Command(Locale.get("menu.Recorder"), Command.SCREEN, 2);
+	//# private final Command CMD_IMAGE_BROWSE = new Command(Locale.get("menu.ImageManagement"), Command.SCREEN, 2);
 	// others
-    private final Command CMD_VIEWMODELS = new Command(Locale.get("menu.ViewSaved"), Command.SCREEN, 3);
-    private final Command CMD_DELETE_FORM = new Command(Locale.get("menu.Delete"),Command.SCREEN,4);
-    private final Command CMD_SHAREFORMS = new Command(Locale.get("menu.ShareForms"), Command.SCREEN, 2);
-    private final Command CMD_SETTINGS = new Command(Locale.get("menu.Settings"), Command.SCREEN, 3);
+	//# private final Command CMD_VIEWMODELS = new Command(Locale.get("menu.ViewSaved"), Command.SCREEN, 3);
+	//# private final Command CMD_DELETE_FORM = new Command(Locale.get("menu.Delete"),Command.SCREEN,4);
+	//# private final Command CMD_SHAREFORMS = new Command(Locale.get("menu.ShareForms"), Command.SCREEN, 2);
+	//# private final Command CMD_SETTINGS = new Command(Locale.get("menu.Settings"), Command.SCREEN, 3);
 	//#else
+    
+    
 	// TODO: These should all be il8n'd
 	private final Command CMD_EXIT = new Command("Exit", Command.BACK, 2);
     private final Command CMD_GETNEWFORMS = new Command("Get New Forms", Command.SCREEN, 2);
