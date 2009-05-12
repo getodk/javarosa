5576557fe7aff056dc0ad4fa2f74bc8b84149a95
routen <routen@localhost>
2009-05-12 11:41:06 +0000

    [r2356] [no message]

--- j2merosa/org.javarosa.reminders/src/org/javarosa/reminders/util/INotificationReceiver.java
+++ j2merosa/org.javarosa.reminders/src/org/javarosa/reminders/util/INotificationReceiver.java
@@ -2,8 +2,6 @@ package org.javarosa.reminders.util;
 
 import java.util.Vector;
 
-import org.javarosa.reminders.model.Reminder;
-
 public interface INotificationReceiver {
 	public void receiveReminders(Vector reminders);
 }
