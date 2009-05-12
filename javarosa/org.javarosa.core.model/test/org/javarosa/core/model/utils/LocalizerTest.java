dc416a7feefe3430420137f503bd0322695369eb
routen <routen@localhost>
2009-05-12 21:00:04 +0000

    [r2388] rollback

--- javarosa/org.javarosa.core.model/test/org/javarosa/core/model/utils/LocalizerTest.java
+++ javarosa/org.javarosa.core.model/test/org/javarosa/core/model/utils/LocalizerTest.java
@@ -610,7 +610,7 @@ public class LocalizerTest extends TestCase  {
 		l.setLocaleMapping("test", "textID", "text");
 		
 		try {
-			//String text = l.getText("textID");
+			String text = l.getText("textID");
 			
 			fail("Retrieved current locale text when current locale not set");
 		} catch (NoSuchElementException nsee) {
