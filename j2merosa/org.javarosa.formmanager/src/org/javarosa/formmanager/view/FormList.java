a2f30b8a8512a8d097cfbd1668473d552c1454b6
routen <routen@localhost>
2009-05-14 19:16:52 +0000

    [r2419] refactoring

--- j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/FormList.java
+++ j2merosa/org.javarosa.formmanager/src/org/javarosa/formmanager/view/FormList.java
@@ -1,12 +1,11 @@
-/**
- *
- */
+
 package org.javarosa.formmanager.view;
 
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Vector;
 
+import javax.microedition.lcdui.Choice;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
@@ -22,59 +21,61 @@ import org.javarosa.formmanager.activity.FormListActivity;
 //# import de.enough.polish.util.Locale;
 //#endif
 
-/**
- * @author Brian DeRenzi
- *
- */
+ 
 public class FormList extends List implements CommandListener, IView {
 	//#if javarosa.usepolishlocalisation
-	//# private final Command CMD_EXIT = new Command(Locale.get("menu.Exit"), Command.BACK, 2);
-	//# private final Command CMD_GETNEWFORMS = new Command(Locale.get("menu.GetNewForms"), Command.SCREEN, 2);
+	//# private final static Command CMD_EXIT = new Command(Locale.get("menu.Exit"), Command.BACK, 2);
+	//# private final static Command CMD_GETNEWFORMS = new Command(Locale.get("menu.GetNewForms"), Command.SCREEN, 2);
     // CZUE added for testing 
-	//# private final Command CMD_CAMERA = new Command(Locale.get("menu.Camera"), Command.SCREEN, 2);
+	//# private final static Command CMD_CAMERA = new Command(Locale.get("menu.Camera"), Command.SCREEN, 2);
     //Added for debugging(Ndubisi)
-	//# private final Command CMD_RECORDER = new Command(Locale.get("menu.Recorder"), Command.SCREEN, 2);
-	//# private final Command CMD_IMAGE_BROWSE = new Command(Locale.get("menu.ImageManagement"), Command.SCREEN, 2);
+	//# private final static Command CMD_RECORDER = new Command(Locale.get("menu.Recorder"), Command.SCREEN, 2);
+	//# private final static Command CMD_IMAGE_BROWSE = new Command(Locale.get("menu.ImageManagement"), Command.SCREEN, 2);
 	// others
-	//# private final Command CMD_VIEWMODELS = new Command(Locale.get("menu.ViewSaved"), Command.SCREEN, 3);
-	//# private final Command CMD_DELETE_FORM = new Command(Locale.get("menu.Delete"),Command.SCREEN,4);
-	//# private final Command CMD_SHAREFORMS = new Command(Locale.get("menu.ShareForms"), Command.SCREEN, 2);
-	//# private final Command CMD_SETTINGS = new Command(Locale.get("menu.Settings"), Command.SCREEN, 3);
+	//# private final static Command CMD_VIEWMODELS = new Command(Locale.get("menu.ViewSaved"), Command.SCREEN, 3);
+	//# private final static Command CMD_DELETE_FORM = new Command(Locale.get("menu.Delete"),Command.SCREEN,4);
+	//# private final static Command CMD_SHAREFORMS = new Command(Locale.get("menu.ShareForms"), Command.SCREEN, 2);
+	//# private final static Command CMD_SETTINGS = new Command(Locale.get("menu.Settings"), Command.SCREEN, 3);
 	//#else
     
     
-	// TODO: These should all be il8n'd
-	private final Command CMD_EXIT = new Command("Exit", Command.BACK, 2);
-    private final Command CMD_GETNEWFORMS = new Command("Get New Forms", Command.SCREEN, 2);
+	private final static Command CMD_EXIT = new Command("Exit", Command.BACK, 2);
+    private final static Command CMD_GETNEWFORMS = new Command("Get New Forms", Command.SCREEN, 2);
     // CZUE added for testing 
-    private final Command CMD_CAMERA = new Command("Camera", Command.SCREEN, 2);
+    private final static Command CMD_CAMERA = new Command("Camera", Command.SCREEN, 2);
     //Added for debugging(Ndubisi)
-    private final Command CMD_RECORDER = new Command("Recorder", Command.SCREEN, 2);
-    private final Command CMD_IMAGE_BROWSE = new Command("Image Management", Command.SCREEN, 2);
+    private final static Command CMD_RECORDER = new Command("Recorder", Command.SCREEN, 2);
+    private final static Command CMD_IMAGE_BROWSE = new Command("Image Management", Command.SCREEN, 2);
 	// others
-    private final Command CMD_VIEWMODELS = new Command("View Saved", Command.SCREEN, 3);
-    private final Command CMD_DELETE_FORM = new Command("Delete",Command.SCREEN,4);
+    private final static Command CMD_VIEWMODELS = new Command("View Saved", Command.SCREEN, 3);
+    private final static Command CMD_DELETE_FORM = new Command("Delete",Command.SCREEN,4);
   //  private final Command CMD_SHAREFORMS = new Command("Share Forms", Command.SCREEN, 2);
-    private final Command CMD_SETTINGS = new Command("Settings", Command.SCREEN, 3);
+    private final static Command CMD_SETTINGS = new Command("Settings", Command.SCREEN, 3);
     //#endif
 
     private FormListActivity parent = null;
 
 	public FormList(FormListActivity p, String title) {
-		this(p, title, List.IMPLICIT);
+		this(p, title, Choice.IMPLICIT);
 	}
 
 	public FormList(FormListActivity p, String title, int listType) {
-
 		super(title, listType);
 		this.parent = p;
 	}
 
-	public Vector loadView(Map args) {
+	/**
+	 * 
+	 * @param formsPositionTitleMap
+	 * @return Vector of form positions
+	 */
+	public Vector loadView(Map formsPositionTitleMap) {
+		// delete all elements of this list
 		this.deleteAll();
+		// create view by adding screen commands
 		this.createView();
 		this.setCommandListener(this);
-		return this.populateWithXForms(args);
+		return this.populateWithXForms(formsPositionTitleMap);
 	}
 
 	private void createView() {
@@ -83,12 +84,12 @@ public class FormList extends List implements CommandListener, IView {
 
 	private void addScreenCommands() {
 		Context c = this.parent.getContext();
-		//boolean demo = false;
-		if(c.getElement("USER")!=null){
-			//User loggedInUser = (User)c.getElement("USER");
-			//if (loggedInUser.getUserType().equals(User.DEMO_USER))
-				//demo = true;
-		}
+//		//boolean demo = false;
+//		if(c.getElement("USER")!=null){
+//			//User loggedInUser = (User)c.getElement("USER");
+//			//if (loggedInUser.getUserType().equals(User.DEMO_USER))
+//				//demo = true;
+//		}
 		this.addCommand(CMD_EXIT);
         //this.addCommand(CMD_OPEN);
         this.addCommand(CMD_DELETE_FORM);
@@ -103,105 +104,110 @@ public class FormList extends List implements CommandListener, IView {
         this.addCommand(CMD_SETTINGS);
         //#endif
         
-        for (int i = 0; i < parent.customCommands.size(); i++) {
-        	addCommand((Command)((ICommand)parent.customCommands.elementAt(i)).getCommand());
+        for (int i = 0; i < this.parent.getCustomCommands().size(); i++) {
+        	Command parentCommand = (Command)((ICommand)this.parent.getCustomCommands().elementAt(i)).getCommand();
+        	addCommand(parentCommand);
         }
 	}
 
+	 
 	/**
-	 * Put the list
+	 * add form titles to this view, in order
+	 * 
+	 * @param formsPositionTitleMap maps ids to form titles
+	 * @return Vector of form positions
 	 */
-	private Vector populateWithXForms(Map listOfForms) {
-		Vector returnVals = new Vector();
-		Enumeration e = listOfForms.keys();
+	private Vector populateWithXForms(Map formsPositionTitleMap) {
+		Vector formPositions = new Vector();
+		Enumeration e = formsPositionTitleMap.keys();
 		while(e.hasMoreElements()){
 			Integer pos = (Integer)e.nextElement();
-			String form = (String)listOfForms.get(pos);
-			this.append(form,null);
-			returnVals.addElement(pos);
+			String formTitle = (String)formsPositionTitleMap.get(pos);
+			this.append(formTitle,null);
+			formPositions.addElement(pos);
 		}
-		return returnVals;
+		return formPositions;
 	}
 
 
+	/* (non-Javadoc)
+	 * @see javax.microedition.lcdui.CommandListener#commandAction(javax.microedition.lcdui.Command, javax.microedition.lcdui.Displayable)
+	 */
 	public void commandAction(Command c, Displayable arg1) {
-		//Hashtable v = new Hashtable();
- 
-		// TODO Auto-generated method stub
+		
 		if (c == List.SELECT_COMMAND) {
-			//LOG
-
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_SELECT_XFORM, new Integer(this.getSelectedIndex()));
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_SELECT_XFORM, new Integer(this.getSelectedIndex()));
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
 
         if (c == CMD_DELETE_FORM) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_DELETE_FORM, new Integer(this.getSelectedIndex()));
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_DELETE_FORM, new Integer(this.getSelectedIndex()));
 			// TODO check for any form dependancies
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
 
 		else if (c == CMD_EXIT) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_EXIT, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_EXIT, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
 
 		else if (c == CMD_GETNEWFORMS) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_GET_NEW_FORM, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_GET_NEW_FORM, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
 
 		else if (c == CMD_VIEWMODELS) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_VIEW_DATA, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_VIEW_DATA, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
         // CZUE: camera test
 		else if (c == CMD_CAMERA) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_CAMERA, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_CAMERA, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
 		else if (c == CMD_IMAGE_BROWSE) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_IMAGE_BROWSE, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_IMAGE_BROWSE, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
 
-		else if (c == parent.CMD_ADD_USER) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_ADD_USER, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+		else if (c == this.parent.CMD_ADD_USER) {
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_ADD_USER, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
         //Recorder debugging(Ndubisi)
 		else if(c == CMD_RECORDER)
 		{
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_RECORDER, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_RECORDER, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
         
         /*
 		if (c == CMD_SHAREFORMS) {
 			this.mainShell.startBToothClient();
 		}
-		*/else if (c == CMD_SETTINGS) {
-			Hashtable returnvals = new Hashtable();
-			returnvals.put(Commands.CMD_SETTINGS, "");
-			this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);
+		*/
+		else if (c == CMD_SETTINGS) {
+			Hashtable commands = new Hashtable();
+			commands.put(Commands.CMD_SETTINGS, "");
+			this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);
 		}
 		//this case should be triggered when new custom commands are added
 		else {
-			for (int i = 0; i < parent.customCommands.size(); i++) {
-				ICommand ic = (ICommand)parent.customCommands.elementAt(i);
-				if (c == ic.getCommand()) {
-					Hashtable returnvals = new Hashtable();
-					returnvals.put(ic.getCommandId(), "");
-					this.parent.viewCompleted(returnvals, ViewTypes.FORM_LIST);		
+			for (int i = 0; i < this.parent.getCustomCommands().size(); i++) {
+				ICommand parentCommand = (ICommand)this.parent.getCustomCommands().elementAt(i);
+				if (c == parentCommand.getCommand()) {
+					Hashtable commands = new Hashtable();
+					commands.put(parentCommand.getCommandId(), "");
+					this.parent.viewCompleted(commands, ViewTypes.FORM_LIST);		
 				}
 			}			
 		}
