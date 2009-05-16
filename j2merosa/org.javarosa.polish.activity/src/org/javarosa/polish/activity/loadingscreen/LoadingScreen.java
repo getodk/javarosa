9a9b670ecab263fd1e1e91c62dc3726b0182452a
routen <routen@localhost>
2009-05-16 18:13:22 +0000

    [r2430] internationalization of loading screen

--- j2merosa/org.javarosa.polish.activity/src/org/javarosa/polish/activity/loadingscreen/LoadingScreen.java
+++ j2merosa/org.javarosa.polish.activity/src/org/javarosa/polish/activity/loadingscreen/LoadingScreen.java
@@ -11,45 +11,47 @@ import javax.microedition.lcdui.Gauge;
 import org.javarosa.core.api.IView;
 
 import de.enough.polish.ui.Form;
+import de.enough.polish.util.Locale;
 
 /**
  * @author Clayton Sims
- * @date Jan 21, 2009 
- *
+ * @date Jan 21, 2009
+ * 
  */
 public class LoadingScreen extends Form implements IView {
 
-	private Gauge g = null;
-	Timer timer;
-	
-	public LoadingScreen() {
-		super("Loading");
-		
-		// Setup the screen
-		this.g = new Gauge("Loading", false, Gauge.INDEFINITE,
-				Gauge.INCREMENTAL_IDLE);
-		append(this.g);
-		TimerTask r = new TimerTask() {
+	private Timer timer;
 
+	public final static String LOADING_STR = Locale.get("view.loading");
+
+	public LoadingScreen() {
+		super(LOADING_STR);
+
+		// Setup the screen
+		final Gauge g = new Gauge(LOADING_STR, false, Gauge.INDEFINITE,
+				Gauge.INCREMENTAL_IDLE);
+		append(g);
+		
+		TimerTask r = new TimerTask() {
 			public void run() {
 				g.setValue(Gauge.INCREMENTAL_UPDATING);
 			}
-			
+
 		};
-		timer = new Timer();
-		timer.schedule(r, 0, 500);
-	}
-	
-	public void destroy(){ 
-		timer.cancel();
+		this.timer = new Timer();
+		this.timer.schedule(r, 0, 500);
 	}
 
-	
-	/* (non-Javadoc)
+	public void destroy() {
+		this.timer.cancel();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.javarosa.core.api.IView#getScreenObject()
 	 */
 	public Object getScreenObject() {
-		// TODO Auto-generated method stub
 		return this;
 	}
 
