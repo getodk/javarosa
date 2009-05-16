e3a93f101d7d96f91092eca25eaf6e2d36db5016
routen <routen@localhost>
2009-05-16 04:31:48 +0000

    [r2423] minor tidying

--- j2merosa/org.javarosa.demo/src/org/javarosa/demo/midlet/JavaRosaDemoMidlet.java
+++ j2merosa/org.javarosa.demo/src/org/javarosa/demo/midlet/JavaRosaDemoMidlet.java
@@ -11,34 +11,33 @@ import org.javarosa.j2me.view.J2MEDisplay;
 
 /**
  * This is the starting point for the JavarosaDemo application
+ * 
  * @author Brian DeRenzi
- *
+ * 
  */
 public class JavaRosaDemoMidlet extends MIDlet {
-	IShell shell = null;
-	
-	
-	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
-		// TODO Auto-generated method stub
-
-	}
-
-	protected void pauseApp() {
-		// TODO Auto-generated method stub
-
-	}
+	private IShell shell = null;
 
 	protected void startApp() throws MIDletStateChangeException {
 		/*
-		 * Duplicate this class and change the following line to 
-		 * create a custom midlet to launch from 
+		 * Duplicate this class and change the following line to create a custom
+		 * midlet to launch from
 		 */
 		shell = new JavaRosaDemoShell(this);
 
 		// Do NOT edit below
 		JavaRosaServiceProvider.instance().initialize();
-		JavaRosaServiceProvider.instance().setDisplay(new J2MEDisplay(Display.getDisplay(this)));
+		JavaRosaServiceProvider.instance().setDisplay(
+				new J2MEDisplay(Display.getDisplay(this)));
 		shell.run();
+	}
+
+	protected void destroyApp(boolean arg0) throws MIDletStateChangeException {
+		// do nothing
+	}
+
+	protected void pauseApp() {
+		// do nothing
 	}
 
 }
