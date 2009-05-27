5fb033e3e5f990801b634279ca0f66465d78bc0b
csims <csims@localhost>
2009-05-27 19:02:26 +0000

    [r2504] [no message]

---
+++ javarosa/org.javarosa.core/src/org/javarosa/core/util/NoLocalizedTextException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.javarosa.core.util;
+
+/**
+ * @author Clayton Sims
+ * @date May 27, 2009 
+ *
+ */
+public class NoLocalizedTextException extends RuntimeException {
+	public NoLocalizedTextException(String message) {
+		super(message);
+	}
+}
