7cd8ef33fb2292c352c5a8e6cba3d7136ffb7fd2
csims <ctsims@users.noreply.github.com>
2009-05-28 17:21:20 +0000

    [r2518] Cleaned up fail-fast exception titling

---
+++ javarosa/org.javarosa.core/src/org/javarosa/core/util/UnregisteredLocaleException.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.javarosa.core.util;
+
+/**
+ * @author Clayton Sims
+ * @date May 28, 2009 
+ *
+ */
+public class UnregisteredLocaleException extends RuntimeException {
+	public UnregisteredLocaleException(String message) {
+		super(message);
+	}
+}
