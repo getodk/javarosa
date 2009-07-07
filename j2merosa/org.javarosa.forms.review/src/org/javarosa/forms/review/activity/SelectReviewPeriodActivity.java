5004132bc05f1192c02f90c37be0635b59684b9a
csims <ctsims@users.noreply.github.com>
2009-07-07 21:45:50 +0000

    [r2714] Review stuff initial ready.

---
+++ j2merosa/org.javarosa.forms.review/src/org/javarosa/forms/review/activity/SelectReviewPeriodActivity.java
@@ -0,0 +1,154 @@
+/**
+ * 
+ */
+package org.javarosa.forms.review.activity;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Hashtable;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.List;
+
+import org.javarosa.core.Context;
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.api.Constants;
+import org.javarosa.core.api.IActivity;
+import org.javarosa.core.api.ICommand;
+import org.javarosa.core.api.IShell;
+import org.javarosa.core.api.IView;
+import org.javarosa.core.model.utils.DateUtils;
+import org.javarosa.forms.review.util.DataModelDateFilter;
+
+/**
+ * @author ctsims
+ *
+ */
+public class SelectReviewPeriodActivity implements IActivity, CommandListener {
+	
+	private static Command BACK = new Command("Back", Command.BACK, 1);
+	
+	IShell shell;
+	
+	List options;
+	
+	Context context;
+	
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#annotateCommand(org.javarosa.core.api.ICommand)
+	 */
+	public void annotateCommand(ICommand command) {
+		throw new RuntimeException("Command annotation is unsupported by the Select Review Period Activity");
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#contextChanged(org.javarosa.core.Context)
+	 */
+	public void contextChanged(Context globalContext) {
+		this.context.mergeInContext(globalContext);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#destroy()
+	 */
+	public void destroy() {
+		options = null;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#getActivityContext()
+	 */
+	public Context getActivityContext() {
+		return context;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#halt()
+	 */
+	public void halt() {
+		//nothing
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#resume(org.javarosa.core.Context)
+	 */
+	public void resume(Context globalContext) {
+		startDisplay();
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#setShell(org.javarosa.core.api.IShell)
+	 */
+	public void setShell(IShell shell) {
+		this.shell = shell;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IActivity#start(org.javarosa.core.Context)
+	 */
+	public void start(Context context) {
+		this.context = context;
+		startDisplay();
+	}
+	
+	private DataModelDateFilter getFilterForOption(int i) {
+		Date start = new Date(0);
+		Date end = new Date();
+		
+		if(i == 0) {
+			start = DateUtils.roundDate(new Date());
+		} else if(i == 1) {
+			end = DateUtils.roundDate(new Date());
+			start = new Date(end.getTime() - DateUtils.DAY_IN_MS);
+		} else if(i == 2) {
+			Calendar c = Calendar.getInstance();
+			c.setTime(new Date());
+			c.set(Calendar.DAY_OF_WEEK, 0);
+			start = c.getTime();
+		} else if(i == 3) {
+			Calendar c = Calendar.getInstance();
+			c.setTime(new Date());
+			c.set(Calendar.DAY_OF_WEEK, 0);
+			end = c.getTime();
+			start = new Date(end.getTime() - DateUtils.DAY_IN_MS*7);
+		}
+		
+		return new DataModelDateFilter(start,end);
+	}
+
+	private void startDisplay() {
+		options = new List(JavaRosaServiceProvider.instance().localize("formreview.period.select"), List.IMPLICIT);
+		options.insert(0,JavaRosaServiceProvider.instance().localize("formreview.period.today"),null);
+		options.insert(1,JavaRosaServiceProvider.instance().localize("formreview.period.yesterday"),null);
+		options.insert(2,JavaRosaServiceProvider.instance().localize("formreview.period.week.this"),null);
+		options.insert(3,JavaRosaServiceProvider.instance().localize("formreview.period.week.last"),null);
+		//options.addCommand(List.SELECT_COMMAND);
+		options.addCommand(BACK);
+		
+		options.setCommandListener(this);
+		shell.setDisplay(this, new IView() {
+			public Object getScreenObject() {
+				// TODO Auto-generated method stub
+				return options;
+			}
+			
+		});
+	}
+
+	public void commandAction(Command c, Displayable d) {
+		Hashtable returnArgs = new Hashtable();
+		if(c == BACK ) {
+			shell.returnFromActivity(this,Constants.ACTIVITY_CANCEL, returnArgs);
+		} else {
+			int index = options.getSelectedIndex();
+			if(index == -1) {
+				System.out.println("How did we do this???");
+			} else {
+				returnArgs.put(Constants.RETURN_ARG_KEY, getFilterForOption(index));
+				shell.returnFromActivity(this, Constants.ACTIVITY_COMPLETE, returnArgs);
+			}
+		}
+	}
+}
---
+++ j2merosa/org.javarosa.forms.review/src/org/javarosa/forms/review/util/DataModelEntity.java
@@ -0,0 +1,143 @@
+/**
+ * 
+ */
+package org.javarosa.forms.review.util;
+
+import java.io.IOException;
+import java.util.Date;
+
+import org.javarosa.core.JavaRosaServiceProvider;
+import org.javarosa.core.model.instance.DataModelTree;
+import org.javarosa.core.model.storage.DataModelTreeRMSUtility;
+import org.javarosa.core.model.utils.DateUtils;
+import org.javarosa.core.services.storage.utilities.RMSUtility;
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.entity.model.IEntity;
+
+/**
+ * @author ctsims
+ *
+ */
+public class DataModelEntity implements IEntity {
+	
+	private String modelName;
+	private Date dateSaved;
+	private int recordId;
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#entityType()
+	 */
+	public String entityType() {
+		return "Data Model";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#factory(int)
+	 */
+	public IEntity factory(int recordID) {
+		DataModelEntity entity = new DataModelEntity();
+		entity.recordId = recordID;
+		return entity;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#fetchRMS(org.javarosa.core.services.storage.utilities.RMSUtility)
+	 */
+	public Object fetchRMS(RMSUtility rmsu) {
+		DataModelTreeRMSUtility rms = (DataModelTreeRMSUtility)rmsu;
+		DataModelTree tree = new DataModelTree();
+		try {
+			rms.retrieveFromRMS(this.recordId, tree);
+			return tree;
+		} catch (IOException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error loading DataModelTree from RMS to load as an Entity.");
+		} catch (DeserializationException e) {
+			e.printStackTrace();
+			throw new RuntimeException("Error loading DataModelTree from RMS to load as an Entity.");
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#getHeaders(boolean)
+	 */
+	public String[] getHeaders(boolean detailed) {
+		if(detailed) {
+			return new String[] {"Name of Form", "Date Entered", }; 
+		} else {
+			return new String[] {"Name", "Date"};
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#getID()
+	 */
+	public String getID() {
+		return String.valueOf(recordId);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#getLongFields(java.lang.Object)
+	 */
+	public String[] getLongFields(Object o) {
+		DataModelTree tree = (DataModelTree)o;
+		return new String[] {tree.getName(), DateUtils.getDifferenceFromToday(tree.getDateSaved())};
+
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#getName()
+	 */
+	public String getName() {
+		return modelName;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#getRecordID()
+	 */
+	public int getRecordID() {
+		return recordId;
+	}
+	
+	public Date getDateSaved() {
+		return new Date(dateSaved.getTime());
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#getShortFields()
+	 */
+	public String[] getShortFields() {
+		return new String[] {modelName, DateUtils.getDifferenceFromToday(dateSaved)};
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#matchID(java.lang.String)
+	 */
+	public boolean matchID(String key) {
+		return matchName(key);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#matchName(java.lang.String)
+	 */
+	public boolean matchName(String key) {
+		String[] fields = getShortFields();
+		for(int i = 0; i < fields.length ; ++i) {
+			if(fields[i].toLowerCase().startsWith(key.toLowerCase())) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.javarosa.entity.model.IEntity#readEntity(java.lang.Object)
+	 */
+	public void readEntity(Object o) {
+		DataModelTree tree = (DataModelTree)o;
+		dateSaved = tree.getDateSaved();
+		modelName = tree.getName();
+		recordId = tree.getRecordId();
+	}
+
+}
