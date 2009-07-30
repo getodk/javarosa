4bc2890d9ba2ef362ac23b1872986db638ba2011
routen <routen@localhost>
2009-07-30 06:25:24 +0300

    more mms

--- javarosa-transport/transport2/main/src/org/javarosa/services/transport/impl/mms/MMSTransportMessage.java
+++ javarosa-transport/transport2/main/src/org/javarosa/services/transport/impl/mms/MMSTransportMessage.java
@@ -1,7 +1,5 @@
 package org.javarosa.services.transport.impl.mms;
 
-import java.util.Vector;
-
 import org.javarosa.services.transport.Transporter;
 import org.javarosa.services.transport.impl.BasicTransportMessage;
 
@@ -11,17 +9,24 @@ import de.enough.polish.io.Serializable;
 public class MMSTransportMessage extends BasicTransportMessage implements
 		Serializable {
 
-	/**
-	 * SMS messages can be no longer than 140 characters in length
-	 */
-	public final static int MAX_SMS_LENGTH = 140;
+ 
+	public MMSTransportMessage(String str, String address,String subject,String priority,String contentId,String contentLocation,String contentMimeType) {
+		setAddress(address);
+		setSubject(subject);
+		setPriority(priority);
+		setContentId(contentId);
+		setContentLocation(contentLocation);
+		setContentMimeType(contentMimeType);
+	}
+
+	 
 
 	/**
 	 * 
 	 */
 	private String address;
 	private String subject;
-	private String priority;
+	private String priority; // "high", "normal" or "low"
 	private String contentId;
 	private String contentLocation;
 	private String contentMimeType;
@@ -74,44 +79,7 @@ public class MMSTransportMessage extends BasicTransportMessage implements
 		this.contentMimeType = contentMimeType;
 	}
 
-	/**
-	 * @param str
-	 * @param destinationURL
-	 */
-	public MMSTransportMessage(String str, String address) {
-		setAddress(address);
-		setContent(splitSMS(str));
-	}
 
-	/**
-	 * 
-	 * SMS can be of maximum 140 characters in length.
-	 * 
-	 * If the message to be sent is greater, it is partitioned.
-	 * 
-	 * @param str
-	 * @return Vector of strings to be sent as separate messages
-	 */
-	private Vector splitSMS(String str) {
-		String message = str;
-		Vector v = new Vector();
-
-		// if message is too long split it
-		while (message.length() > MAX_SMS_LENGTH) {
-			String part = message.substring(0, MAX_SMS_LENGTH);
-			v.addElement(part);
-			message = message.substring(MAX_SMS_LENGTH + 1);
-		}
-
-		// whatever remaining of the message after
-		// chopping out 140 character length chunks
-		// must also be added
-		if (message.length() > 0)
-			v.addElement(message);
-		return v;
-	}
-
- 
 
 	/*
 	 * (non-Javadoc)
