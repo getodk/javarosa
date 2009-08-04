a9a9b02b15235eee8a4d22b1a05bc7de4d3fa5cc
ctsims <ctsims@localhost>
2009-08-04 16:05:45 +0300

    Added back in transport listener

---
+++ javarosa-transport/transport2/main/src/org/javarosa/services/transport/ITransporter.java
@@ -0,0 +1,33 @@
+package org.javarosa.services.transport;
+
+import org.javarosa.services.transport.message.TransportMessage;
+
+/**
+ * 
+ * A Transporter is given a TransportMessage in its constructor and has the
+ * ability to send it via the send method
+ * 
+ * Example Transporter constructor: <code>
+ * public SimpleHttpTransporter(SimpleHttpTransportMessage message) {
+	this.message = message;
+ * }
+ * </code>
+ * 
+ * The TransportService spawns a thread which calls the send method. So
+ * exceptions are caught and are recorded via message object methods:
+ * <code>setFailureReason()</code> and <code>setStatus()</code>
+ * 
+ */
+public interface ITransporter {
+
+	/**
+	 * 
+	 * (Attempt to) send the message given
+	 * 
+	 */
+	public void send(TransportMessage message);
+	
+	public ITransporter getTransporter();
+	
+	public void stop();
+}
---
+++ javarosa-transport/transport2/main/src/org/javarosa/services/transport/http/HttpTransportMessage.java
@@ -0,0 +1,159 @@
+/**
+ * 
+ */
+package org.javarosa.services.transport.http;
+
+import java.io.InputStream;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.javarosa.core.services.transport.ByteArrayPayload;
+import org.javarosa.core.services.transport.IDataPayload;
+import org.javarosa.services.transport.ITransporter;
+import org.javarosa.services.transport.listeners.TransportListener;
+import org.javarosa.services.transport.message.TransportMessage;
+
+/**
+ * @author ctsims
+ *
+ */
+public class HttpTransportMessage implements TransportMessage {
+	
+	private IDataPayload messagePayload;
+	private Date created;
+	private Date sent;
+	
+	private Vector transportListeners = new Vector();
+	private int status = TransportListener.TRANSPORTING;
+	
+	private String URI;
+	
+	private int attempts = 1;
+	
+	private int delay = 0; 
+	
+	private byte[] response;
+	
+	private String contentType;
+	
+	private long nextReady;
+	
+	/**
+	 * NOTE: For Serialization only!!!
+	 */
+	public HttpTransportMessage() {
+		
+	}
+	public HttpTransportMessage(String URI, byte[] data) {
+		this(URI, new ByteArrayPayload(data, null, ByteArrayPayload.PAYLOAD_TYPE_TEXT));
+	}
+	public HttpTransportMessage(String URI, IDataPayload payload) {
+		HttpHeaderAppendingVisitor visitor = new HttpHeaderAppendingVisitor();
+		messagePayload = (IDataPayload)payload.accept(visitor);
+		contentType = visitor.getOverallContentType();
+		nextReady = new Date().getTime();
+	}
+
+	public ITransporter createTransporter() {
+		return new HttpTransporter().getTransporter();
+	}
+
+	public InputStream getContentStream() {
+		return messagePayload.getPayloadStream();
+	}
+
+	public String getContentType() {
+		return contentType;
+	}
+
+	public Date getCreated() {
+		return created;
+	}
+
+	public Date getSent() {
+		return sent;
+	}
+
+	public int getStatus() {
+		return status;
+	}
+	
+	public byte[] getResponse() {
+		return response;
+	}
+
+	protected void setSuccesfulResponse(byte[] response) {
+		this.response = response;
+		this.sent = new Date();
+		this.status = TransportListener.SUCCESS;
+		this.succeedListeners(response);
+	}
+	
+	public void setAttemptsLeft(int attempts) {
+		this.attempts = attempts;
+	}
+	
+	public int getAttemptsLeft() {
+		return this.attempts;
+	}
+	
+	public void setRetryDelay(int delay) {
+		this.delay = delay;
+	}
+	
+	public String getDestinationURL() {
+		return URI;
+	}
+	
+	public boolean isReady() {
+		if(nextReady < new Date().getTime()) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	protected void fail(String reason, int code) { 
+		fail(reason, code, false);
+	}
+	
+	protected void fail(String reason, int code, boolean completely) {
+		this.attempts--;
+		this.nextReady = new Date().getTime() + delay;
+		if(completely) {
+			this.attempts = 0;
+		}
+		if(this.attempts > 0) {
+			updateListeners("Sending failed, retrying " + getAttemptsLeft() + "more times...");
+		}
+		else {
+			failListeners(code, reason);
+		}
+	}
+	
+	private void updateListeners(String message) {
+		for(Enumeration en = transportListeners.elements(); en.hasMoreElements();) {
+			TransportListener listener = (TransportListener)en.nextElement();
+			listener.onUpdate(this, message);
+		}
+	}
+	
+	private void failListeners(int failureType, String message) {
+		for(Enumeration en = transportListeners.elements(); en.hasMoreElements();) {
+			TransportListener listener = (TransportListener)en.nextElement();
+			listener.onFailure(this, failureType, message);
+		}
+	}
+	
+	private void succeedListeners(byte[] response) {
+		for(Enumeration en = transportListeners.elements(); en.hasMoreElements();) {
+			TransportListener listener = (TransportListener)en.nextElement();
+			listener.onSuccess(this, response);
+		}
+	}
+
+	public void addTransportListener(TransportListener listener) {
+		transportListeners.addElement(listener);
+	}
+}
