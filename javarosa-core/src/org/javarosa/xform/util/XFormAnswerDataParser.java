c0c7f9debe417197b1f2b30dd88ffa8c62fa81f0
carlhartung <carlhartung@localhost>
2009-08-05 10:11:14 -0700

    geopoint is delinated with a space, not a ,

--- javarosa-core/src/org/javarosa/xform/util/XFormAnswerDataParser.java
+++ javarosa-core/src/org/javarosa/xform/util/XFormAnswerDataParser.java
@@ -125,7 +125,7 @@ public class XFormAnswerDataParser {
 		case Constants.DATATYPE_GEOPOINT:
 
             try {
-                Vector gpv = (trimmedText == null ? null : DateUtils.split(trimmedText,",",false));
+                Vector gpv = (trimmedText == null ? null : DateUtils.split(trimmedText," ",false));
                 double gp[] = new double[2];
                 gp[0] = Double.parseDouble(((String)gpv.elementAt(0)));
                 gp[1] = Double.parseDouble(((String)gpv.elementAt(1)));
