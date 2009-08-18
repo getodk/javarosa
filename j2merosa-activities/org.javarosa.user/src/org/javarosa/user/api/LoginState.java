c399609b1e41776c9743a193186d9806af357215
ctsims <ctsims@localhost>
2009-08-18 14:25:08 -0400

    Move appropriate new code into the right branch.

---
+++ j2merosa-activities/org.javarosa.user/src/org/javarosa/user/api/LoginState.java
@@ -0,0 +1,116 @@
+/**
+ * 
+ */
+package org.javarosa.user.api;
+
+import javax.microedition.lcdui.Alert;
+import javax.microedition.lcdui.AlertType;
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Item;
+
+import org.javarosa.core.api.State;
+import org.javarosa.core.services.locale.Localization;
+import org.javarosa.j2me.view.J2MEDisplay;
+import org.javarosa.user.api.transitions.LoginStateTransitions;
+import org.javarosa.user.model.User;
+import org.javarosa.user.utility.LoginContext;
+import org.javarosa.user.utility.Terms;
+import org.javarosa.user.view.LoginForm;
+
+/**
+ * @author ctsims
+ *
+ */
+public class LoginState implements State<LoginStateTransitions>, CommandListener {
+	
+	LoginStateTransitions transitions;
+	LoginForm view;
+	
+	public LoginState() {
+		
+	}
+
+	public State<LoginStateTransitions> enter(LoginStateTransitions transitions) {
+		this.transitions = transitions;
+		view = new LoginForm();
+		view.setCommandListener(this);
+		view.setPasswordMode(LoginContext.PASSWORD_FORMAT_NUMERIC);
+		return this;
+	}
+	
+	public void start() {
+		J2MEDisplay.getDisplay().setCurrent(view);
+	}
+
+	/*
+	 * listener for the String Item (login button)
+	 * 
+	 * @see
+	 * javax.microedition.lcdui.ItemCommandListener#commandAction(javax.microedition
+	 * .lcdui.Command, javax.microedition.lcdui.Item)
+	 */
+	public void commandAction(Command c, Item item) {
+		if (c == LoginForm.CMD_LOGIN_BUTTON) {
+
+			// user trying to login, must be validated..
+			if (this.view.validateUser()) {
+				transitions.loggedIn(view.getLoggedInUser());
+				return;
+
+			}
+			showError(Terms.loginIncorrect, Terms.tryAgain, this);
+
+		}
+
+		// #if javarosa.login.demobutton
+		else if (c == LoginForm.CMD_DEMO_BUTTON) {
+			showError(Terms.demoMode, Terms.demoModeIntro, this);
+		}
+		// #endif
+
+	}
+
+	// listener for the display
+	public void commandAction(Command c, Displayable d) {
+		if (c == LoginForm.CMD_CANCEL_LOGIN) {
+			transitions.exit();
+		} else if (c == LoginForm.CMD_LOGIN_BUTTON) {
+			if (this.view.validateUser()) {
+				transitions.loggedIn(view.getLoggedInUser());
+				return;
+			}
+			showError(Localization.get("activity.login.loginincorrect"), Localization.get("activity.login.tryagain"));
+
+		}
+
+		// #if javarosa.login.demobutton
+		else if (c == LoginForm.CMD_DEMO_BUTTON) {
+			showError(Localization.get("activity.login.demomode"), Localization.get("activity.login.demomode.intro"), this);
+		} else if (d instanceof Alert) {
+			// returning from demo mode warning popup
+			User u = new User();
+			u.setUsername(User.DEMO_USER); // NOTE: Using a user type as a
+			// username also!
+			u.setUserType(User.DEMO_USER);
+			transitions.loggedIn(u);
+		}
+		// #endif
+	}
+
+	private void showError(String s, String message) {
+		showError(s, message, this);
+	}
+
+	private void showError(String s, String message, CommandListener cl) {
+		//#style mailAlert
+		final Alert alert = new Alert(s, message, null, AlertType.ERROR);
+		alert.setTimeout(Alert.FOREVER);
+		if (cl != null) {
+			//alert.setCommandListener(cl);
+		}
+		J2MEDisplay.getDisplay().setCurrent(alert, view);
+	}
+
+}
