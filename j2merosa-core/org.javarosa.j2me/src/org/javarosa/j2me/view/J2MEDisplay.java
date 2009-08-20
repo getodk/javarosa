b8463d5e898d1fc0bd2eaa1397f010ab59831939
Drew Roos <droos@dimagi.com>
2009-08-20 13:11:18 -0400

    refactor J2MEDisplay

--- j2merosa-core/org.javarosa.j2me/src/org/javarosa/j2me/view/J2MEDisplay.java
+++ j2merosa-core/org.javarosa.j2me/src/org/javarosa/j2me/view/J2MEDisplay.java
@@ -18,37 +18,59 @@ package org.javarosa.j2me.view;
 
 import javax.microedition.lcdui.Display;
 import javax.microedition.lcdui.Displayable;
-
-import org.javarosa.core.api.IView;
-import org.javarosa.core.util.IncompatibleViewException;
+import javax.microedition.midlet.MIDlet;
 
 public class J2MEDisplay {
-
 	private static Display display;
 	
-	public static void setDisplay(Display display) {
+	public static void init (MIDlet m) {
+		setDisplay(Display.getDisplay(m));
+	}
+	
+	public static void setDisplay (Display display) {
 		J2MEDisplay.display = display;
 	}
-	
-	/* (non-Javadoc)
-	 * @see org.javarosa.core.api.IDisplay#setView(org.javarosa.core.api.IView)
-	 */
-	public void setView(IView view) {
-		if(view instanceof Displayable) {
-			display.setCurrent((Displayable)view);
-		} else if(view.getScreenObject() instanceof Displayable) {
-			display.setCurrent((Displayable)view.getScreenObject());
+
+	public static Display getDisplay () {
+		if (display == null) {
+			throw new RuntimeException("Display has not been set from MIDlet; call J2MEDisplay.init()");
 		}
-		else {
-			throw new IncompatibleViewException("J2ME Display did not recognize the type of a view " + view.toString());
-		}
-	}
-	
-	public static Object getDisplayObject() {
-		return getDisplay();
-	}
-	
-	public static Display getDisplay() {
+		
 		return display;
 	}
+	
+	public void setView (Displayable d) {
+		display.setCurrent(d);
+	}
+	
+	
+//    //good utility function
+//    private void showError(String title, String message) {
+//    	//#style mailAlert
+//    	Alert alert = new Alert(title, message, null, AlertType.ERROR);
+//    	alert.setTimeout(Alert.FOREVER);
+//    	//alert.setCommandListener(this);
+//    	
+//    	// Clayton Sims - Apr 27, 2009 : 
+//    	//Really not-thrilled with how this works. Unfortunately this is an instance of views not being able
+//    	//to propogate up new views. Maybe there should be a centralized way to use alerts? Or are we fine
+//    	//with using the existing elements?
+//    	Alert.setCurrent((Display)JavaRosaServiceProvider.instance().getDisplay().getDisplayObject(), alert, this);
+//    }
+//    
+//    
+//	private void showError(String s, String message) {
+//		showError(s, message, this);
+//	}
+//
+//	private void showError(String s, String message, CommandListener cl) {
+//		//#style mailAlert
+//		final Alert alert = new Alert(s, message, null, AlertType.ERROR);
+//		alert.setTimeout(Alert.FOREVER);
+//		if (cl != null) {
+//			//alert.setCommandListener(cl);
+//		}
+//		J2MEDisplay.getDisplay().setCurrent(alert, view);
+//	}
+	
 }
