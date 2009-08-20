554e00de45554d44332ec6aa2c1b0369db10cce6
Drew Roos <mrgriscom@users.noreply.github.com>
2009-08-20 11:27:08 -0400

    convert entity select from activity to state

---
+++ j2merosa-activities/org.javarosa.entity/src/org/javarosa/entity/model/view/EntitySelectSortPopup.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (C) 2009 JavaRosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.javarosa.entity.model.view;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.Displayable;
+
+import org.javarosa.entity.activity.EntitySelectActivity;
+import org.javarosa.entity.activity.EntitySelectState;
+
+import de.enough.polish.ui.Choice;
+import de.enough.polish.ui.ChoiceGroup;
+import de.enough.polish.ui.Form;
+import de.enough.polish.ui.Item;
+import de.enough.polish.ui.ItemStateListener;
+
+public class EntitySelectSortPopup extends Form implements IView, CommandListener, ItemStateListener {
+	private EntitySelectView psv;
+	private EntitySelectState psa;
+	
+    private ChoiceGroup sortField;
+    private Command cancelCmd;
+
+    public EntitySelectSortPopup (EntitySelectView psv, EntitySelectState psa) {
+		//#style patselSortPopup
+		super("Sort by...");
+
+		this.psv = psv;
+		this.psa = psa;
+		
+		sortField = new ChoiceGroup("", Choice.EXCLUSIVE);
+		sortField.append("Name", null);
+		sortField.append("ID", null);
+		sortField.setSelectedIndex(psv.sortByName ? 0 : 1, true);
+		append(sortField);
+		sortField.setItemStateListener(this);
+		
+		cancelCmd = new Command("Cancel", Command.CANCEL, 1);
+		addCommand(cancelCmd);
+		setCommandListener(this);
+    }
+
+	public Object getScreenObject() {
+		return this;
+	}
+    
+    public void show () {
+    	psa.setView(this);
+    }
+	
+	public void commandAction(Command cmd, Displayable d) {
+		if (d == this) {
+			if (cmd == cancelCmd) {
+				psa.showList();
+			}
+		}
+	}
+
+	public void itemStateChanged(Item item) {
+		if (item == sortField) {
+			System.out.println(sortField.getSelectedIndex());
+			psv.changeSort(sortField.getSelectedIndex() == 0);
+			psa.showList();
+		}
+	}
+}
---
+++ j2merosa-core/org.javarosa.j2me/src/org/javarosa/j2me/view/J2MEDisplay.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2009 JavaRosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.javarosa.j2me.view;
+
+import javax.microedition.lcdui.Display;
+import javax.microedition.lcdui.Displayable;
+
+import org.javarosa.core.api.IDisplay;
+import org.javarosa.core.api.IView;
+import org.javarosa.core.util.IncompatibleViewException;
+
+public class J2MEDisplay implements IDisplay {
+
+	private final Display display;
+	
+	public J2MEDisplay(Display display) {
+		this.display = display;
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.javarosa.core.api.IDisplay#setView(org.javarosa.core.api.IView)
+	 */
+	public void setView(IView view) {
+		if(view instanceof Displayable) {
+			display.setCurrent((Displayable)view);
+		} else if(view.getScreenObject() instanceof Displayable) {
+			display.setCurrent((Displayable)view.getScreenObject());
+		}
+		else {
+			throw new IncompatibleViewException("J2ME Display did not recognize the type of a view " + view.toString());
+		}
+	}
+	
+	public Object getDisplayObject() {
+		return getDisplay();
+	}
+	
+	public Display getDisplay() {
+		return display;
+	}
+}
