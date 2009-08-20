d465d264a019359c4abb4d7459dcda342c0fca0b
Drew Roos <droos@dimagi.com>
2009-08-20 11:09:44 -0400

    tweak state interface; add state stack

--- javarosa-core/org.javarosa.core/src/org/javarosa/core/api/StateMachine.java
+++ javarosa-core/org.javarosa.core/src/org/javarosa/core/api/StateMachine.java
@@ -3,9 +3,25 @@
  */
 package org.javarosa.core.api;
 
+import java.util.EmptyStackException;
+import java.util.Stack;
+
 /**
  * @author ctsims
  *
  */
-public interface StateMachine {
+public class StateMachine {
+	private static Stack statesToReturnTo = new Stack();
+	
+	public static void setStateToReturnTo (State st) {
+		statesToReturnTo.push(st);
+	}
+	
+	public static State getStateToReturnTo () {
+		try {
+			return (State)statesToReturnTo.pop();
+		} catch (EmptyStackException e) {
+			throw new RuntimeException("Tried to return to a saved state, but no state to return to had been set earlier in the workflow");
+		}
+	}
 }
