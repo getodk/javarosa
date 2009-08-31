3eb832c01d08fe3fb498cf07baa3836a8d2c2e8a
Drew Roos <droos@dimagi.com>
2009-08-31 09:27:40 -0400

    fixing activities

--- j2merosa-extensions/org.javarosa.formmanager/src/org/javarosa/formmanager/api/CompletedFormOptionsState.java
+++ j2merosa-extensions/org.javarosa.formmanager/src/org/javarosa/formmanager/api/CompletedFormOptionsState.java
@@ -6,6 +6,8 @@ package org.javarosa.formmanager.api;
 import javax.microedition.lcdui.Command;
 import javax.microedition.lcdui.CommandListener;
 import javax.microedition.lcdui.Displayable;
+import javax.microedition.lcdui.Item;
+import javax.microedition.lcdui.ItemStateListener;
 
 import org.javarosa.core.api.State;
 import org.javarosa.core.model.instance.DataModelTree;
@@ -17,7 +19,7 @@ import org.javarosa.j2me.view.J2MEDisplay;
  * @author ctsims
  *
  */
-public class CompletedFormOptionsState implements State<CompletedFormOptionsStateTransitions>, CommandListener {
+public class CompletedFormOptionsState implements State<CompletedFormOptionsStateTransitions>, CommandListener, ItemStateListener {
 	CompletedFormOptionsStateTransitions transitions;
 	DataModelTree data;
 	SendNowSendLaterForm view;
@@ -46,4 +48,18 @@ public class CompletedFormOptionsState implements State<CompletedFormOptionsStat
 		}
 	}
 
+	public void itemStateChanged(Item arg0) {
+		switch (view.getCommandChoice()) {
+		case SendNowSendLaterForm.SEND_NOW_DEFAULT:
+			transitions.sendData(data);
+			break;
+		case SendNowSendLaterForm.SEND_LATER:
+			transitions.skipSend();
+			break;
+		case SendNowSendLaterForm.SEND_NOW_SPEC:
+			transitions.sendToFreshLocation(data);
+			break;
+		}
+	}
+	
 }
