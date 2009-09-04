3650c10f1f12bfb2984656483fac4bcb87f4436c
ctsims <ctsims@users.noreply.github.com>
2009-09-04 15:15:51 -0400

    Language changes. Also: Turned off preprocessors for next button

--- j2merosa-extensions/org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/WidgetEscapeComponent.java
+++ j2merosa-extensions/org.javarosa.polish.chatterbox/src/org/javarosa/formmanager/view/chatterbox/widget/WidgetEscapeComponent.java
@@ -16,6 +16,8 @@
 
 package org.javarosa.formmanager.view.chatterbox.widget;
 
+import org.javarosa.core.services.locale.Localization;
+
 import de.enough.polish.ui.Container;
 import de.enough.polish.ui.Item;
 import de.enough.polish.ui.StringItem;
@@ -24,45 +26,32 @@ public class WidgetEscapeComponent implements IWidgetComponentWrapper {
 	private StringItem nextItem;
 	
 	public WidgetEscapeComponent() {
-		
-		//#if commcare.lang.sw
 		//#style button
-		 nextItem = new StringItem(null,"Endelea",Item.BUTTON);
-	    //#else
-		//#style button
-		 nextItem = new StringItem(null,"Done",Item.BUTTON);
-		 //#endif
-		 
+		 nextItem = new StringItem(null,Localization.get("chatterbox.button.next"),Item.BUTTON);
 	}
 
 	public void init() {
 	}
 	
 	public Item wrapEntryWidget(Item i) {
-		//#if chatterbox.selectmulti.nextbutton
 		Container c = new Container(false);
 			
 		c.add(i);
 		c.add(this.nextItem);
 		i = (Item)c;
-		//#endif
 		
 		return i;
 	}
 
 	public Item wrapInteractiveWidget(Item interactiveWidget) {
 		Item i = interactiveWidget;
-		//#if chatterbox.selectmulti.nextbutton
 		i = this.nextItem;
-		//#endif
 		return i;
 	}
 	
 	public int wrapNextMode(int topReturnMode) {
 		int i = topReturnMode;
-		//#if chatterbox.selectmulti.nextbutton
 		i = ChatterboxWidget.NEXT_ON_SELECT;
-		//#endif
 		return i;
 	}
 }
