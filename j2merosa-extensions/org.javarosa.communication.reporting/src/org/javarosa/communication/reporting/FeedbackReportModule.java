6e8917ba662ef713bd3c5ec369db795a19d5fbfa
Drew Roos <droos@dimagi.com>
2009-10-27 16:17:10 -0400

    slowly fixing build errors

--- j2merosa-extensions/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/FeedbackReportModule.java
+++ j2merosa-extensions/org.javarosa.communication.reporting/src/org/javarosa/communication/reporting/FeedbackReportModule.java
@@ -17,13 +17,12 @@
 package org.javarosa.communication.reporting;
 
 import org.javarosa.communication.reporting.properties.FeedbackReportProperties;
-import org.javarosa.core.Context;
 import org.javarosa.core.JavaRosaServiceProvider;
 import org.javarosa.core.api.IModule;
 
 public class FeedbackReportModule implements IModule {
 
-	public void registerModule(Context context) {
+	public void registerModule() {
 		JavaRosaServiceProvider.instance().getPropertyManager().addRules(new FeedbackReportProperties());
 	}
 
