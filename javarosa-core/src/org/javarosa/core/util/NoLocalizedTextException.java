00f6d10cb74adecebdbcfff5473c6df08dd4e445
Drew Roos <mrgriscom@users.noreply.github.com>
2009-10-28 16:06:14 -0400

    for GUI localizations, force that default locale always has an entry

--- javarosa-core/src/org/javarosa/core/util/NoLocalizedTextException.java
+++ javarosa-core/src/org/javarosa/core/util/NoLocalizedTextException.java
@@ -25,7 +25,19 @@ package org.javarosa.core.util;
  *
  */
 public class NoLocalizedTextException extends RuntimeException {
-	public NoLocalizedTextException(String message) {
+	private String keynames;
+	private String locale;
+	public NoLocalizedTextException(String message, String keynames, String locale) {
 		super(message);
+		this.keynames = keynames;
+		this.locale = locale;
+	}
+	
+	public String getMissingKeyNames() {
+		return keynames;
+	}
+	
+	public String getLocaleMissingKey() {
+		return locale;
 	}
 }
