3d64e9536f1c7aa2c4de0cbcabf78c2b2c00ccbe
Drew Roos <mrgriscom@users.noreply.github.com>
2009-10-29 18:20:29 -0400

    refactor settings screen

--- j2merosa-activities/org.javarosa.services.properties/src/org/javarosa/services/properties/api/PropertyUpdateState.java
+++ j2merosa-activities/org.javarosa.services.properties/src/org/javarosa/services/properties/api/PropertyUpdateState.java
@@ -3,10 +3,10 @@
  */
 package org.javarosa.services.properties.api;
 
-import org.javarosa.core.JavaRosaServiceProvider;
 import org.javarosa.core.api.State;
+import org.javarosa.core.services.PropertyManager;
+import org.javarosa.core.util.TrivialTransitions;
 import org.javarosa.j2me.view.J2MEDisplay;
-import org.javarosa.services.properties.api.transitions.PropertyUpdateTransitions;
 import org.javarosa.services.properties.controller.PropertyUpdateController;
 import org.javarosa.services.properties.controller.PropertyUpdateControllerListener;
 import org.javarosa.services.properties.view.PropertiesScreen;
@@ -15,29 +15,17 @@ import org.javarosa.services.properties.view.PropertiesScreen;
  * @author ctsims
  *
  */
-public class PropertyUpdateState implements State<PropertyUpdateTransitions>, PropertyUpdateControllerListener {
+public abstract class PropertyUpdateState implements State, TrivialTransitions {
 
-	private PropertyUpdateTransitions transitions;
 	private PropertiesScreen screen;
 	
 	public PropertyUpdateState() {
-		screen = new PropertiesScreen(JavaRosaServiceProvider.instance().getPropertyManager());
-		PropertyUpdateController controller = new PropertyUpdateController(screen);
-		controller.setListener(this);
-	}
-
-	public void enter(PropertyUpdateTransitions transitions) {
-		this.transitions = transitions;
+		screen = new PropertiesScreen(PropertyManager._());
+		PropertyUpdateController controller = new PropertyUpdateController(this, screen);
 	}
 
 	public void start() {
 		J2MEDisplay.setView(screen);
 	}
-
-	public void finished() {
-		transitions.done();
-	}
-	
-	
 
 }
