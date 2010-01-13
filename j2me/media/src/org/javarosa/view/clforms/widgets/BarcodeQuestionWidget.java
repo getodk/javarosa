bcfd333b81f4a0669fef4c6469f64f478f906353
Clayton Sims <ctsims@users.noreply.github.com>
2010-01-13 16:21:32 -0500

    getQuestionPrompt body

---
+++ j2me/media/src/org/javarosa/view/clforms/widgets/BarcodeQuestionWidget.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright (C) 2009 JavaRosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.javarosa.view.clforms.widgets;
+
+import javax.microedition.lcdui.Command;
+import javax.microedition.lcdui.CommandListener;
+import javax.microedition.lcdui.TextField;
+
+import org.javarosa.barcode.acquire.BarcodeCaptureScreen;
+import org.javarosa.core.model.QuestionDef;
+import org.javarosa.core.model.data.IAnswerData;
+import org.javarosa.core.model.data.StringData;
+import org.javarosa.formmanager.view.FormElementBinding;
+import org.javarosa.formmanager.view.clforms.acquire.AcquireScreen;
+import org.javarosa.formmanager.view.clforms.acquire.AcquiringQuestionScreen;
+import org.javarosa.j2me.services.BarcodeCaptureService;
+
+/**
+ * @author mel
+ * 
+ *         A SingleQuestionScreen-syle widget that adds a scan command for
+ *         scaning a barcode
+ * 
+ */
+public class BarcodeQuestionWidget extends AcquiringQuestionScreen {
+
+	protected TextField tf;
+	protected BarcodeCaptureService barcodeProcessor;
+
+	public BarcodeQuestionWidget(FormElementBinding question) {
+		super(question);
+	}
+
+	public BarcodeQuestionWidget(FormElementBinding prompt, int num) {
+		super(prompt, num);
+	}
+
+	public BarcodeQuestionWidget(FormElementBinding prompt, String str) {
+		super(prompt, str);
+	}
+
+	public BarcodeQuestionWidget(FormElementBinding prompt, char c) {
+		super(prompt, c);
+	}
+
+	public void creatView() {
+
+		//#if javarosa.usepolishlocalisation
+		//setHint(Locale.get("hint.TypeOrScan"));
+		//#else
+		setHint("Type in your answer");
+		//#endif
+		
+		//#style textBox
+		 tf = new TextField("", "", 200, TextField.ANY);
+		 if(qDef.instanceNode.required)
+				tf.setLabel("*"+((QuestionDef)qDef.element).getLongText()); //visual symbol for required
+				else
+					tf.setLabel(((QuestionDef)qDef.element).getLongText());
+		this.append(tf);
+		this.addNavigationButtons();
+		if (((QuestionDef)qDef.element).getHelpText()!=null){
+			setHint(((QuestionDef)qDef.element).getHelpText());
+		}
+
+	}
+
+	public Command getAcquireCommand() {
+		Command acquire;
+		//#if javarosa.usepolishlocalisation
+		// acquire =  new Command(Locale.get( "menu.Scan"), Command.SCREEN, 3);
+		//#else
+		 acquire = new Command("Scan", Command.SCREEN, 3);
+		//#endif
+		return acquire;
+	}
+
+	public AcquireScreen getAcquireScreen(CommandListener callingListener) {
+		String title = "Scan barcode";
+		//#if javarosa.usepolishlocalisation
+		//title = Locale.get( "title.ScanBarcode");
+		//#endif
+		BarcodeCaptureScreen bcScreen = new BarcodeCaptureScreen(
+				title, this, callingListener);
+		bcScreen.setBarcodeProcessor(barcodeProcessor);
+		return bcScreen;
+	}
+
+	public IAnswerData getWidgetValue() {
+		String s = tf.getString();
+		return (s == null || s.equals("") ? null : new StringData(s));
+	}
+
+	protected void updateDisplay() {
+		tf.setString(((StringData) acquiredData).getDisplayText());
+	}
+
+	public void setBarcodeProcessor(BarcodeCaptureService barcodeProcessor) {
+		this.barcodeProcessor = barcodeProcessor;
+
+	}
+
+}
