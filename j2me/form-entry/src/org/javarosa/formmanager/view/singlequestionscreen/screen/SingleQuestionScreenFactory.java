9fe9775e53b0acf3819ca1798f956c7c95a614d4
Melissa Loudon <mloudon@users.noreply.github.com>
2010-01-14 19:41:13 +0200

    refactoring singlequestionscreenfactory

--- j2me/form-entry/src/org/javarosa/formmanager/view/singlequestionscreen/screen/SingleQuestionScreenFactory.java
+++ j2me/form-entry/src/org/javarosa/formmanager/view/singlequestionscreen/screen/SingleQuestionScreenFactory.java
@@ -4,7 +4,7 @@
 package org.javarosa.formmanager.view.singlequestionscreen.screen;
 
 import org.javarosa.core.model.Constants;
-import org.javarosa.core.model.QuestionDef;
+import org.javarosa.form.api.FormEntryPrompt;
 import org.javarosa.formmanager.view.FormElementBinding;
 import org.javarosa.formmanager.view.singlequestionscreen.acquire.IAcquiringService;
 
@@ -29,11 +29,11 @@ public class SingleQuestionScreenFactory {
 	 * @throws IllegalStateException
 	 */
 	public static SingleQuestionScreen getQuestionScreen(
-			FormElementBinding prompt, boolean fromFormView,
+			FormEntryPrompt prompt, String groupTitle, boolean fromFormView,
 			boolean goingForward) throws IllegalStateException {
 		SingleQuestionScreen screenToReturn = null;
-		int qType = prompt.instanceNode.dataType;
-		int contType = ((QuestionDef) prompt.element).getControlType();
+		int qType = prompt.getDataType();
+		int contType = prompt.getControlType();
 
 		Style style = StyleSheet
 				.getStyle(fromFormView || goingForward ? "OneQPS_Form_Right"
@@ -45,47 +45,47 @@ public class SingleQuestionScreenFactory {
 			case Constants.DATATYPE_TEXT:
 			case Constants.DATATYPE_NULL:
 			case Constants.DATATYPE_UNSUPPORTED:
-				screenToReturn = new TextQuestionScreen(prompt, style);
+				screenToReturn = new TextQuestionScreen(prompt,groupTitle, style);
 
 				break;
 			case Constants.DATATYPE_DATE:
 			case Constants.DATATYPE_DATE_TIME:
-				screenToReturn = new DateQuestionScreen(prompt, style);
+				screenToReturn = new DateQuestionScreen(prompt,groupTitle, style);
 
 				break;
 			case Constants.DATATYPE_TIME:
-				screenToReturn = new TimeQuestionScreen(prompt, style);
+				screenToReturn = new TimeQuestionScreen(prompt,groupTitle, style);
 
 				break;
 			case Constants.DATATYPE_INTEGER:
-				screenToReturn = new NumericQuestionScreen(prompt, style);
+				screenToReturn = new NumericQuestionScreen(prompt,groupTitle, style);
 
 				break;
 			case Constants.DATATYPE_DECIMAL:
-				screenToReturn = new DecimalQuestionScreen(prompt, style);
+				screenToReturn = new DecimalQuestionScreen(prompt,groupTitle, style);
 
 				break;
 
 			case Constants.DATATYPE_BARCODE:
-				screenToReturn = new TextQuestionScreen(prompt, style);
+				screenToReturn = new TextQuestionScreen(prompt,groupTitle, style);
 				break;
 
 			default:
-				screenToReturn = new TextQuestionScreen(prompt, style);
+				screenToReturn = new TextQuestionScreen(prompt,groupTitle, style);
 
 			}
 			break;
 			
 		case Constants.CONTROL_SELECT_ONE:
-			screenToReturn = new Select1QuestionScreen(prompt, style);
+			screenToReturn = new Select1QuestionScreen(prompt,groupTitle, style);
 			break;
 			
 		case Constants.CONTROL_SELECT_MULTI:
-			screenToReturn = new SelectQuestionScreen(prompt, style);
+			screenToReturn = new SelectQuestionScreen(prompt,groupTitle, style);
 			break;
 			
 		case Constants.CONTROL_TEXTAREA:
-			screenToReturn = new TextQuestionScreen(prompt, style);
+			screenToReturn = new TextQuestionScreen(prompt,groupTitle, style);
 			break;
 			
 		default:
@@ -109,7 +109,7 @@ public class SingleQuestionScreenFactory {
 	 * @return
 	 */
 	public static SingleQuestionScreen getQuestionScreen(
-			FormElementBinding prompt, boolean fromFormView,
+			FormElementBinding prompt, String groupTitle, boolean fromFormView,
 			boolean goingForward, IAcquiringService barcodeService) {
 
 		Style style = StyleSheet
