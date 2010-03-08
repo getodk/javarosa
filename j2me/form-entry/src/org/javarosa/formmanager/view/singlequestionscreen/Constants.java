0613199d946985f49a3fe5c06e808eefc01f561e
mloudon <mloudon@localhost>
2010-03-08 12:20:13 +0200

    moving singlequestionscreen style constants out of core

--- j2me/form-entry/src/org/javarosa/formmanager/view/singlequestionscreen/Constants.java
+++ j2me/form-entry/src/org/javarosa/formmanager/view/singlequestionscreen/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
 	public static final String STYLE_TRANSITION_LEFT = "SingleQuestionScreen_Form_Left";
 	public static final String STYLE_TRANSITION_RIGHT = "SingleQuestionScreen_Form_Right";
 	public static final String STYLE_TRANSITION_FLASH = "SingleQuestionScreen_Form_Flash";
+	public static final  String STYLE_PROMPT = "View_All_Prompt";
+	public static final  String STYLE_HEADER = "View_All_Header";
 }
