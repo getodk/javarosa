7df0b4df6c78cd31edb827f791bde6eba1f41d6e
Melissa Loudon <mloudon@users.noreply.github.com>
2010-03-12 16:06:29 +0200

    singlequestionscreen form summary view styles

--- j2me/form-entry/src/org/javarosa/formmanager/view/summary/FormSummaryView.java
+++ j2me/form-entry/src/org/javarosa/formmanager/view/summary/FormSummaryView.java
@@ -1,6 +1,7 @@
 package org.javarosa.formmanager.view.summary;
 
 import java.io.IOException;
+import java.util.Vector;
 
 import javax.microedition.lcdui.Image;
 
@@ -16,19 +17,33 @@ import org.javarosa.form.api.FormEntryPrompt;
 import org.javarosa.formmanager.view.singlequestionscreen.Constants;
 import org.javarosa.j2me.view.J2MEDisplay;
 
+import de.enough.polish.ui.ChoiceItem;
+import de.enough.polish.ui.Color;
 import de.enough.polish.ui.Command;
 import de.enough.polish.ui.List;
+import de.enough.polish.ui.Style;
 import de.enough.polish.ui.StyleSheet;
-import de.enough.polish.util.HashMap;
+import de.enough.polish.ui.borders.BottomBorder;
 
 public class FormSummaryView extends List {
 	private FormEntryModel model;
-	private HashMap indexHash;
 
 	public final Command CMD_EXIT = new Command(Localization.get("menu.Exit"),
 			Command.EXIT, 4);
 	public final Command CMD_SAVE_EXIT = new Command(Localization
 			.get("menu.SaveAndExit"), Command.SCREEN, 4);
+
+	private Entry[] entries;
+
+	static class Entry extends ChoiceItem {
+		FormIndex index;
+
+		Entry(String caption, Image img, Style style, FormIndex index) {
+			super(caption, img, List.IMPLICIT);
+			this.setStyle(style);
+			this.index = index;
+		}
+	}
 
 	public FormSummaryView(FormEntryModel model) {
 		// #style View_All_Form
@@ -44,13 +59,13 @@ public class FormSummaryView extends List {
 		}
 	}
 
-	protected void createView() {
-		int captioncount = 0;
-
-		indexHash = new HashMap();
-
+	private void populateEntries() {
+		Vector<Entry> entriesVec = new Vector<Entry>();
 		FormIndex index = FormIndex.createBeginningOfFormIndex();
 		FormDef form = model.getForm();
+
+		int prevDepth = 0;
+
 		while (!index.isEndOfFormIndex()) {
 			if (index.isInForm() && model.isRelevant(index)) {
 				String text = "";
@@ -59,12 +74,6 @@ public class FormSummaryView extends List {
 				if (model.getEvent(index) == FormEntryController.EVENT_QUESTION) {
 					FormEntryPrompt prompt = model.getQuestionPrompt(index);
 					text = getText(prompt);
-					try {
-						img = Image.createImage(Localization.get(prompt.isRequired()?"questioncompulsory":"question"));
-					} catch (IOException ioe) {
-						img = null;
-						Logger.exception(ioe);
-					}
 				} else if (model.getEvent(index) == FormEntryController.EVENT_PROMPT_NEW_REPEAT) {
 					FormEntryCaption[] hierachy = model
 							.getCaptionHierarchy(index);
@@ -84,22 +93,37 @@ public class FormSummaryView extends List {
 					isHeader = true;
 				}
 				if (!text.equals("")) {
-					append(text, img, isHeader ? StyleSheet
+					Style style = isHeader ? StyleSheet
 							.getStyle(Constants.STYLE_HEADER) : StyleSheet
-							.getStyle(Constants.STYLE_PROMPT));
-					indexHash.put(new Integer(captioncount), index);
-
-					captioncount++;
+							.getStyle(Constants.STYLE_PROMPT);
+					String spacer="";
+					int i = isHeader?-1:0;
+					while (i < index.getDepth() - 2) {
+						spacer = "――" + spacer;
+						i++;
+					}
+					text=img==null?spacer+text:text;
+					entriesVec.addElement(new Entry(text, img, style, index));
 				}
 			}
+			prevDepth = index.getDepth();
 			index = form.incrementIndex(index);
+		}
+		entries = new Entry[entriesVec.size()];
+		entriesVec.copyInto(entries);
+	}
+
+	protected void createView() {
+		populateEntries();
+		for (Entry entry : entries) {
+			append(entry);
 		}
 	}
 
 	private String getText(FormEntryPrompt prompt) {
 		String line = "";
 		line += prompt.getLongText() + " => ";
-		
+
 		IAnswerData answerValue = prompt.getAnswerValue();
 		if (answerValue != null) {
 			line += answerValue.getDisplayText();
@@ -114,7 +138,7 @@ public class FormSummaryView extends List {
 			headertext += caption.getLongText();
 
 			if (caption.getIndex().getInstanceIndex() > -1)
-				headertext += " #" + (caption.getMultiplicity() + 1);
+				headertext += " " + (caption.getMultiplicity() + 1);
 
 			headertext += ": ";
 		}
@@ -129,8 +153,7 @@ public class FormSummaryView extends List {
 		if (selectedIndex < 0) {
 			return null;
 		}
-		FormIndex formIndex = (FormIndex) indexHash.get(new Integer(
-				selectedIndex));
+		FormIndex formIndex = ((Entry) getItem(selectedIndex)).index;
 		return formIndex;
 	}
 
