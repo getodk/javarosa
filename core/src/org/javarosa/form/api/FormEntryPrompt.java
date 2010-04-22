515b40dea5b49ede11bd45ec78db8b4300483036
Clayton Sims <ctsims@users.noreply.github.com>
2010-04-22 17:52:10 -0400

    Enabled a default set of properties for Device Reporting which sets up daily/weekly reports based on initialization (and does nothing if logging is disabled)

---
+++ core/src/org/javarosa/form/api/FormEntryPrompt.java
@@ -0,0 +1,216 @@
+/*
+ * Copyright (C) 2009 JavaRosa
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.javarosa.form.api;
+
+import java.util.Hashtable;
+import java.util.Vector;
+
+import org.javarosa.core.model.Constants;
+import org.javarosa.core.model.FormDef;
+import org.javarosa.core.model.FormIndex;
+import org.javarosa.core.model.ItemsetBinding;
+import org.javarosa.core.model.QuestionDef;
+import org.javarosa.core.model.SelectChoice;
+import org.javarosa.core.model.condition.EvaluationContext;
+import org.javarosa.core.model.data.IAnswerData;
+import org.javarosa.core.model.data.SelectMultiData;
+import org.javarosa.core.model.data.SelectOneData;
+import org.javarosa.core.model.data.helper.Selection;
+import org.javarosa.core.model.instance.TreeElement;
+import org.javarosa.core.model.instance.TreeReference;
+import org.javarosa.formmanager.view.IQuestionWidget;
+
+/**
+ * This class gives you all the information you need to display a question when
+ * your current FormIndex references a QuestionEvent.
+ * 
+ * @author Yaw Anokwa
+ */
+public class FormEntryPrompt extends FormEntryCaption {
+
+    TreeElement mTreeElement;
+    boolean dynamicChoicesPopulated = false;
+    
+    /**
+     * This empty constructor exists for convenience of any supertypes of this prompt
+     */
+    protected FormEntryPrompt() {
+    }
+    
+    /**
+	 * Creates a FormEntryPrompt for the element at the given index in the form.
+	 * 
+	 * @param form
+	 * @param index
+	 */
+    public FormEntryPrompt(FormDef form, FormIndex index) {
+        super(form, index);
+        if (!(element instanceof QuestionDef))
+        	throw new IllegalArgumentException("FormEntryPrompt can only be created for QuestionDef elements");
+        this.mTreeElement = form.getInstance().resolveReference(index.getReference());
+    }
+
+    public int getControlType() {
+        return getQuestion().getControlType();
+    }
+
+    public int getDataType() {
+        return mTreeElement.dataType;
+    }
+
+    // attributes available in the bind, instance and body
+    public String getPromptAttributes() {
+        // TODO: implement me.
+        return null;
+    }
+
+    //note: code overlap with FormDef.copyItemsetAnswer
+    public IAnswerData getAnswerValue() {
+    	QuestionDef q = getQuestion();
+    	
+		ItemsetBinding itemset = q.getDynamicChoices();
+    	if (itemset != null) {
+    		if (itemset.valueRef != null) {
+	    		Vector<SelectChoice> choices = getSelectChoices();
+	    		Vector<String> preselectedValues = new Vector<String>();
+
+	    		//determine which selections are already present in the answer
+	    		if (itemset.copyMode) {
+	    			TreeReference destRef = itemset.getDestRef().contextualize(mTreeElement.getRef());
+	    			Vector<TreeReference> subNodes = form.getInstance().expandReference(destRef);
+	    			for (int i = 0; i < subNodes.size(); i++) {
+	    				TreeElement node = form.getInstance().resolveReference(subNodes.elementAt(i));
+    					String value = itemset.getRelativeValue().evalReadable(form.getInstance(), new EvaluationContext(form.exprEvalContext, node.getRef()));
+    					preselectedValues.addElement(value);
+	    			}
+	    		} else {
+	    			Vector<Selection> sels = new Vector<Selection>();
+	    			IAnswerData data = mTreeElement.getValue();
+	    			if (data instanceof SelectMultiData) {
+	    				sels = (Vector<Selection>)data.getValue();
+	    			} else if (data instanceof SelectOneData) {
+	    				sels = new Vector<Selection>();
+	    				sels.addElement((Selection)data.getValue());
+	    			}
+	    			for (int i = 0; i < sels.size(); i++) {
+	    				preselectedValues.addElement(sels.elementAt(i).xmlValue);
+	    			}
+	    		}
+	    			    		  
+    			//populate 'selection' with the corresponding choices (matching 'value') from the dynamic choiceset
+	    		Vector<Selection> selection = new Vector<Selection>();    		
+	    		for (int i = 0; i < preselectedValues.size(); i++) {
+	    			String value = preselectedValues.elementAt(i);
+	    			SelectChoice choice = null;
+	    			for (int j = 0; j < choices.size(); j++) {
+	    				SelectChoice ch = choices.elementAt(j);
+	    				if (value.equals(ch.getValue())) {
+	    					choice = ch;
+	    					break;
+	    				}
+	    			}
+	    			
+	    			selection.addElement(choice.selection());
+	    		}
+	    		
+	    		//convert to IAnswerData
+	    		if (selection.size() == 0) {
+	    			return null;
+	    		} else if (q.getControlType() == Constants.CONTROL_SELECT_MULTI) {
+	    			return new SelectMultiData(selection);
+	    		} else if (q.getControlType() == Constants.CONTROL_SELECT_ONE) {
+	    			return new SelectOneData(selection.elementAt(0)); //do something if more than one selected?
+	    		} else {
+	    			throw new RuntimeException("can't happen");
+	    		}
+    		} else {
+    			return null; //cannot map up selections without <value>
+    		}
+    	} else { //static choices
+            return mTreeElement.getValue();
+    	}
+    }
+
+    public String getAnswerText() {
+        if (mTreeElement.getValue() == null)
+            return null;
+        else
+            return mTreeElement.getValue().getDisplayText();
+    }
+
+    public String getConstraintText() {
+        return mTreeElement.getConstraint().constraintMsg;
+    }
+
+    public Vector<SelectChoice> getSelectChoices() {
+    	QuestionDef q = getQuestion();
+    	
+		ItemsetBinding itemset = q.getDynamicChoices();
+    	if (itemset != null) {
+    		if (!dynamicChoicesPopulated) {
+    			form.populateDynamicChoices(itemset, mTreeElement.getRef());
+    			dynamicChoicesPopulated = true;
+    		}
+    		return itemset.getChoices();
+    	} else { //static choices
+    		return q.getChoices();
+    	}
+    }
+
+    public void expireDynamicChoices () {
+    	dynamicChoicesPopulated = false;
+		ItemsetBinding itemset = getQuestion().getDynamicChoices();
+		if (itemset != null) {
+			itemset.clearChoices();
+		}
+    }
+    
+    public String getHelpText() {
+        return getQuestion().getHelpText();
+    }
+
+    public boolean isRequired() {
+        return mTreeElement.required;
+    }
+
+    public boolean isReadOnly() {
+        return !mTreeElement.isEnabled();
+    }
+    
+    public QuestionDef getQuestion() {
+    	return (QuestionDef)element;
+    }
+    
+    //==== observer pattern ====//
+    
+	public void register (IQuestionWidget viewWidget) {
+		super.register(viewWidget);
+		mTreeElement.registerStateObserver(this);
+	}
+
+	public void unregister () {
+		mTreeElement.unregisterStateObserver(this);
+		super.unregister();
+	}
+		
+	public void formElementStateChanged(TreeElement instanceNode, int changeFlags) {
+		if (this.mTreeElement != instanceNode)
+			throw new IllegalStateException("Widget received event from foreign question");
+		if (viewWidget != null)
+			viewWidget.refreshWidget(changeFlags);		
+	}
+}
---
+++ j2me/deprecated/org.javarosa.patient/src/org/javarosa/model/GraphDataGroup.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (C) 2009 JavaRosa
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package org.javarosa.model;
+
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Vector;
+
+import org.javarosa.core.model.IDataReference;
+import org.javarosa.core.model.data.DateData;
+import org.javarosa.core.model.data.IAnswerData;
+import org.javarosa.core.model.data.IntegerData;
+import org.javarosa.core.model.instance.TreeElement;
+import org.javarosa.patient.model.data.NumericListData;
+import org.javarosa.patient.util.DateValueTuple;
+
+public class GraphDataGroup extends TreeElement {
+	public static final int GRAPH_DATA_ID = 11;
+	
+	IDataReference reference;
+	
+	public GraphDataGroup() {
+		super();
+	}
+	
+	public void setReference(IDataReference reference) {
+		this.reference = reference;
+	}
+	
+	
+	public IAnswerData getValue() {
+		NumericListData returnVal = new NumericListData();
+		for (int k1 = 0; k1 < this.getNumChildren(); k1++) {
+			TreeElement element = this.getChildAt(k1);
+				
+				Date dateValue = null;
+				Integer intValue = null;
+				
+				for (int k2 = 0; k2 < element.getNumChildren(); k2++) {
+					TreeElement subElement = element.getChildAt(k2);
+					if("date".equals(subElement.getName())) {
+						IAnswerData dateAnswer = (IAnswerData)subElement.getValue();
+						if(dateAnswer != null) {
+							dateValue = (Date)dateAnswer.getValue();
+						}
+					}
+					if("value".equals(subElement.getName())) {
+						IAnswerData valueAnswer = (IAnswerData)subElement.getValue();
+						if(valueAnswer != null) {
+							Object answer = valueAnswer.getValue();
+							if(answer instanceof String) {
+								intValue = Integer.valueOf(((String)answer));
+							} else if(answer instanceof Integer) {
+								intValue = (Integer)answer;
+							}
+						}
+					}
+				}
+				if(intValue != null) {
+					if(dateValue == null) {
+						dateValue = new Date();
+					}
+					returnVal.addMeasurement(new DateValueTuple(dateValue, intValue.intValue()));
+				}
+		}
+		return returnVal;
+	}
+	
+	public void setValue(IAnswerData data) {
+		if(data instanceof NumericListData) {
+			clearDataNodes();
+			Vector values = (Vector)((NumericListData) data).getValue();
+			Enumeration en  = values.elements();
+			while(en.hasMoreElements()) {
+				DateValueTuple element = ((DateValueTuple)en.nextElement());
+				TreeElement dataNode = new TreeElement("data");
+				//The this.reference thing? Sketchy
+				TreeElement date = new TreeElement("date");
+				date.setValue(new DateData(element.date));
+				TreeElement value = new TreeElement("value");
+				value.setValue(new IntegerData(element.value));
+				dataNode.addChild(date);
+				dataNode.addChild(value);
+				this.addChild(dataNode);
+			}
+		}
+	}
+	
+	private void clearDataNodes() {
+		for (int i = 0; i < this.getNumChildren(); i++) {
+			TreeElement element = this.getChildAt(i);
+			
+			if(element.getName() == "data") {
+				this.removeChild(element);
+			}
+		}
+	}
+}
