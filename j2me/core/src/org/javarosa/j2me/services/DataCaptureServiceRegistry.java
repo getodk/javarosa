50a451690577cee9408b1c8b654b338151604e92
Melissa Loudon <mloudon@users.noreply.github.com>
2010-07-09 14:52:39 +0200

    adding location service

--- j2me/core/src/org/javarosa/j2me/services/DataCaptureServiceRegistry.java
+++ j2me/core/src/org/javarosa/j2me/services/DataCaptureServiceRegistry.java
@@ -19,7 +19,6 @@ package org.javarosa.j2me.services;
 import java.util.Hashtable;
 
 import org.javarosa.core.services.UnavailableServiceException;
-import org.javarosa.j2me.services.DataCaptureService;
 
 /**
  * This is a registry of services to be passed to a state (such as form entry) that can perform data capture.
@@ -77,8 +76,8 @@ public class DataCaptureServiceRegistry {
 	  		(DataCaptureService.IMAGE.equals(type) && !(service instanceof ImageCaptureService)) || */
 			(DataCaptureService.AUDIO.equals(type) && !(service instanceof AudioCaptureService)) || /*
 			(DataCaptureService.VIDEO.equals(type) && !(service instanceof VideoCaptureService)) || */
-			(DataCaptureService.BARCODE.equals(type) && !(service instanceof BarcodeCaptureService)) /*||
-			(DataCaptureService.LOCATION.equals(type) && !(service instanceof LocationCaptureService)) ||
+			(DataCaptureService.BARCODE.equals(type) && !(service instanceof BarcodeCaptureService)) ||
+			(DataCaptureService.LOCATION.equals(type) && !(service instanceof LocationCaptureService))/*||
 			(DataCaptureService.RFID.equals(type) && !(service instanceof RFIDCaptureService))*/) {
 			throw new RuntimeException("Service is not of the proper type!");
 		}
@@ -106,11 +105,12 @@ public class DataCaptureServiceRegistry {
 		return (BarcodeCaptureService)getService(DataCaptureService.BARCODE);
 	}
 
-	/*
+
 	public LocationCaptureService getLocationCaptureService () throws UnavailableServiceException {
 		return (LocationCaptureService)getService(DataCaptureService.LOCATION);
 	}
 
+	/*
 	public RFIDCaptureService getRFIDCaptureService () throws UnavailableServiceException {
 		return (RFIDCaptureService)getService(DataCaptureService.RFID);
 	}
