6c6d85b7a78eb680ab3510c3e24c717c68f0982e
Drew Roos <droos@dimagi.com>
2010-08-29 18:26:03 +0200

    allow recovery if phone is turned off mid-transaction

--- j2me/core/src/org/javarosa/j2me/storage/rms/RMSTransaction.java
+++ j2me/core/src/org/javarosa/j2me/storage/rms/RMSTransaction.java
@@ -3,9 +3,12 @@ package org.javarosa.j2me.storage.rms;
 import java.util.Enumeration;
 import java.util.Hashtable;
 import java.util.Random;
+import java.util.Vector;
 
 import org.javarosa.core.log.FatalException;
 import org.javarosa.core.log.WrappedException;
+import org.javarosa.core.services.Logger;
+import org.javarosa.core.services.storage.IStorageIterator;
 import org.javarosa.core.services.storage.StorageFullException;
 
 public class RMSTransaction {
@@ -70,6 +73,7 @@ public class RMSTransaction {
 		try {
 			purgeCached(tx);
 		} catch (Exception e) {
+			Logger.log("rms-tx", "critical failure during transaction commit");
 			throw new WrappedException("ERROR: failed to commit transaction! you should assume your application data has become inconsistent/corrupted. sorry...", e);
 		} finally {
 			clearTx();
@@ -85,12 +89,35 @@ public class RMSTransaction {
 		try {
 			restoreCached(tx);
 		} catch (Exception e) {
+			Logger.log("rms-tx", "critical failure during transaction rollback");
 			throw new WrappedException("ERROR: failed to rollback transaction! you should assume your application data has become inconsistent/corrupted. sorry...", e);
 		} finally {
 			clearTx();
 		}
 	}
 		
+	public static void cleanup () {
+		if (anyTxOpen()) {
+			throw new RuntimeException("should only be called on a fresh boot with no transactions open");
+		}
+		
+		if (getCacheRMS().getNumRecords() == 0)
+			return;
+		
+		Logger.log("rms-tx", "incomplete transactions found; cleaning up...");
+		try {		
+			Vector<Integer> tx_cache_ids = new Vector<Integer>();
+			for (IStorageIterator ii = getCacheRMS().iterate(); ii.hasMore(); ) {
+				tx_cache_ids.addElement(new Integer(ii.nextID()));
+			}
+			
+			processCache(tx_cache_ids.elements(), true);
+		} catch (Exception e) {
+			Logger.log("rms-tx", "error during transaction clean-up; major data corruption");
+			throw new FatalException("can't clean up aborted transactions; data corruption");
+		}
+	}
+	
 	public static RMSTransaction getTx () {
 		synchronized(transactions) {
 			return transactions.get(Thread.currentThread());
@@ -124,37 +151,44 @@ public class RMSTransaction {
 	}
 		
 	private static void processCache (RMSTransaction tx, boolean restore) {	
+		processCache(tx.tx_records.elements(), restore);
+	}
+		
+	private static void processCache (Enumeration tx_cache_ids, boolean restore) {
 		Hashtable<String, RMSStorageUtility> rmses = new Hashtable<String, RMSStorageUtility>();
 		
-		for (Enumeration e = tx.tx_records.keys(); e.hasMoreElements(); ) {
-			int tx_cache_id = tx.tx_records.get(e.nextElement());
+		while (tx_cache_ids.hasMoreElements()) {
+			int tx_cache_id = ((Integer)tx_cache_ids.nextElement()).intValue();
 			
 			if (restore) {
 				TxCacheEntry entry = (TxCacheEntry)getCacheRMS().read(tx_cache_id);
-				
-				RMSStorageUtility rms = rmses.get(entry.rms);
-				if (rms == null) {
-					rms = new RMSStorageUtility(entry.rms, RawRecord.class);
-					rmses.put(entry.rms, rms);
-				}
-					
-				if (entry.data == null) {
-					if (rms.exists(entry.rec_id)) {
-						rms.remove(entry.rec_id);
-					}
-				} else {
-					if (!rms.exists(entry.rec_id) || !cmpData(entry.data, rms.readBytes(entry.rec_id))) {
-						try {
-							rms.write(entry.toRawRec());
-						} catch (StorageFullException sfe) {
-							throw new WrappedException(sfe);
-						}
-					}
-				}
+				revertRecord(entry, rmses);
 			}
 			
 			getCacheRMS().remove(tx_cache_id);
 		}		
+	}
+
+	private static void revertRecord (TxCacheEntry entry, Hashtable<String, RMSStorageUtility> rmses) {	
+		RMSStorageUtility rms = rmses.get(entry.rms);
+		if (rms == null) {
+			rms = new RMSStorageUtility(entry.rms, RawRecord.class);
+			rmses.put(entry.rms, rms);
+		}
+			
+		if (entry.data == null) {
+			if (rms.exists(entry.rec_id)) {
+				rms.remove(entry.rec_id);
+			}
+		} else {
+			if (!rms.exists(entry.rec_id) || !cmpData(entry.data, rms.readBytes(entry.rec_id))) {
+				try {
+					rms.write(entry.toRawRec());
+				} catch (StorageFullException sfe) {
+					throw new WrappedException(sfe);
+				}
+			}
+		}
 	}
 	
 	private static boolean cmpData (byte[] a, byte[] b) {
