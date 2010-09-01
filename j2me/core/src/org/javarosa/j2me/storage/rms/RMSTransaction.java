20d74f6f547e7e5e729ca9231876c60515f01f24
Drew Roos <mrgriscom@users.noreply.github.com>
2010-09-01 11:14:04 +0200

    add documentation about transactions

--- j2me/core/src/org/javarosa/j2me/storage/rms/RMSTransaction.java
+++ j2me/core/src/org/javarosa/j2me/storage/rms/RMSTransaction.java
@@ -11,6 +11,71 @@ import org.javarosa.core.services.Logger;
 import org.javarosa.core.services.storage.IStorageIterator;
 import org.javarosa.core.services.storage.StorageFullException;
 
+/**
+ * This class enables basic transactions when writing to RMS -- that is, you can begin a transaction,
+ * modify records in RMS, and optionally rollback to the state before you began the transaction, undoing
+ * all your record operations.
+ * 
+ * This works behind the scenes by copying the original version of each touched record to a temporary
+ * RMS, and copying them all back to their original RMSes in the event of rollback. This means:
+ * 
+ * a) the transaction is not guaranteed to be atomic or succeed, but it makes a best-faith effort to do
+ * so. the integrity of the RMSStorageUtilitys is maintained just as with non-transactional access, but
+ * in the event of an unforeseen transaction error, the contents of the records may not be as you expect
+ * (for example, referential integrity between records may be violated, even though each individual record
+ * is intact).
+ * 
+ * b) the state of the underlying RMSes will not be identical after rollback (records may have different
+ * raw RMS record IDs than when they started), but should look the same from the perspective of a user of
+ * the IStorageUtility interface.
+ * 
+ * transaction support is thread-safe, in that multiple transactions can be active in multiple threads at
+ * once (at most one transaction per thread). however, multiple threads accessing records in the same RMS
+ * while at least one transaction is active amongst the threads may lead to unpredictable behavior (i.e., 
+ * thread 2's record changes may be overwritten if thread 1's transaction is rolled back). it is the
+ * responsibility of the threads to coordinate not stepping on each other's toes.
+ * 
+ * beginTransaction(), commitTransaction(), and rollbackTransaction() are the only methods you need be
+ * concerned with as an end-user.
+ * 
+ * once a transaction is active, you can use all the normal IStorageUtility methods as usual, without any
+ * change.
+ * 
+ * beginTransaction() starts a transaction. at most one active transaction per thread is allowed. once the
+ * transaction is active, every write/add/update/remove action by this thread in ANY RMS is logged and
+ * will be undone if the transaction is rolled back. you must ensure that this call is ALWAYS matched by
+ * exactly ONE call to commitTransaction() or rollbackTransaction(), through all possible code and
+ * exception paths. leaving a transaction open indefinitely will seriously tax your device storage, and
+ * none of your record changes will stick.
+ * 
+ * commitTransaction() ends the transaction, causing the changes to the RMS to 'stick'. the backup records
+ * are purged.
+ * 
+ * rollbackTransaction() undoes the changes since beginTransaction(). the transaction is ended and the
+ * RMSUtilities should have the same record IDs and contents as before the transaction. there is NO
+ * provision for auto-rollback (such as in the event of a runtime exception in the storage layer); YOU are
+ * responsible for initiating rollback.
+ * 
+ * when the application is booted, it attempts to rollback any unterminated transactions. this is to
+ * protect against, say, pulling the battery out while a transaction is in progress, or unexpected
+ * application crash.
+ * 
+ * failure modes:
+ * 
+ * there is a chance that committing or rolling back the transaction will fail due to an underlying RMS
+ * error. if this happens, the records of your application's RMS will likely be in an inconsistent state,
+ * and you will probably be hosed. the best bet will be to re-initialize your app from scratch, from an
+ * authoritative source. committing only deletes RMS records, thus (i assume) is less likely to fail
+ * 
+ * while a transaction is in progress, a failure may occur when trying to make the backup during a normal
+ * RMS operation (such as a write()). if this happens, an exception is thrown, and the operation will be
+ * aborted (i.e., the record was not written). HOWEVER, the transaction is still in progress, and you must
+ * decide what to do. you could attempt to keep writing records, for example, though the most logical choice
+ * is to initiate rollback.
+ * 
+ * @author Drew Roos
+ *
+ */
 public class RMSTransaction {
 
 	private class RMSRec {
