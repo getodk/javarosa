424881014206a7484ceb39bc135734add61d3abf
nlauchande <nlauchande@localhost>
2010-09-15 07:05:29 +0200

    Changed class SimpleHttpTransportMessage to allow both Get & POST method. Increased the side of properties screen Textfield to allow 150 characters.

---
+++ j2me/misc/org.javarosa.log/src/org/javarosa/log/util/LogReportUtils.java
@@ -0,0 +1,113 @@
+package org.javarosa.log.util;
+import java.util.Date;
+
+import org.javarosa.core.model.utils.DateUtils;
+import org.javarosa.core.services.PropertyManager;
+import org.javarosa.core.services.properties.JavaRosaPropertyRules;
+import org.javarosa.log.properties.LogPropertyRules;
+
+/**
+ * 
+ */
+
+/**
+ * @author ctsims
+ *
+ */
+public class LogReportUtils {
+	
+	public static final int REPORT_FORMAT_SKIP = 0;
+	public static final int REPORT_FORMAT_COMPACT = 1;
+	public static final int REPORT_FORMAT_FULL = 2;
+	
+	/**
+	 * Updates any enabled report types to set when their next
+	 * report is pending. This method assumes that the most
+	 * pertinent report was sent.
+	 * 
+	 * @param now
+	 */
+	public static void setPendingFromNow(long now) {
+		setPendingWeekly(now);
+		setPendingDaily(now);
+	}
+	
+	private static void setPendingWeekly(long now) {
+		if(getPendingWeeklyReportType(now) > 0) {
+			Date nextWeekly = DateUtils.dateAdd(new Date(now), 7);
+			PropertyManager._().setProperty(LogPropertyRules.LOG_NEXT_WEEKLY_SUBMIT, String.valueOf(nextWeekly.getTime()));
+		}
+	}
+	
+	private static void setPendingDaily(long now) {
+		if(getPendingDailyReportType(now) > 0) {
+			Date nextDaily = DateUtils.dateAdd(new Date(now), 1);
+			PropertyManager._().setProperty(LogPropertyRules.LOG_NEXT_DAILY_SUBMIT, String.valueOf(nextDaily.getTime()));
+		}
+	}
+	
+	/**
+	 * Note: This method is not guaranteed to set any values. If logging is 
+	 * disabled, this will essentially perform a no-op. 
+	 * 
+	 * @param now
+	 */
+	public static void initPendingDates(long now) {
+		//TODO: If the phone's clock is moved way back it's possible for these 
+		//values to become stale. We should check for that somewhere.
+		
+		if(PropertyManager._().getSingularProperty(LogPropertyRules.LOG_NEXT_WEEKLY_SUBMIT) == null) {
+			setPendingWeekly(now);
+		}
+		if(PropertyManager._().getSingularProperty(LogPropertyRules.LOG_NEXT_DAILY_SUBMIT) == null ){
+			setPendingDaily(now);
+		}
+	}
+	
+	public static int getPendingWeeklyReportType(long now) {
+		if(!JavaRosaPropertyRules.LOGS_ENABLED_YES.equals(PropertyManager._().getSingularProperty(JavaRosaPropertyRules.LOGS_ENABLED))) {
+			return REPORT_FORMAT_SKIP;
+		}
+		
+		return reportNeeded(now,PropertyManager._().getSingularProperty(LogPropertyRules.LOG_WEEKLY_SUBMIT), 
+                PropertyManager._().getSingularProperty(LogPropertyRules.LOG_NEXT_WEEKLY_SUBMIT));
+	}
+	
+	public static int getPendingDailyReportType(long now) {
+		if(!JavaRosaPropertyRules.LOGS_ENABLED_YES.equals(PropertyManager._().getSingularProperty(JavaRosaPropertyRules.LOGS_ENABLED))) {
+			return REPORT_FORMAT_SKIP;
+		}
+		return reportNeeded(now,PropertyManager._().getSingularProperty(LogPropertyRules.LOG_DAILY_SUBMIT), 
+                PropertyManager._().getSingularProperty(LogPropertyRules.LOG_NEXT_DAILY_SUBMIT));
+	}
+	
+	private static int reportNeeded(long now, String mode, String next) {
+		if(LogPropertyRules.SHORT.equals(mode) ||
+		   LogPropertyRules.FULL.equals(mode)) {
+
+			if(now > getNextTimeForString(next)) {
+				if(LogPropertyRules.SHORT.equals(mode)) {
+					return REPORT_FORMAT_COMPACT;
+				} else {
+					return REPORT_FORMAT_FULL;
+				}
+			}
+		}
+		return REPORT_FORMAT_SKIP;
+	}
+	
+	
+	private static long getNextTimeForString(String pending) {
+		long nextTime;
+		if(pending == null) {
+			nextTime = 0;
+		} else {
+			try {
+				nextTime = Long.parseLong(pending);
+			} catch(NumberFormatException ife) {
+				nextTime = 0;
+			}
+		}
+		return nextTime;
+	}
+}
