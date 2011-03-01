9545f913b24f705982b188542c28c639b23c5afc
Clayton Sims <csims@dimagi.com>
2011-03-01 17:32:39 -0500

    Updated the login transitions to provide the user's password on login so that it can be processed in memory.

--- j2me/objects/org.javarosa.user/src/org/javarosa/user/api/transitions/LoginTransitions.java
+++ j2me/objects/org.javarosa.user/src/org/javarosa/user/api/transitions/LoginTransitions.java
@@ -11,7 +11,15 @@ import org.javarosa.user.model.User;
  */
 public interface LoginTransitions {
 	
-	public void loggedIn(User u);
+	/**
+	 * Signal that the provided user logged in, and provide the secret provided
+	 * (in case it should be processed to create sync tokens, etc). The password
+	 * _should not be stored into memory_. 
+	 * 
+	 * @param u The User who successfully authenticated
+	 * @param password The secret used
+	 */
+	public void loggedIn(User u, String password);
 	
 	public void exit();
 
