5687164336ad4ea1a5b6e56c3c7609d987e42122
Yaw Anokwa <yanokwa@gmail.com>
2011-08-10 22:22:25 -0700

    expose arbitrary attributes on the <submission> tag

--- core/src/org/javarosa/xform/parse/SubmissionParser.java
+++ core/src/org/javarosa/xform/parse/SubmissionParser.java
@@ -4,6 +4,8 @@
 package org.javarosa.xform.parse;
 
 
+import java.util.Hashtable;
+
 import org.javarosa.core.model.IDataReference;
 import org.javarosa.core.model.SubmissionProfile;
 import org.kxml2.kdom.Element;
@@ -16,11 +18,21 @@ import org.kxml2.kdom.Element;
  */
 public class SubmissionParser {
 	
-	public SubmissionProfile parseSubmission(String method, String action, IDataReference ref, Element element) {
-		String mediatype = element.getAttributeValue(null,"mediatype");
-		
-		return new SubmissionProfile(ref, method, action, mediatype);
-	}
+    public SubmissionProfile parseSubmission(String method, String action, IDataReference ref, Element element) {
+        String mediatype = element.getAttributeValue(null,"mediatype");
+        Hashtable<String,String> attributeMap = new Hashtable<String,String>();
+        int nAttr = element.getAttributeCount();
+        for ( int i = 0 ; i < nAttr ; ++i ) {
+            String name = element.getAttributeName(i);
+            if ( name.equals("ref")) continue;
+            if ( name.equals("bind")) continue;
+            if ( name.equals("method")) continue;
+            if ( name.equals("action")) continue;
+            String value = element.getAttributeValue(i);
+            attributeMap.put(name, value);
+        }
+        return new SubmissionProfile(ref, method, action, mediatype, attributeMap);
+    }
 	
 	public boolean matchesCustomMethod(String method) {
 		 return false;
