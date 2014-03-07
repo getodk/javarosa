245cfd8849732c07ad533e7907e5cc19d83972df
Mitch Sundt <mitchellsundt@users.noreply.github.com>
2014-03-07 11:23:56 -0800

    Remove J2ME code that no longer builds.

--- j2me/form-entry/src/org/javarosa/formmanager/api/transitions/FormListTransitions.java
+++
@@ -1,6 +0,0 @@
-package org.javarosa.formmanager.api.transitions;
-
-public interface FormListTransitions {
-	public void cancel();
-	public void formSelected(String formURI);
-}
--- j2me/javarosa-app/src/org/javarosa/demo/activity/remoteformlist/JRDemoRemoteFormListTransitions.java
+++
@@ -1,6 +0,0 @@
-package org.javarosa.demo.activity.remoteformlist;
-
-public interface JRDemoRemoteFormListTransitions {
-	void formDownload (String formUrl);
-	void back();
-}
