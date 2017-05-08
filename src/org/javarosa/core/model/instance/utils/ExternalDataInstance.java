80fb97b1f2ecbe52f85650ee9b524144bc5334a2
Dave Briccetti <daveb@davebsoft.com>
2017-05-07 23:09:23 -0700

    Make an attempt at reading and parsing the file referenced by jr://file/<path>.

--- src/org/javarosa/core/model/instance/utils/ExternalDataInstance.java
+++ src/org/javarosa/core/model/instance/utils/ExternalDataInstance.java
@@ -4,6 +4,16 @@ import org.javarosa.core.model.instance.AbstractTreeElement;
 import org.javarosa.core.model.instance.DataInstance;
 import org.javarosa.core.model.instance.InstanceInitializationFactory;
 import org.javarosa.core.model.instance.TreeElement;
+import org.kxml2.io.KXmlParser;
+import org.kxml2.kdom.Document;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 public class ExternalDataInstance extends DataInstance {
     private final String srcLocation;
@@ -12,6 +22,22 @@ public class ExternalDataInstance extends DataInstance {
         super(instanceid);
         setName(instanceid);
         this.srcLocation = srcLocation;
+        TreeElement root;
+        Document doc = new Document();
+        try {
+            URI uri = new URI(srcLocation);
+            if (Objects.equals(uri.getScheme(), "jr") && Objects.equals(uri.getHost(), "file")) {
+                KXmlParser parser = new KXmlParser();
+                final String absolutePath = /* ToDo: find out how to get the actual location */
+                        System.getProperty("user.dir") + "/resources" + uri.getPath();
+                parser.setInput( new InputStreamReader(new FileInputStream(absolutePath), "UTF-8"));
+                parser.setFeature(KXmlParser.FEATURE_PROCESS_NAMESPACES, true);
+                doc.parse(parser);
+            }
+        } catch (XmlPullParserException | IOException | URISyntaxException e) {
+            e.printStackTrace(); // ToDo: decide what errors to report and how to report them
+        }
+        //todo root = ...
     }
 
     @Override
