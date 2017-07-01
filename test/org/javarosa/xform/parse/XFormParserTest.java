50dba588875049d2902d84bdadaeb2fd48a8a283
Dave Briccetti <daveb@davebsoft.com>
2017-06-30 21:41:03 -0700

    Add more asserts to show that the XML from the external secondary instance is read and can be retrieved.

--- test/org/javarosa/xform/parse/XFormParserTest.java
+++ test/org/javarosa/xform/parse/XFormParserTest.java
@@ -3,6 +3,8 @@ package org.javarosa.xform.parse;
 import org.javarosa.core.model.FormDef;
 import org.javarosa.core.model.RangeQuestion;
 import org.javarosa.core.model.condition.EvaluationContext;
+import org.javarosa.core.model.instance.AbstractTreeElement;
+import org.javarosa.core.model.instance.DataInstance;
 import org.javarosa.core.model.instance.TreeReference;
 import org.javarosa.xpath.expr.XPathPathExpr;
 import org.javarosa.xpath.parser.XPathSyntaxException;
@@ -15,7 +17,6 @@ import java.util.List;
 import static org.javarosa.core.model.Constants.CONTROL_RANGE;
 import static org.javarosa.xpath.XPathParseTool.parseXPath;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 public class XFormParserTest {
     @Test
@@ -39,8 +40,13 @@ public class XFormParserTest {
                 parseXPath("instance('towns')/data_set")).getReference();
         EvaluationContext evaluationContext = formDef.getEvaluationContext();
         List<TreeReference> treeReferences = evaluationContext.expandReference(treeReference);
-        System.out.println(treeReferences);
-        assertNotEquals(0, treeReference.size());
+        assertEquals(1, treeReferences.size());
+        DataInstance townInstance = formDef.getNonMainInstance("towns");
+        AbstractTreeElement tiRoot = townInstance.getRoot();
+        assertEquals("towndata", tiRoot.getName());
+        assertEquals(1, tiRoot.getNumChildren());
+        AbstractTreeElement dataSetChild = tiRoot.getChild("data_set", 0);
+        assertEquals("us_east", dataSetChild.getValue().getDisplayText());
     }
 
     @Test
