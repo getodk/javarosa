9d60dbd9ecb87795acf212d4718b918c468c4e59
Michal Dudzinski <mdudzinski@soldevelo.com>
2017-07-18 16:07:44 +0200

    Add serialize and restore form instance test

--- test/org/javarosa/xform/parse/XFormParserTest.java
+++ test/org/javarosa/xform/parse/XFormParserTest.java
@@ -2,17 +2,45 @@ package org.javarosa.xform.parse;
 
 import org.javarosa.core.model.FormDef;
 import org.javarosa.core.model.RangeQuestion;
+import org.javarosa.core.model.data.StringData;
+import org.javarosa.core.model.instance.FormInstance;
+import org.javarosa.core.model.instance.TreeElement;
+import org.javarosa.core.services.transport.payload.ByteArrayPayload;
+import org.javarosa.model.xform.XFormSerializingVisitor;
+import org.junit.After;
 import org.junit.Test;
 
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
+import static java.nio.file.Files.copy;
+import static java.nio.file.Files.readAllBytes;
+import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
 import static org.javarosa.core.model.Constants.CONTROL_RANGE;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 
 public class XFormParserTest {
+
+    private static final String FORM_INSTANCE_XML_FILE_NAME = "instance.xml";
+    private static final String AUDIT_NODE = "audit";
+    private static final String AUDIT_ANSWER = "audit111.csv";
+
+    private static final String AUDIT_2_NODE = "audit2";
+    private static final String AUDIT_2_ANSWER = "audit222.csv";
+
+    private static final String AUDIT_3_NODE = "audit3";
+    private static final String AUDIT_3_ANSWER = "audit333.csv";
+
+    @After
+    public void tearDown() throws Exception {
+        new File(FORM_INSTANCE_XML_FILE_NAME).delete();
+    }
+
     @Test
     public void parsesSimpleForm() throws IOException {
         FormDef formDef = parse("simple-form.xml").formDef;
@@ -49,6 +77,50 @@ public class XFormParserTest {
         assertEquals("Number of error messages", 0, parseResult.errorMessages.size());
     }
 
+    @Test
+    public void serializeAndRestoreMetaNamespaceFormInstance() throws IOException {
+        // Given
+        ParseResult parseResult = parse("meta-namespace-form.xml");
+        assertEquals(parseResult.formDef.getTitle(), "Namespace for Metadata");
+        assertEquals("Number of error messages", 0, parseResult.errorMessages.size());
+
+        FormDef formDef = parseResult.formDef;
+        TreeElement audit = findDepthFirst(formDef.getInstance().getRoot(), AUDIT_NODE);
+        TreeElement audit2 = findDepthFirst(formDef.getInstance().getRoot(), AUDIT_2_NODE);
+        TreeElement audit3 = findDepthFirst(formDef.getInstance().getRoot(), AUDIT_3_NODE);
+        assertNotNull(audit);
+        assertNotNull(audit2);
+        assertNotNull(audit3);
+        audit.setAnswer(new StringData(AUDIT_ANSWER));
+        audit2.setAnswer(new StringData(AUDIT_2_ANSWER));
+        audit3.setAnswer(new StringData(AUDIT_3_ANSWER));
+
+        // When
+
+        // serialize the form instance
+        XFormSerializingVisitor serializer = new XFormSerializingVisitor();
+        ByteArrayPayload xml = (ByteArrayPayload) serializer.createSerializedPayload(formDef.getInstance());
+        copy(xml.getPayloadStream(), new File(FORM_INSTANCE_XML_FILE_NAME).toPath(), REPLACE_EXISTING);
+
+        // restore (deserialize) the form instance
+        byte[] formInstanceBytes = readAllBytes(Paths.get(FORM_INSTANCE_XML_FILE_NAME));
+        FormInstance formInstance = XFormParser.restoreDataModel(formInstanceBytes, null);
+
+        // Then
+        audit = findDepthFirst(formInstance.getRoot(), AUDIT_NODE);
+        audit2 = findDepthFirst(formInstance.getRoot(), AUDIT_2_NODE);
+        audit3 = findDepthFirst(formInstance.getRoot(), AUDIT_3_NODE);
+
+        assertNotNull(audit);
+        assertEquals(AUDIT_ANSWER, audit.getValue().getValue());
+
+        assertNotNull(audit2);
+        assertEquals(AUDIT_2_ANSWER, audit2.getValue().getValue());
+
+        assertNotNull(audit3);
+        assertEquals(AUDIT_3_ANSWER, audit3.getValue().getValue());
+    }
+
     private ParseResult parse(String formName) throws IOException {
         XFormParser parser = new XFormParser(new FileReader("resources/" + formName));
         final List<String> errorMessages = new ArrayList<>();
@@ -76,5 +148,21 @@ public class XFormParserTest {
             this.formDef = formDef;
             this.errorMessages = errorMessages;
         }
+    }
+
+    private TreeElement findDepthFirst(TreeElement parent, String name) {
+        int len = parent.getNumChildren();
+        for (int i = 0; i < len; ++i) {
+            TreeElement e = parent.getChildAt(i);
+            if (name.equals(e.getName())) {
+                return e;
+            } else if (e.getNumChildren() != 0) {
+                TreeElement v = findDepthFirst(e, name);
+                if (v != null) {
+                    return v;
+                }
+            }
+        }
+        return null;
     }
 }
