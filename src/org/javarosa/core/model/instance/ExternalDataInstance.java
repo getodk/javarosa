58999d95e14b4583756175d4894f2edad3cf6100
Dave Briccetti <daveb@davebsoft.com>
2017-08-19 20:09:14 -0700

    Don’t assume that any instance with a src attribute is an external data instance, unless the src location’s scheme is “jr” and its host is “file”.

--- src/org/javarosa/core/model/instance/ExternalDataInstance.java
+++ src/org/javarosa/core/model/instance/ExternalDataInstance.java
@@ -12,25 +12,22 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+// This is still a work in progress.
+
 public class ExternalDataInstance extends DataInstance {
     private TreeElement root;
 
-    public ExternalDataInstance(String srcLocation, String instanceId) {
+    // todo Make @mdudzinski’s recommended changes from https://github.com/opendatakit/javarosa/pull/154#pullrequestreview-51806826
+
+    public ExternalDataInstance(String path, String instanceId)
+        /* todo implement error handling */ throws IOException, UnfullfilledRequirementsException, XmlPullParserException, InvalidStructureException {
         super(instanceId);
         setName(instanceId);
-        try {
-            URI uri = new URI(srcLocation);
-            if ("jr".equals(uri.getScheme()) && "file".equals(uri.getHost())) {
-                String absolutePath = /* ToDo: find out how to get the actual location */
-                        System.getProperty("user.dir") + "/resources" + uri.getPath();
-                KXmlParser xmlParser = ElementParser.instantiateParser(new FileInputStream(absolutePath));
-                TreeElementParser treeElementParser = new TreeElementParser(xmlParser, 0, instanceId);
-                root = treeElementParser.parse();
-            }
-        } catch (XmlPullParserException | IOException | URISyntaxException | UnfullfilledRequirementsException |
-                InvalidStructureException e) {
-            e.printStackTrace(); // ToDo: decide what errors to report and how to report them
-        }
+        String absolutePath = /* ToDo: find out how to get the actual location */
+                System.getProperty("user.dir") + "/resources" + path;
+        KXmlParser xmlParser = ElementParser.instantiateParser(new FileInputStream(absolutePath));
+        TreeElementParser treeElementParser = new TreeElementParser(xmlParser, 0, instanceId);
+        root = treeElementParser.parse();
     }
 
     @Override
@@ -46,5 +43,25 @@ public class ExternalDataInstance extends DataInstance {
     @Override
     public void initialize(InstanceInitializationFactory initializer, String instanceId) {
         throw new RuntimeException("Not implemented");
+    }
+
+    /**
+     * Returns the path of the URI at srcLocation if the scheme is <code>jr</code> and the host is
+     * <code>file</code>, otherwise returns <code>null</code>.
+     * @param srcLocation the value of the <code>src</code> attribute of the <code>instance</code> element
+     * @throws URISyntaxException if srcLocation can’t be parsed as a URI
+     */
+    public static String getPathIfExternalDataInstance(String srcLocation) {
+        if (srcLocation != null && !srcLocation.isEmpty()) {
+            try {
+                URI uri = new URI(srcLocation);
+                if ("jr".equals(uri.getScheme()) && "file".equals(uri.getHost())) {
+                    return uri.getPath();
+                }
+            } catch (URISyntaxException e) {
+                e.printStackTrace(); // ToDo: decide what errors to report and how to report them
+            }
+        }
+        return null;
     }
 }
