c16c91e15fef2ce7eb9582cae20a2f9798d308f0
Dave Briccetti <daveb@davebsoft.com>
2017-09-27 23:34:20 -0700

    Abstract the TreeElement child processing, using an interface and an implementation that delegates to ArrayList. Let’s write a new implementation of that interface that has good performance.

---
+++ src/org/javarosa/core/model/instance/utils/TreeElementChildren.java
@@ -0,0 +1,28 @@
+package org.javarosa.core.model.instance.utils;
+
+import org.javarosa.core.model.instance.TreeElement;
+
+import java.util.Iterator;
+
+public interface TreeElementChildren extends Iterable<TreeElement> {
+    int size();
+
+    @Override
+    Iterator<TreeElement> iterator();
+
+    void add(TreeElement treeElement);
+
+    void add(int index, TreeElement treeElement);
+
+    TreeElement remove(int index);
+
+    boolean remove(TreeElement treeElement);
+
+    TreeElement get(int index);
+
+    void clear();
+
+    void addAll(TreeElementChildren children);
+
+    boolean isEmpty();
+}
---
+++ src/org/javarosa/core/model/instance/utils/TreeElementChildrenList.java
@@ -0,0 +1,61 @@
+package org.javarosa.core.model.instance.utils;
+
+import org.javarosa.core.model.instance.TreeElement;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class TreeElementChildrenList implements TreeElementChildren {
+    private List<TreeElement> children = new ArrayList<>();
+
+    @Override
+    public int size() {
+        return children.size();
+    }
+
+    @Override
+    public Iterator<TreeElement> iterator() {
+        return children.iterator();
+    }
+
+    @Override
+    public void add(TreeElement treeElement) {
+        children.add(treeElement);
+    }
+
+    @Override
+    public void add(int index, TreeElement treeElement) {
+        children.add(index, treeElement);
+    }
+
+    @Override
+    public TreeElement remove(int index) {
+        return children.remove(index);
+    }
+
+    @Override
+    public boolean remove(TreeElement treeElement) {
+        return children.remove(treeElement);
+    }
+
+    @Override
+    public TreeElement get(int index) {
+        return children.get(index);
+    }
+
+    @Override
+    public void clear() {
+        children.clear();
+    }
+
+    @Override
+    public void addAll(TreeElementChildren children) {
+        children.addAll(children);
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return children.isEmpty();
+    }
+}
