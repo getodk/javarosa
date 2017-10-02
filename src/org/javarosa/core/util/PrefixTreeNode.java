e82cd13945b5141d5a34146347e0e3373ad258d0
Alex Anderson <anderson@medicmobile.org>
2017-10-02 16:05:11 +0000

    Use StringBuilder instead of StringBuffer
    StringBuffer is synchronized, so for performance reasons StringBuilder should be
    used in preference unless thread safety is required.
    See https://docs.oracle.com/javase/7/docs/api/java/lang/StringBuffer.html

--- src/org/javarosa/core/util/PrefixTreeNode.java
+++ src/org/javarosa/core/util/PrefixTreeNode.java
@@ -58,7 +58,7 @@ public class PrefixTreeNode {
     }
 
     public String toString () {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
         sb.append("{");
         sb.append(prefix);
         if (terminal)
