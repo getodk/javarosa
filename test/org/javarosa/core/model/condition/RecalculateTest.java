d9e7b8efc7e243218f0112c8cbbf26b9bae75ec3
Guille <ggalmazor@gmail.com>
2018-03-06 10:32:25 +0100

    Write a test that shows how the time part is lost

---
+++ test/org/javarosa/core/model/condition/RecalculateTest.java
@@ -0,0 +1,31 @@
+package org.javarosa.core.model.condition;
+
+import static org.javarosa.test.utils.ResourcePathHelper.r;
+import static org.junit.Assert.assertEquals;
+
+import org.javarosa.core.model.FormDef;
+import org.javarosa.core.model.FormIndex;
+import org.javarosa.core.model.instance.InstanceInitializationFactory;
+import org.javarosa.core.test.FormParseInit;
+import org.javarosa.form.api.FormEntryPrompt;
+import org.junit.Before;
+import org.junit.Test;
+
+public class RecalculateTest {
+    private FormDef formDef;
+
+    @Before
+    public void setUp() {
+        FormParseInit fpi = new FormParseInit();
+        fpi.setFormToParse(r("calculate-now.xml").toString());
+        formDef = fpi.getFormDef();
+        formDef.initialize(true, new InstanceInitializationFactory());
+    }
+
+    @Test
+    public void testComputedConstraintText() {
+        FormIndex nowNoteField = new FormIndex(0, 0, formDef.getMainInstance().getRoot().getChild("now_note", 0).getRef());
+        String actualQuestionText = new FormEntryPrompt(formDef, nowNoteField).getQuestionText();
+        assertEquals("2018-01-01T10:20:30.400", actualQuestionText);
+    }
+}
