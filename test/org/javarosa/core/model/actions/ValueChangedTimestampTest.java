d0ee3e06ef8dc27b0dd601afd68b1624b47ceddd
Michal Dudzinski <mdudzinski@soldevelo.com>
2018-03-29 21:14:08 +0200

    Test case for serialization and deserialization

--- test/org/javarosa/core/model/actions/ValueChangedTimestampTest.java
+++ test/org/javarosa/core/model/actions/ValueChangedTimestampTest.java
@@ -1,5 +1,6 @@
 package org.javarosa.core.model.actions;
 
+import org.javarosa.core.model.CoreModelModule;
 import org.javarosa.core.model.FormDef;
 import org.javarosa.core.model.data.DateTimeData;
 import org.javarosa.core.model.data.DecimalData;
@@ -7,14 +8,23 @@ import org.javarosa.core.model.data.IAnswerData;
 import org.javarosa.core.model.instance.TreeReference;
 import org.javarosa.form.api.FormEntryController;
 import org.javarosa.form.api.FormEntryModel;
+import org.javarosa.core.services.PrototypeManager;
+import org.javarosa.core.util.JavaRosaCoreModule;
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.model.xform.XFormsModule;
 import org.joda.time.DateTimeUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Date;
 
+import static org.javarosa.core.util.externalizable.ExtUtil.defaultPrototypes;
 import static org.javarosa.test.utils.ResourcePathHelper.r;
 import static org.javarosa.xform.parse.FormParserHelper.parse;
 import static org.junit.Assert.assertEquals;
@@ -132,4 +142,45 @@ public class ValueChangedTimestampTest {
         assertEquals(expectedValue.getValue(), targetValue.getValue());
     }
 
+    @Test
+    public void testSerializationAndDeserialization() throws IOException, DeserializationException {
+        PrototypeManager.registerPrototypes(JavaRosaCoreModule.classNames);
+        PrototypeManager.registerPrototypes(CoreModelModule.classNames);
+        new XFormsModule().registerModule();
+
+        FormDef formDef = parse(r("nested-setvalue-action.xml")).formDef;
+        Path p = Files.createTempFile("serialized-form", null);
+
+        final DataOutputStream dos = new DataOutputStream(Files.newOutputStream(p));
+        formDef.writeExternal(dos);
+        dos.close();
+
+        final DataInputStream dis = new DataInputStream(Files.newInputStream(p));
+        formDef.readExternal(dis, defaultPrototypes());
+        dis.close();
+
+        Files.delete(p);
+
+        TreeReference triggerRef = new TreeReference();
+        triggerRef.setRefLevel(TreeReference.REF_ABSOLUTE);
+        triggerRef.add("data", 0);
+        triggerRef.add("cost", 0);
+
+        TreeReference targetRef = new TreeReference();
+        targetRef.setRefLevel(TreeReference.REF_ABSOLUTE);
+        targetRef.add("data", 0);
+        targetRef.add("cost_timestamp", 0);
+
+        IAnswerData targetValue = formDef.getMainInstance().resolveReference(targetRef).getValue();
+        assertNull(targetValue);
+
+        // When
+        formDef.setValue(new DecimalData(22.0), triggerRef, true);
+
+        // Then
+        targetValue = formDef.getMainInstance().resolveReference(targetRef).getValue();
+        IAnswerData expectedValue = new DateTimeData(new Date(DATE_NOW));
+
+        assertEquals(expectedValue.getValue(), targetValue.getValue());
+    }
 }
