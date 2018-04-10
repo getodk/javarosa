0250fab5486506e9ffe67facade057edcd8b0354
Guille <ggalmazor@gmail.com>
2018-04-10 08:48:19 +0200

    Add a new randomize() xform function and write a test for it

---
+++ src/org/javarosa/xpath/expr/XPathPathExprEval.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.javarosa.xpath.expr;
+
+import org.javarosa.core.model.condition.EvaluationContext;
+import org.javarosa.core.model.instance.DataInstance;
+import org.javarosa.core.model.instance.TreeReference;
+import org.javarosa.xpath.XPathException;
+import org.javarosa.xpath.XPathMissingInstanceException;
+import org.javarosa.xpath.XPathNodeset;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+/** The eval operation of XPathPathExpr */
+public class XPathPathExprEval {
+    private static final Logger logger = LoggerFactory.getLogger(XPathPathExprEval.class.getSimpleName());
+
+    public XPathNodeset eval(TreeReference reference, EvaluationContext ec) {
+        TreeReference ref = getContextualizedTreeReference(reference, ec);
+        logger.debug("eval called for {}. contextualized ref is {}.", reference, ref);
+        DataInstance dataInstance = getDataInstance(ec, ref);
+        List<TreeReference> nodesetRefs = ec.expandReference(ref);
+        removeIrrelevantNodesets(dataInstance, nodesetRefs);
+        return new XPathNodeset(nodesetRefs, dataInstance, ec);
+    }
+
+    /** Removes irrelevant nodesets, to fix conditions based on non-relevant data */
+    private void removeIrrelevantNodesets(DataInstance dataInstance, List<TreeReference> nodesetRefs) {
+        for (int i = 0; i < nodesetRefs.size(); i++) {
+            if (!dataInstance.resolveReference(nodesetRefs.get(i)).isRelevant()) {
+                nodesetRefs.remove(i);
+                i--;
+            }
+        }
+    }
+
+    private DataInstance getDataInstance(EvaluationContext ec, TreeReference ref) {
+        final DataInstance dataInstance;
+
+        if (refersToNonMainInstance(ref)) {
+            final DataInstance nonMainInstance = ec.getInstance(ref.getInstanceName());
+            logger.debug("ref refers to non-main instance {}", nonMainInstance);
+            if (nonMainInstance != null) {
+                dataInstance = nonMainInstance;
+            } else {
+                throw new XPathMissingInstanceException(ref.getInstanceName(),
+                    "Instance referenced by " + ref.toString(true) + " does not exist");
+            }
+        } else {
+            //TODO: We should really stop passing 'dataInstance' around and start just getting the right instance from ec at a more central level
+            dataInstance = ec.getMainInstance();
+
+            if (dataInstance == null) {
+                throw new XPathException("Cannot evaluate the reference [" + ref.toString(true) +
+                    "] in the current evaluation context. No default instance has been declared!");
+            }
+        }
+        // Regardless of the above, we want to ensure there is a definition
+        if (dataInstance.getRoot() == null) {
+            //This instance is _declared_, but doesn't actually have any data in it.
+            throw new XPathMissingInstanceException(ref.getInstanceName(),
+                "Instance referenced by " + ref.toString(true) + " has not been loaded");
+        }
+
+        return dataInstance;
+    }
+
+    private boolean refersToNonMainInstance(TreeReference ref) {
+        return ref.getInstanceName() != null && ref.isAbsolute();
+    }
+
+    private TreeReference getContextualizedTreeReference(TreeReference genericRef, EvaluationContext ec) {
+        // We don't necessarily know the model we want to be working with until we've contextualized the node
+        TreeReference contextRef = genericRef.getContext() == TreeReference.CONTEXT_ORIGINAL ?
+            ec.getOriginalContext() : ec.getContextRef();
+        return genericRef.contextualize(contextRef);
+    }
+}
