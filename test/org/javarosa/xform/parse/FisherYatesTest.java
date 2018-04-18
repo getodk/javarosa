573c97804708ed70cecd65b2afc723f6ed3aae83
Guille <ggalmazor@gmail.com>
2018-04-18 13:58:24 +0200

    Refactor shuffling code and add extensive JavaDoc - Changed Fisher-Yates shuffling to make it immutable for ease of use on caller sites.

--- test/org/javarosa/xform/parse/FisherYatesTest.java
+++ test/org/javarosa/xform/parse/FisherYatesTest.java
@@ -1,14 +1,12 @@
 package org.javarosa.xform.parse;
 
+import static java.util.Collections.emptyList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
-import java.util.Random;
 import org.junit.Test;
 
 public class FisherYatesTest {
@@ -16,37 +14,29 @@ public class FisherYatesTest {
 
     @Test
     public void it_produces_a_list_with_the_same_elements_in_different_order() {
-        List<Integer> output = new ArrayList<>(INPUT);
-        FisherYates.shuffle(output, new Random());
-        assertNotEquals(INPUT, output);
+        assertNotEquals(INPUT, FisherYates.shuffle(INPUT));
     }
 
     @Test
     public void shuffling_an_empty_list_has_no_effect() {
-        List<Object> emptyList = Collections.emptyList();
-        FisherYates.shuffle(emptyList, new Random());
-        assertTrue(emptyList.isEmpty());
+        assertTrue(FisherYates.shuffle(emptyList()).isEmpty());
     }
 
     @Test
     public void different_random_seeds_output_different_predictable_outputs() {
-        List<Integer> output1 = new ArrayList<>(INPUT);
-        FisherYates.shuffle(output1, new Random(42));
-        assertEquals(Arrays.asList(2, 3, 7, 4, 8, 5, 0, 9, 1, 6), output1);
+        List<Integer> output1 = FisherYates.shuffle(INPUT, 33);
+        assertEquals(Arrays.asList(3, 5, 4, 8, 2, 0, 1, 6, 9, 7), output1);
 
-        List<Integer> output2 = new ArrayList<>(INPUT);
-        FisherYates.shuffle(output2, new Random(33));
-        assertEquals(Arrays.asList(0, 8, 1, 6, 7, 2, 5, 4, 9, 3), output2);
+        List<Integer> output2 = FisherYates.shuffle(INPUT, 42);
+        assertEquals(Arrays.asList(0, 5, 9, 1, 8, 4, 6, 3, 7, 2), output2);
     }
 
     @Test
     public void same_random_seeds_output_same_predictable_outputs() {
-        List<Integer> output1 = new ArrayList<>(INPUT);
-        FisherYates.shuffle(output1, new Random(42));
-        assertEquals(Arrays.asList(2, 3, 7, 4, 8, 5, 0, 9, 1, 6), output1);
+        List<Integer> output1 = FisherYates.shuffle(INPUT, 42);
+        assertEquals(Arrays.asList(0, 5, 9, 1, 8, 4, 6, 3, 7, 2), output1);
 
-        List<Integer> output2 = new ArrayList<>(INPUT);
-        FisherYates.shuffle(output2, new Random(42));
-        assertEquals(Arrays.asList(2, 3, 7, 4, 8, 5, 0, 9, 1, 6), output2);
+        List<Integer> output2 = FisherYates.shuffle(INPUT, 42);
+        assertEquals(Arrays.asList(0, 5, 9, 1, 8, 4, 6, 3, 7, 2), output2);
     }
 }
