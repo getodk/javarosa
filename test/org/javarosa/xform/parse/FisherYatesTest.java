7438d404d5c90c2b7614d3b35518837b8cadf3f2
Guille <ggalmazor@gmail.com>
2018-04-25 09:46:14 +0200

    Refactor FisherYates tests to separate common behavior verifications from more automatizable, parameterized tests based on a table of examples of inputs and predictable results

--- test/org/javarosa/xform/parse/FisherYatesTest.java
+++ test/org/javarosa/xform/parse/FisherYatesTest.java
@@ -1,7 +1,7 @@
 package org.javarosa.xform.parse;
 
 import static java.util.Collections.emptyList;
-import static org.junit.Assert.assertEquals;
+import static org.javarosa.xform.parse.FisherYates.shuffle;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
@@ -10,33 +10,29 @@ import java.util.List;
 import org.junit.Test;
 
 public class FisherYatesTest {
-    private static final List<Integer> INPUT = Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+    private static final List<Integer> INPUT = list(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
 
     @Test
     public void it_produces_a_list_with_the_same_elements_in_different_order() {
-        assertNotEquals(INPUT, FisherYates.shuffle(INPUT));
+        assertNotEquals(INPUT, shuffle(INPUT));
     }
 
     @Test
     public void shuffling_an_empty_list_has_no_effect() {
-        assertTrue(FisherYates.shuffle(emptyList()).isEmpty());
+        assertTrue(shuffle(emptyList()).isEmpty());
     }
 
     @Test
-    public void different_random_seeds_output_different_predictable_outputs() {
-        List<Integer> output1 = FisherYates.shuffle(INPUT, 33);
-        assertEquals(Arrays.asList(3, 5, 4, 8, 2, 0, 1, 6, 9, 7), output1);
-
-        List<Integer> output2 = FisherYates.shuffle(INPUT, 42);
-        assertEquals(Arrays.asList(0, 5, 9, 1, 8, 4, 6, 3, 7, 2), output2);
+    public void different_random_seeds_output_different_outputs() {
+        assertNotEquals(shuffle(INPUT, 33), shuffle(INPUT, 42));
     }
 
-    @Test
-    public void same_random_seeds_output_same_predictable_outputs() {
-        List<Integer> output1 = FisherYates.shuffle(INPUT, 42);
-        assertEquals(Arrays.asList(0, 5, 9, 1, 8, 4, 6, 3, 7, 2), output1);
+    @SafeVarargs
+    static <T> List<T> list(T... values) {
+        return Arrays.asList(values);
+    }
 
-        List<Integer> output2 = FisherYates.shuffle(INPUT, 42);
-        assertEquals(Arrays.asList(0, 5, 9, 1, 8, 4, 6, 3, 7, 2), output2);
+    static List<String> list(String values) {
+        return Arrays.asList(values.split(""));
     }
 }
