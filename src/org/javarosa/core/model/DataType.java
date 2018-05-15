c13b3051c4583b0ece32a39069ae1d67e88a03af
Dave Briccetti <daveb@davebsoft.com>
2018-05-14 21:22:58 -0700

    Improve DATATYPE constants. Remove the doc comments from the DATATYPE constants. They are redundant, as Guillermo pointed out. Inline the DATATYPE values (using IDEA’s refactoring), to remove dependency on Constants class (which eventually will have the DATATYPE constants removed). Move code to return a class for a data type to a new class, DataTypeClasses. Change to use DataType enum where it can be done safely and there is some test coverage.

--- src/org/javarosa/core/model/DataType.java
+++ src/org/javarosa/core/model/DataType.java
@@ -16,45 +16,27 @@
 
 package org.javarosa.core.model;
 
-import static org.javarosa.core.model.Constants.DATATYPE_BARCODE;
-import static org.javarosa.core.model.Constants.DATATYPE_BINARY;
-import static org.javarosa.core.model.Constants.DATATYPE_BOOLEAN;
-import static org.javarosa.core.model.Constants.DATATYPE_CHOICE;
-import static org.javarosa.core.model.Constants.DATATYPE_MULTIPLE_ITEMS;
-import static org.javarosa.core.model.Constants.DATATYPE_DATE;
-import static org.javarosa.core.model.Constants.DATATYPE_DATE_TIME;
-import static org.javarosa.core.model.Constants.DATATYPE_DECIMAL;
-import static org.javarosa.core.model.Constants.DATATYPE_GEOPOINT;
-import static org.javarosa.core.model.Constants.DATATYPE_GEOSHAPE;
-import static org.javarosa.core.model.Constants.DATATYPE_GEOTRACE;
-import static org.javarosa.core.model.Constants.DATATYPE_INTEGER;
-import static org.javarosa.core.model.Constants.DATATYPE_LONG;
-import static org.javarosa.core.model.Constants.DATATYPE_NULL;
-import static org.javarosa.core.model.Constants.DATATYPE_TEXT;
-import static org.javarosa.core.model.Constants.DATATYPE_TIME;
-import static org.javarosa.core.model.Constants.DATATYPE_UNSUPPORTED;
-
 /**
  * The model data types.
  */
 public enum DataType {
-    UNSUPPORTED     (DATATYPE_UNSUPPORTED),
-    NULL            (DATATYPE_NULL),
-    TEXT            (DATATYPE_TEXT),
-    INTEGER         (DATATYPE_INTEGER),
-    DECIMAL         (DATATYPE_DECIMAL),
-    DATE            (DATATYPE_DATE),
-    TIME            (DATATYPE_TIME),
-    DATE_TIME       (DATATYPE_DATE_TIME),
-    CHOICE          (DATATYPE_CHOICE),
-    MULTIPLE_ITEMS  (DATATYPE_MULTIPLE_ITEMS),
-    BOOLEAN         (DATATYPE_BOOLEAN),
-    GEOPOINT        (DATATYPE_GEOPOINT),
-    BARCODE         (DATATYPE_BARCODE),
-    BINARY          (DATATYPE_BINARY),
-    LONG            (DATATYPE_LONG),
-    GEOSHAPE        (DATATYPE_GEOSHAPE),
-    GEOTRACE        (DATATYPE_GEOTRACE);
+    UNSUPPORTED     (-1),
+    NULL            (0),
+    TEXT            (1),
+    INTEGER         (2),
+    DECIMAL         (3),
+    DATE            (4),
+    TIME            (5),
+    DATE_TIME       (6),
+    CHOICE          (7),
+    MULTIPLE_ITEMS  (8),
+    BOOLEAN         (9),
+    GEOPOINT        (10),
+    BARCODE         (11),
+    BINARY          (12),
+    LONG            (13),
+    GEOSHAPE        (14),
+    GEOTRACE        (15);
 
     public final int value;
 
