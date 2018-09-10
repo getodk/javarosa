9193bcd6bbaf2c701aba16b355be0490e3796ec3
Dave Briccetti <daveb@davebsoft.com>
2018-09-10 13:06:37 -0700

    Since we no longer throw an exception when encountering invalid data, remove the tests that were checking for it

--- test/org/javarosa/core/util/GeoInvalidDataTest.java
+++ test/org/javarosa/core/util/GeoInvalidDataTest.java
@@ -1,36 +1,31 @@
 package org.javarosa.core.util;
 
+import org.javarosa.core.model.FormDef;
+import org.javarosa.core.model.data.IAnswerData;
 import org.javarosa.core.model.instance.InstanceInitializationFactory;
-import org.javarosa.xpath.XPathTypeMismatchException;
+import org.javarosa.core.model.instance.TreeElement;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
-
-import java.io.IOException;
 
 import static org.javarosa.test.utils.ResourcePathHelper.r;
 import static org.javarosa.xform.parse.FormParserHelper.parse;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(Parameterized.class)
 public class GeoInvalidDataTest {
-    @Parameter public String formName;
-
-    @Parameters(name = "{0}") public static Object[] data() {
-        return "invalid-geoshape invalid-geotrace invalid-points distance_with_less_than_two_points".split(" ");
-    }
-
     @Test
-    public void invalidGeoDataCausesException() throws IOException {
-        try {
-            parse(r(formName + ".xml")).formDef.initialize(
-                true, new InstanceInitializationFactory());
-            fail("Expected XPathTypeMismatchException not thrown");
-        } catch (RuntimeException e) {
-            assertTrue(e.getCause() instanceof XPathTypeMismatchException);
-        }
-    }
+     public void testDistanceWithLessThanTwoPoints() throws Exception {
+         // Read the form definition
+         final FormDef formDef = parse(r("distance_with_less_than_two_points.xml")).formDef;
+
+         // Trigger all calculations
+         formDef.initialize(true, new InstanceInitializationFactory());
+
+         // Check the results. The data and expected results come from GeoUtilsTest.
+         TreeElement root = formDef.getMainInstance().getRoot();
+
+         IAnswerData area = root.getChildAt(2).getValue();
+         assertEquals(0, area.getValue());
+
+         area = root.getChildAt(3).getValue();
+         assertEquals(0, area.getValue());
+     }
 }
