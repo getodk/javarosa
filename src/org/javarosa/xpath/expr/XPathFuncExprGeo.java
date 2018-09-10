bfa508a7f4df5b70b70b6effb0fdb8f44be21909
Dave Briccetti <daveb@davebsoft.com>
2018-09-09 20:19:16 -0700

    In XPathFuncExprGeo.getGpsCoordinatesFromNodeset, recognize when invalid geo data has not been parsed (it appears here as an empty string, having been set to null earlier), and log a message and throw an XPathTypeMismatchException

--- src/org/javarosa/xpath/expr/XPathFuncExprGeo.java
+++ src/org/javarosa/xpath/expr/XPathFuncExprGeo.java
@@ -7,12 +7,19 @@ import org.javarosa.core.util.GeoUtils;
 import org.javarosa.xpath.XPathNodeset;
 import org.javarosa.xpath.XPathTypeMismatchException;
 import org.javarosa.xpath.XPathUnhandledException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 /** XPath function expression geographic logic */
 class XPathFuncExprGeo {
+    private static final Logger logger = LoggerFactory.getLogger(XPathFuncExprGeo.class.getSimpleName());
+
+    /** How XPathPathExpr.getRefValue returns unparsable, therefore null, geo data */
+    private static final String UNPARSABLE_GEO_DATA = "";
+
     List<GeoUtils.LatLong> getGpsCoordinatesFromNodeset(String name, Object argVal) {
         if (!(argVal instanceof XPathNodeset)) {
             throw new XPathUnhandledException("function \'" + name + "\' requires a field as the parameter.");
@@ -23,27 +30,37 @@ class XPathFuncExprGeo {
         final List<GeoUtils.LatLong> latLongs = new ArrayList<>();
 
         if (repeatSize == 1) {
+            Object arg = argList[0];
+            if (arg.equals(UNPARSABLE_GEO_DATA)) throwMismatch(name);
+
             // Try to determine if the argument is of type GeoShapeData
             try {
-                GeoShapeData geoShapeData = new GeoShapeData().cast(new UncastData(XPathFuncExpr.toString(argList[0])));
+                GeoShapeData geoShapeData = new GeoShapeData().cast(new UncastData(XPathFuncExpr.toString(arg)));
                 for (GeoPointData point : geoShapeData.points) {
                     latLongs.add(new GeoUtils.LatLong(point.getPart(0), point.getPart(1)));
                 }
             } catch (Exception e) {
-                throw new XPathTypeMismatchException("The function \'" + name + "\' received a value that does not represent GPS coordinates: " + argList[0]);
+                throwMismatch(name);
             }
         } else if (repeatSize >= 2) {
             // treat the input as a series of GeoPointData
 
             for (Object arg : argList) {
+                if (arg.equals(UNPARSABLE_GEO_DATA)) throwMismatch(name);
                 try {
                     GeoPointData geoPointData = new GeoPointData().cast(new UncastData(XPathFuncExpr.toString(arg)));
                     latLongs.add(new GeoUtils.LatLong(geoPointData.getPart(0), geoPointData.getPart(1)));
                 } catch (Exception e) {
-                    throw new XPathTypeMismatchException("The function \'" + name + "\' received a value that does not represent GPS coordinates: " + arg);
+                    throwMismatch(name);
                 }
             }
         }
         return latLongs;
+    }
+
+    private void throwMismatch(String name) {
+        String msg = "The function '" + name + "' received a value that does not represent GPS coordinates";
+        logger.warn(msg);
+        throw new XPathTypeMismatchException(msg);
     }
 }
