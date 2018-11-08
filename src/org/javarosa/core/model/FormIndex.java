32532101452ad47c6e32de1de7a13ab81e005125
lognaturel <lognaturel@gmail.com>
2018-11-07 20:45:57 -0800

    Describe FormIndex and provide examples

--- src/org/javarosa/core/model/FormIndex.java
+++ src/org/javarosa/core/model/FormIndex.java
@@ -20,18 +20,68 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.javarosa.core.model.instance.FormInstance;
+import org.javarosa.core.model.instance.TreeElement;
 import org.javarosa.core.model.instance.TreeReference;
 
 /**
- * A Form Index is an immutable index into a specific question definition that
- * will appear in an interaction with a user.
+ * {@code FormIndex} is an immutable index which is structured to provide quick access to a specific node in a
+ * {@link FormDef} (tree of {@link IFormElement} representing a blank form) or a {@link FormInstance}
+ * (tree of {@link TreeElement} representing a form instance that can be filled). It also includes a
+ * {@link TreeReference} representing the XPath path of that node.
  *
- * An index is represented by different levels into hierarchical groups.
+ * The XML node referred to by a FormIndex can correspond to either a question or a group. The index is constructed as a
+ * linked list of levels.
  *
- * Indices can represent both questions and groups.
+ * A FormIndex can refer to:
+ * - a node at the top level of the form (only {@link #localIndex} is used)
+ * - a node nested in a group hierarchy ({@link #localIndex} and {@link #nextLevel} are used)
+ * - a node nested in a repeat ({@link #localIndex}, {@link #nextLevel} and {@link #instanceIndex} are all used)
  *
- * It is absolutely essential that there be no circularity of reference in
- * FormIndex's, IE, no form index's ancestor can be itself.
+ * When a FormIndex list refers to a nested node, that root FormIndex represents the path root and links to subsequent
+ * levels using the {@link #nextLevel} field. The @{link #getReference} method returns the reference of the FormIndex at
+ * the end of the list.
+ *
+ * Consider the following instance from a blank form with {@code friends} as a repeat group and {@code school_info} as a
+ * non-repeat group:
+ *
+ * <pre>{@code
+ *  <instance>
+ *    <data id="my_form">
+ *      <address/>
+ *      <dob/>
+ *      <school_info>
+ *        <favorite_class/>
+ *        <favorite_teacher/>
+ *      </school_info>
+ *      <friends>
+ *        <firstname/>
+ *      </friends>
+ *    </data>
+ *  </instance>
+ * </pre>}
+ *
+ * The {@code dob} question's path is {@code /data/dob}. The index that refers to it has a {@code localIndex} of 1.
+ *
+ * The {@code friends} group's path is {@code /data/friends}. The index that refers to it has a {@code localIndex} of 3.
+ *
+ * The {@code favorite_teacher} question's path is {@code /data/school_info/favorite_teacher}. The index that refers to
+ * it has a {@code localIndex} of 2 which is {@code school_info}'s index at the form root. It also has a
+ * {@code nextLevel} set which itself has a {@code localIndex} of 1 because it is the second node in the school_info group.
+ *
+ * The {@code firstname} question's path is {@code /data/friends[N]/firstname} where @{N} is a 0-based index representing
+ * which of potentially several {@code friends} repeat group instances' {@code firstname} is desired (also referred to as
+ * multiplicity). Since {@code friends} is a repeat group, the filled instance can contain several instances of the
+ * {@code friends} node. The index that refers to it always starts with a {@code localIndex} of 3 which is
+ * {@code friends}'s index at the form root. It also has an {@code instanceIndex} which represents which of the
+ * potentially several {@code friends} to use. To index a {@code firstname} node, a {@code nextLevel} is added which
+ * always has a @{code localIndex} of 0.
+ *
+ * To go from a {@code FormIndex} to a form element in a blank form, {@link FormDef#getChild(FormIndex)} can be used.
+ * To go from a {@code FormIndex} to a node in a filled form, {@link FormInstance#resolveReference(TreeReference)} can
+ * be used along with {@link #getReference()}.
+ *
+ * No circularity is allowed. That is, no {@code FormIndex}'s ancestor can be itself.
  *
  * Datatype Productions:
  * FormIndex = BOF | EOF | CompoundIndex(nextIndex:FormIndex,Location)
@@ -46,20 +96,29 @@ public class FormIndex implements Serializable {
 
     private boolean endOfForm = false;
 
-    /** The index of the questiondef in the current context */
+    /**
+     * The 0-based index of the group or question in its parent.
+     */
     private int localIndex;
 
-    /** The multiplicity of the current instance of a repeated question or group */
+    /**
+     * The 0-based index of the current instance of a repeated node.
+     */
     private int instanceIndex = -1;
 
-    /** The next level of this index */
+    /**
+     * The next level of this index. A {@code FormIndex} chain starts at the root and is linked to deeper nodes in the
+     * tree it indexes into.
+     */
     private FormIndex nextLevel;
 
+    /**
+     * The XPath reference this index refers to.
+     */
     private TreeReference reference;
 
     /**
-     *
-     * @return an index before the start of the form
+     * Returns an index before the start of the form
      */
     public static FormIndex createBeginningOfFormIndex() {
         FormIndex begin = new FormIndex(-1, null);
@@ -68,8 +127,7 @@ public class FormIndex implements Serializable {
     }
 
     /**
-     *
-     * @return an index after the end of the form
+     * Returns an index after the end of the form
      */
     public static FormIndex createEndOfFormIndex() {
         FormIndex end = new FormIndex(-1,null);
@@ -78,10 +136,10 @@ public class FormIndex implements Serializable {
     }
 
     /**
-     * Constructs a simple form index that references a specific element in
-     * a list of elements.
+     * Constructs a @{code FormIndex} for a node that does not have any children.
+     *
      * @param localIndex An integer index into a flat list of elements
-     * @param reference A reference to the instance element identified by this index;
+     * @param reference A reference to the instance element identified by this index
      */
     public FormIndex(int localIndex, TreeReference reference) {
         this.localIndex = localIndex;
@@ -89,32 +147,46 @@ public class FormIndex implements Serializable {
 
     }
     /**
-     * Constructs a simple form index that references a specific element in
-     * a list of elements.
+     * Constructs a @{code FormIndex} for a node that does not have any children but that may have repeat instances in a
+     * filled form. The {@code instanceIndex} identifies which of these instances should be referred to.
+     *
      * @param localIndex An integer index into a flat list of elements
-     * @param instanceIndex An integer index expressing the multiplicity
-     * of the current level
-     * @param reference A reference to the instance element identified by this index;
+     * @param instanceIndex An integer index expressing the multiplicity of the current level
+     * @param reference A reference to the instance element identified by this index
      *
      */
-    public FormIndex(int localIndex, int instanceIndex,TreeReference reference) {
+    public FormIndex(int localIndex, int instanceIndex, TreeReference reference) {
         this.localIndex = localIndex;
         this.instanceIndex = instanceIndex;
         this.reference = reference;
     }
 
     /**
-     * Constructs an index which indexes an element, and provides an index
-     * into that elements children
+     * Constructs a @{code FormIndex} for a node that has children.
      *
      * @param nextLevel An index into the referenced element's index
      * @param localIndex An index to an element at the current level, a child
      * element of which will be referenced by the nextLevel index.
      * @param reference A reference to the instance element identified by this index;
      */
-    public FormIndex(FormIndex nextLevel, int localIndex,TreeReference reference) {
+    public FormIndex(FormIndex nextLevel, int localIndex, TreeReference reference) {
         this(localIndex, reference);
         this.nextLevel = nextLevel;
+    }
+
+    /**
+     * Constructs a @{code FormIndex} for a node that has children and that may have repeat instances in a filled form.
+     *
+     * @param nextLevel An index into the referenced element's index
+     * @param localIndex An index to an element at the current level, a child
+     * element of which will be referenced by the nextLevel index.
+     * @param instanceIndex How many times the element referenced has been
+     * repeated.
+     * @param reference A reference to the instance element identified by this index;
+     */
+    public FormIndex(FormIndex nextLevel, int localIndex, int instanceIndex, TreeReference reference) {
+        this(nextLevel, localIndex, reference);
+        this.instanceIndex = instanceIndex;
     }
 
     /**
@@ -135,23 +207,6 @@ public class FormIndex implements Serializable {
             this.instanceIndex = currentLevel.getInstanceIndex();
             this.reference = currentLevel.reference;
         }
-    }
-
-    /**
-     * Constructs an index which indexes an element, and provides an index
-     * into that elements children, along with the current index of a
-     * repeated instance.
-     *
-     * @param nextLevel An index into the referenced element's index
-     * @param localIndex An index to an element at the current level, a child
-     * element of which will be referenced by the nextLevel index.
-     * @param instanceIndex How many times the element referenced has been
-     * repeated.
-     * @param reference A reference to the instance element identified by this index;
-     */
-    public FormIndex(FormIndex nextLevel, int localIndex, int instanceIndex, TreeReference reference) {
-        this(nextLevel, localIndex, reference);
-        this.instanceIndex = instanceIndex;
     }
 
     /**
