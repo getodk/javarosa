2104fa5f1f074e71a0bb9880b69cffd27fa61953
Grzegorz Orczykowski <gorczykowski@soldevelo.com>
2019-03-26 14:36:37 +0100

    Read/write range parameters (#411)

--- src/org/javarosa/core/model/RangeQuestion.java
+++ src/org/javarosa/core/model/RangeQuestion.java
@@ -1,5 +1,14 @@
 package org.javarosa.core.model;
 
+import org.javarosa.core.util.externalizable.DeserializationException;
+import org.javarosa.core.util.externalizable.ExtUtil;
+import org.javarosa.core.util.externalizable.ExtWrapNullable;
+import org.javarosa.core.util.externalizable.PrototypeFactory;
+import org.javarosa.xform.parse.RangeParser;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import java.math.BigDecimal;
 
 /** A Range-type question, with information pulled from attributes of the range form element */
@@ -31,5 +40,21 @@ public class RangeQuestion extends QuestionDef {
 
     public BigDecimal getRangeStep() {
         return rangeStep;
+    }
+
+    @Override
+    public void readExternal(DataInputStream dis, PrototypeFactory pf) throws IOException, DeserializationException {
+        super.readExternal(dis, pf);
+        rangeStart = RangeParser.getDecimalValue((String) ExtUtil.read(dis, new ExtWrapNullable(String.class), pf));
+        rangeEnd = RangeParser.getDecimalValue((String) ExtUtil.read(dis, new ExtWrapNullable(String.class), pf));
+        rangeStep = RangeParser.getDecimalValue((String) ExtUtil.read(dis, new ExtWrapNullable(String.class), pf));
+    }
+
+    @Override
+    public void writeExternal(DataOutputStream dos) throws IOException {
+        super.writeExternal(dos);
+        ExtUtil.write(dos, new ExtWrapNullable(rangeStart != null ? rangeStart.toString() : null));
+        ExtUtil.write(dos, new ExtWrapNullable(rangeEnd != null ? rangeEnd.toString() : null));
+        ExtUtil.write(dos, new ExtWrapNullable(rangeStep != null ? rangeStep.toString() : null));
     }
 }
