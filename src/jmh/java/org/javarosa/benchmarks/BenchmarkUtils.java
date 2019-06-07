853f2147e4fdad809650334d14fab10850fe58d8
JohnTheBeloved <topzy20@gmail.com>
2019-06-07 10:16:08 +0100

    Added Benchmarks for XFormPaser parsing jmh/resources/nigeria_wards*.xml artifacts

--- src/jmh/java/org/javarosa/benchmarks/BenchmarkUtils.java
+++ src/jmh/java/org/javarosa/benchmarks/BenchmarkUtils.java
@@ -1,6 +1,7 @@
 package org.javarosa.benchmarks;
 
 import static org.javarosa.test.utils.ResourcePathHelper.r;
+import static org.javarosa.core.reference.ReferenceManagerTestUtils.setUpSimpleReferenceManager;
 
 import java.io.IOException;
 import java.lang.annotation.Annotation;
@@ -129,4 +130,37 @@ public class BenchmarkUtils {
                 return new StringData("");
         }
     }
+
+    public static Path getNigeriaWardsXMLWithInternal2ndryInstance(){
+        Path assetsPath = prepareAssets("nigeria_wards_internal_2ndry_instance.xml");
+        Path filePath = assetsPath.resolve("nigeria_wards_internal_2ndry_instance.xml");
+        return filePath;
+    }
+
+    public static Path getMinifiedNigeriaWardsXMLWithInternal2ndryInstance(){
+        Path assetsPath = prepareAssets("nigeria_wards_internal_2ndry_instance_minified.xml");
+        Path filePath = assetsPath.resolve("nigeria_wards_internal_2ndry_instance_minified.xml");
+        return filePath;
+    }
+
+    public static Path getNigeriaWardsXMLWithExternal2ndryInstance(){
+        Path assetsPath = prepareAssets("nigeria_wards_external_2ndry_instance.xml", "lgas.xml", "wards.xml");
+        setUpSimpleReferenceManager("file", assetsPath);
+        Path filePath = assetsPath.resolve("nigeria_wards_external_2ndry_instance.xml");
+        return filePath;
+    }
+
+    public static Path getWardsExternalInstance(){
+        Path assetsPath = prepareAssets( "wards.xml");
+        setUpSimpleReferenceManager("file", assetsPath);
+        Path filePath = assetsPath.resolve("wards.xml");
+        return filePath;
+    }
+
+    public static Path getLGAsExternalInstance(){
+        Path assetsPath = prepareAssets( "lgas.xml");
+        Path filePath = assetsPath.resolve("lgas.xml");
+        return filePath;
+    }
+
 }
---
+++ src/jmh/java/org/javarosa/benchmarks/XFormParserBenchmark.java
@@ -0,0 +1,109 @@
+package org.javarosa.benchmarks;
+
+import org.javarosa.core.reference.InvalidReferenceException;
+import org.javarosa.xform.parse.XFormParser;
+import org.javarosa.xml.util.InvalidStructureException;
+import org.javarosa.xml.util.UnfullfilledRequirementsException;
+import org.kxml2.kdom.Document;
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.Level;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Path;
+
+import static org.javarosa.benchmarks.BenchmarkUtils.dryRun;
+
+public class XFormParserBenchmark {
+    public static void main(String[] args) {
+        dryRun(XFormParserBenchmark.class);
+    }
+
+    @State(Scope.Thread)
+    public static class XFormParserState {
+        Path xFormInternalSecondaryInstancesMinified;
+        Path xFormInternalSecondaryInstances;
+        Path xFormExternalSecondaryInstances;
+        Path lgaSecondaryInstance;
+        Path wardExternalSecondaryInstance;
+
+        @Setup(Level.Trial)
+        public void
+        initialize() throws FileNotFoundException {
+            xFormInternalSecondaryInstancesMinified = BenchmarkUtils.getMinifiedNigeriaWardsXMLWithInternal2ndryInstance();
+            xFormInternalSecondaryInstances = BenchmarkUtils.getNigeriaWardsXMLWithInternal2ndryInstance();
+            xFormExternalSecondaryInstances = BenchmarkUtils.getNigeriaWardsXMLWithExternal2ndryInstance();
+            lgaSecondaryInstance = BenchmarkUtils.getLGAsExternalInstance();
+            wardExternalSecondaryInstance = BenchmarkUtils.getWardsExternalInstance();
+        }
+    }
+
+    @Benchmark
+    public void
+    benchmarkParseExternalInstanceXFormOnly(XFormParserState state, Blackhole bh)
+        throws IOException, XmlPullParserException, InvalidReferenceException,
+        UnfullfilledRequirementsException, InvalidStructureException {
+        Reader reader = new FileReader(state.xFormInternalSecondaryInstancesMinified.toFile());
+        Document kxmlDocument = XFormParser.getXMLDocument(reader);
+        bh.consume(kxmlDocument);
+    }
+
+    @Benchmark
+    public void
+    benchmarkParseInternalInstanceXForm(XFormParserState state, Blackhole bh)
+        throws IOException, XmlPullParserException, InvalidReferenceException,
+        UnfullfilledRequirementsException, InvalidStructureException {
+        Reader reader = new FileReader(state.xFormInternalSecondaryInstances.toFile());
+        Document kxmlDocument = XFormParser.getXMLDocument(reader);
+        bh.consume(kxmlDocument);
+    }
+
+
+    @Benchmark
+    public void
+    benchmarkParseExternalInstanceLGA(XFormParserState state, Blackhole bh)
+        throws IOException, XmlPullParserException, InvalidReferenceException,
+        UnfullfilledRequirementsException, InvalidStructureException {
+        Reader reader = new FileReader(state.lgaSecondaryInstance.toFile());
+        Document kxmlDocument = XFormParser.getXMLDocument(reader);
+        bh.consume(kxmlDocument);
+    }
+
+    @Benchmark
+    public void
+    benchmarkParseExternalInstanceWards(XFormParserState state, Blackhole bh)
+        throws IOException, XmlPullParserException, InvalidReferenceException,
+        UnfullfilledRequirementsException, InvalidStructureException {
+        Reader reader = new FileReader(state.wardExternalSecondaryInstance.toFile());
+        Document kxmlDocument = XFormParser.getXMLDocument(reader);
+        bh.consume(kxmlDocument);
+    }
+
+    @Benchmark
+    public void
+    benchmarkParseExternalInstanceXFormWithInstanceFiles(XFormParserState state, Blackhole bh)
+        throws IOException, XmlPullParserException, InvalidReferenceException,
+        UnfullfilledRequirementsException, InvalidStructureException {
+
+        Reader externalInstanceXFormReader = new FileReader(state.xFormExternalSecondaryInstances.toFile());
+        Reader lgaSecondaryInstanceReader = new FileReader(state.lgaSecondaryInstance.toFile());
+        Reader wardsSecondaryInstanceReader = new FileReader(state.wardExternalSecondaryInstance.toFile());
+        Document externalXFormInstanceDocument = XFormParser.getXMLDocument(externalInstanceXFormReader);
+        Document lgaExternalInstanceDocument = XFormParser.getXMLDocument(lgaSecondaryInstanceReader);
+        Document wardExternalInstanceDocument = XFormParser.getXMLDocument(wardsSecondaryInstanceReader);
+
+        bh.consume(externalXFormInstanceDocument);
+        bh.consume(lgaExternalInstanceDocument);
+        bh.consume(wardExternalInstanceDocument);
+
+    }
+ 
+
+}
