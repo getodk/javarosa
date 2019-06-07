bd769030dd5b59dedba4e99358f2577c5073326b
JohnTheBeloved <topzy20@gmail.com>
2019-06-07 22:07:32 +0100

    tests&benchmarks: Added Tests and Benchmarks for KXmlParser - parsing efficiency improved 40%

--- src/jmh/java/org/javarosa/benchmarks/BenchmarkUtils.java
+++ src/jmh/java/org/javarosa/benchmarks/BenchmarkUtils.java
@@ -1,5 +1,6 @@
 package org.javarosa.benchmarks;
 
+import static org.javarosa.core.reference.ReferenceManagerTestUtils.setUpSimpleReferenceManager;
 import static org.javarosa.test.utils.ResourcePathHelper.r;
 
 import java.io.IOException;
@@ -129,4 +130,38 @@ public class BenchmarkUtils {
                 return new StringData("");
         }
     }
+
+
+    public static Path getNigeriaWardsXMLWithInternal2ndryInstance(){
+        Path assetsPath = prepareAssets("nigeria_wards_internal_2ndry_instance.xml");
+        Path filePath = assetsPath.resolve("nigeria_wards_internal_2ndry_instance.xml");
+        return filePath;
+    }
+
+    public static Path getMinifiedNigeriaWardsXMLWithInternal2ndryInstance(){
+        Path assetsPath = prepareAssets("nigeria_wards_internal_2ndry_instance_minified.xml");
+        Path filePath = assetsPath.resolve("nigeria_wards_internal_2ndry_instance_minified.xml");
+        return filePath;
+    }
+
+    public static Path getNigeriaWardsXMLWithExternal2ndryInstance(){
+        Path assetsPath = prepareAssets("nigeria_wards_external_2ndry_instance.xml", "lgas.xml", "wards.xml");
+        setUpSimpleReferenceManager("file", assetsPath);
+        Path filePath = assetsPath.resolve("nigeria_wards_external_2ndry_instance.xml");
+        return filePath;
+    }
+
+    public static Path getWardsExternalInstance(){
+        Path assetsPath = prepareAssets( "wards.xml");
+        setUpSimpleReferenceManager("file", assetsPath);
+        Path filePath = assetsPath.resolve("wards.xml");
+        return filePath;
+    }
+
+    public static Path getLGAsExternalInstance(){
+        Path assetsPath = prepareAssets( "lgas.xml");
+        Path filePath = assetsPath.resolve("lgas.xml");
+        return filePath;
+    }
+
 }
---
+++ src/test/java/org/javarosa/xml/KXmlElementParserTest.java
@@ -0,0 +1,104 @@
+package org.javarosa.xml;
+
+import org.junit.Test;
+import org.kxml2.io.KXmlParser;
+import org.kxml2.kdom.Document;
+import org.kxml2.kdom.Element;
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.*;
+import java.nio.file.Path;
+import static org.junit.Assert.assertEquals;
+
+import static org.javarosa.test.utils.ResourcePathHelper.r;
+import static org.junit.Assert.assertNotNull;
+
+public class KXmlElementParserTest {
+
+    @Test
+    public void testParseXForm() throws IOException, XmlPullParserException {
+        Path xFormFilePath = r("simple-form.xml");
+        Document document;
+        KXmlElementParser kxmlElementParser ;
+
+        InputStream inputStream = new FileInputStream(xFormFilePath.toString());
+        KXmlParser kXmlParser = KXmlElementParser.instantiateParser(inputStream);
+        kxmlElementParser = new KXmlElementParser(kXmlParser);
+        document = kxmlElementParser.parseDoc();
+
+
+        assertNotNull(document);
+        Element htmlElement = document.getRootElement();
+        //HTML
+        assertEquals( "html", htmlElement.getName());
+        assertEquals(2, htmlElement.getChildCount());
+        //HEAD
+        Element headElement = htmlElement.getElement(0);
+        assertEquals( "head", headElement.getName());
+        assertEquals("http://www.w3.org/1999/xhtml", headElement.getNamespace());
+        //TITLE
+        Element titleElement = headElement.getElement(0);
+        assertEquals(2, headElement.getChildCount());
+        assertEquals("Simple Form", titleElement.getText(0));
+        //MODEL
+        Element modelElement = headElement.getElement(1);
+        assertEquals(3, modelElement.getChildCount());
+        //MAIN INSTANCE
+        Element mainInstanceElement = modelElement.getElement(0);
+        assertEquals(null, mainInstanceElement.getAttributeValue(null, "id"));
+        //FIRST SECONDARY INSTANCE
+        Element firstSecondaryInstanceElement = modelElement.getElement(1);
+        assertEquals(1, firstSecondaryInstanceElement.getChildCount());
+        assertEquals("options1", firstSecondaryInstanceElement.getAttributeValue(null, "id"));
+        Element rootElement = firstSecondaryInstanceElement.getElement(0);
+        assertEquals("root", rootElement.getName());
+        assertEquals(5, rootElement.getChildCount());
+        //BIND ELEMENT
+        Element bindElement = modelElement.getElement(2);
+        assertEquals(4, bindElement.getAttributeCount());
+
+    }
+
+
+    @Test
+    public void testParseXFormSkipInternalInstances() throws IOException, XmlPullParserException {
+        ElementSkipper elementToSkip = new ElementSkipper("instance", 1);
+        Path xFormFilePath = r("simple-form.xml");
+        Document document;
+        KXmlElementParser kxmlElementParser ;
+
+        InputStream inputStream = new FileInputStream(xFormFilePath.toString());
+        KXmlParser kXmlParser = KXmlElementParser.instantiateParser(inputStream);
+        kxmlElementParser = new KXmlElementParser(kXmlParser, elementToSkip);
+        document = kxmlElementParser.parseDoc();
+
+        assertNotNull(document);
+        Element htmlElement = document.getRootElement();
+        //HTML
+        assertEquals( "html", htmlElement.getName());
+        assertEquals(2, htmlElement.getChildCount());
+        //HEAD
+        Element headElement = htmlElement.getElement(0);
+        assertEquals( "head", headElement.getName());
+        assertEquals("http://www.w3.org/1999/xhtml", headElement.getNamespace());
+        //TITLE
+        Element titleElement = headElement.getElement(0);
+        assertEquals(2, headElement.getChildCount());
+        assertEquals("Simple Form", titleElement.getText(0));
+        //MODEL
+        Element modelElement = headElement.getElement(1);
+        assertEquals(3, modelElement.getChildCount());
+        //MAIN INSTANCE
+        Element mainInstanceElement = modelElement.getElement(0);
+        assertEquals(null, mainInstanceElement.getAttributeValue(null, "id"));
+        //FIRST SECONDARY INSTANCE
+        Element firstSecondaryInstanceElement = modelElement.getElement(1);
+        assertEquals("options1", firstSecondaryInstanceElement.getAttributeValue(null, "id"));
+        assertEquals(0, firstSecondaryInstanceElement.getChildCount());
+        //BIND ELEMENT
+        Element bindElement = modelElement.getElement(2);
+        assertEquals(4, bindElement.getAttributeCount());
+
+    }
+
+}
