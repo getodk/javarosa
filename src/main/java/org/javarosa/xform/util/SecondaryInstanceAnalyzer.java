dbd96255d85cf10bac826c65f490166d7d739fef
Musa Musa <musakunte@gmail.com>
2019-06-23 23:51:50 +0100

    replaced local method with javarosa utils method

--- src/main/java/org/javarosa/xform/util/SecondaryInstanceAnalyzer.java
+++ src/main/java/org/javarosa/xform/util/SecondaryInstanceAnalyzer.java
@@ -1,14 +1,13 @@
 package org.javarosa.xform.util;
 
-import org.kxml2.io.KXmlSerializer;
 import org.kxml2.kdom.Element;
 
-import java.io.IOException;
-import java.io.StringWriter;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+
+import static org.javarosa.xform.util.XFormSerializer.elementToString;
 
 public class SecondaryInstanceAnalyzer {
     // a list of SIs that need to be built in memory
@@ -20,26 +19,11 @@ public class SecondaryInstanceAnalyzer {
         inMemorySecondaryInstances = new ArrayList<>();
     }
 
-    private String toXMLTag(Element element) {
-        String converted = "";
-        KXmlSerializer kXmlSerializer = new KXmlSerializer();
-        StringWriter stringWriter = new StringWriter();
-        kXmlSerializer.setOutput(stringWriter);
-        try {
-            element.write(kXmlSerializer);
-            kXmlSerializer.flush();
-            kXmlSerializer.endDocument();
-            stringWriter.close();
-            converted = stringWriter.toString();
-        } catch (IOException e) {
-            System.out.println(e.toString());
-        }
-
-        return converted;
-    }
-
     public void analyzeElement(Element element) {
-        String elementTagString = toXMLTag(element);
+        String elementTagString = elementToString(element);
+        if (elementTagString == null)
+            return;
+
         Matcher matcher = INSTANCE_FUNCTION_PATTERN.matcher(elementTagString);
         String functionFirstParam = matcher.find() ? matcher.group(1) : null;
         if (functionFirstParam != null && !inMemorySecondaryInstances.contains(functionFirstParam))
