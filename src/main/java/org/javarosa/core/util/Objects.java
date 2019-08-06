c510aadc889424130029e1934962f739146a2c13
Hélène Martin <lognaturel@gmail.com>
2019-08-05 20:16:24 -0700

    Don't consider instances when comparing TreeReferences

--- src/main/java/org/javarosa/core/util/Objects.java
+++ src/main/java/org/javarosa/core/util/Objects.java
@@ -23,13 +23,6 @@ import java.util.Arrays;
  */
 public class Objects {
     /**
-     * Null-safe equivalent of {@code a.equals(b)}.
-     */
-    public static boolean equals(Object a, Object b) {
-        return (a == null) ? (b == null) : a.equals(b);
-    }
-
-    /**
      * Convenience wrapper for {@link Arrays#hashCode}, adding varargs.
      * This can be used to compute a hash code for an object's fields as follows:
      * {@code Objects.hash(a, b, c)}.
