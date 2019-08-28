f124e1008ce8a94904844f8fc45ea75b3748f648
JohnTheBeloved <topzy20@gmail.com>
2019-08-28 14:22:02 +0100

    Bit of fix

--- src/main/java/org/javarosa/core/model/instance/XmlExternalInstance.java
+++ src/main/java/org/javarosa/core/model/instance/XmlExternalInstance.java
@@ -15,6 +15,7 @@ public class XmlExternalInstance {
         InputStream inputStream = new FileInputStream(path);
         KXmlParser xmlParser = ElementParser.instantiateParser(inputStream);
         TreeElementParser treeElementParser = new TreeElementParser(xmlParser, 0, instanceId);
+        TreeElementParser.currentTreeReference = null;
         return treeElementParser.parse();
     }
 }
