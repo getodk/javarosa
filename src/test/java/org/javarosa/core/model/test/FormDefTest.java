9ef030a5be677152f86c01f43d36f9b81ee29de5
Guille <ggalmazor@gmail.com>
2019-09-11 09:12:53 +0200

    Migrate test to junit4

--- src/test/java/org/javarosa/core/model/test/FormDefTest.java
+++ src/test/java/org/javarosa/core/model/test/FormDefTest.java
@@ -16,104 +16,59 @@
 
 package org.javarosa.core.model.test;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.javarosa.form.api.FormEntryController.ANSWER_CONSTRAINT_VIOLATED;
+import static org.javarosa.form.api.FormEntryController.ANSWER_OK;
+import static org.javarosa.form.api.FormEntryController.EVENT_END_OF_FORM;
+import static org.junit.Assert.fail;
+
 import org.javarosa.core.model.FormIndex;
-import org.javarosa.core.model.IDataReference;
 import org.javarosa.core.model.QuestionDef;
 import org.javarosa.core.model.data.IntegerData;
 import org.javarosa.core.services.PrototypeManager;
 import org.javarosa.core.test.FormParseInit;
 import org.javarosa.core.util.externalizable.ExtUtil;
-import org.javarosa.core.util.externalizable.PrototypeFactory;
 import org.javarosa.form.api.FormEntryController;
-import org.javarosa.form.api.FormEntryPrompt;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 /**
- * See testAnswerConstraint() for an example of how to write the 
+ * See testAnswerConstraint() for an example of how to write the
  * constraint unit type tests.
- * @author adewinter
- *
  */
-public class FormDefTest extends TestCase {
-    private static final Logger logger = LoggerFactory.getLogger(FormDefTest.class);
-    QuestionDef q = null;
-    FormEntryPrompt fep = null;
-    FormParseInit fpi = null;
+public class FormDefTest {
+    private FormParseInit fpi;
 
-    public FormDefTest(String name) {
-        super(name);
-        logger.info("Running {} test: {}...", this.getClass().getName(), name);
-    }
-
-    public void setUp(){
-        fpi = new FormParseInit();
-        q = fpi.getFirstQuestionDef();
-        fep = new FormEntryPrompt(fpi.getFormDef(), fpi.getFormEntryModel().getFormIndex());
-    }
-
-    static PrototypeFactory pf;
-
-    static {
+    @BeforeClass
+    public static void init() {
         PrototypeManager.registerPrototype("org.javarosa.model.xform.XPathReference");
-        pf = ExtUtil.defaultPrototypes();
+        ExtUtil.defaultPrototypes();
     }
 
-    public static Test suite() {
-        TestSuite aSuite = new TestSuite();
-
-        for (int i = 1; i <= NUM_TESTS; i++) {
-            final int testID = i;
-            aSuite.addTest(new FormDefTest(doTest(testID)));
-        }
-
-        return aSuite;
+    @Before
+    public void setUp() {
+        fpi = new FormParseInit();
     }
 
-    private void testSerialize (QuestionDef q, String msg) {
-        //ExternalizableTest.testExternalizable(q, this, pf, "QuestionDef [" + msg + "]");
-    }
-
-    public final static int NUM_TESTS = 1;
-
-    public static String doTest (int i) {
-        switch (i) {
-        case 1: return "testAnswerConstraint";
-        }
-        throw new IllegalStateException("Unexpected index");
-    }
-
-
-    public void testAnswerConstraint(){
+    @Test
+    public void testAnswerConstraint() {
         IntegerData ans = new IntegerData(13);
         FormEntryController fec = fpi.getFormEntryController();
         fec.jumpToIndex(FormIndex.createBeginningOfFormIndex());
 
-        do{
-
+        do {
             QuestionDef q = fpi.getCurrentQuestion();
-            if(q==null || q.getTextID() == null || q.getTextID().length() == 0)continue;
-            if(q.getTextID().equals("constraint-test")){
+            if (q == null || q.getTextID() == null || q.getTextID().length() == 0)
+                continue;
+            if (q.getTextID().equals("constraint-test")) {
                 int response = fec.answerQuestion(ans, true);
-                if(response == FormEntryController.ANSWER_CONSTRAINT_VIOLATED){
+                if (response == ANSWER_CONSTRAINT_VIOLATED)
                     fail("Answer Constraint test failed.");
-                }else if(response == FormEntryController.ANSWER_OK){
+                else if (response == ANSWER_OK)
                     break;
-                }else{
+                else
                     fail("Bad response from fec.answerQuestion()");
-                }
             }
-        }while(fec.stepToNextEvent()!=FormEntryController.EVENT_END_OF_FORM);
+        } while (fec.stepToNextEvent() != EVENT_END_OF_FORM);
     }
-
-    public IDataReference newRef(String xpath){
-            IDataReference ref = new DummyReference();
-            ref.setReference(xpath);
-            pf.addClass(DummyReference.class);
-            return ref;
-    }
-
 }
