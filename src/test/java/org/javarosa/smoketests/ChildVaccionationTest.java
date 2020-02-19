fd3a0fa51422f23bf6f2073baddf141f04edde3e
Guille <ggalmazor@gmail.com>
2020-02-19 14:40:42 +0100

    Add assertions about invariants during form filling
    There's not much we can assert for sure because the form relies upon complex conditional navigation that can be inconsistent on similar branches of options.

--- src/test/java/org/javarosa/smoketests/ChildVaccionationTest.java
+++ src/test/java/org/javarosa/smoketests/ChildVaccionationTest.java
@@ -17,6 +17,9 @@
 package org.javarosa.smoketests;
 
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.is;
 import static org.javarosa.core.test.Scenario.getRef;
 import static org.javarosa.smoketests.ChildVaccionationTest.Sex.FEMALE;
 import static org.javarosa.smoketests.ChildVaccionationTest.Sex.MALE;
@@ -26,32 +29,39 @@ import static org.javarosa.smoketests.ChildVaccionationTest.Vaccines.DIPHTERIA_F
 import static org.javarosa.smoketests.ChildVaccionationTest.Vaccines.DIPHTERIA_FIRST_AND_MEASLES;
 import static org.javarosa.smoketests.ChildVaccionationTest.Vaccines.MEASLES;
 import static org.javarosa.smoketests.ChildVaccionationTest.Vaccines.NONE;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
 
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
-import java.util.stream.Collectors;
 import org.javarosa.core.model.instance.TreeReference;
 import org.javarosa.core.test.Scenario;
 import org.junit.Test;
 
 public class ChildVaccionationTest {
 
-    public static final TreeReference NEXT_CHILD_REF = getRef("/data/household/child_repeat/nextChild");
-    public static final TreeReference NEXT_CHILD_NO_MOTHER_REF = getRef("/data/household/child_repeat/nextChild_no_mother");
-    public static final TreeReference FINAL_FLAT_REF = getRef("/data/household/finalflat");
+    public static final TreeReference DOB_DAY_MONTH_TYPE_1_REF = getRef("/data/household/child_repeat/dob_day_1");
+    public static final TreeReference DOB_DAY_MONTH_TYPE_2_REF = getRef("/data/household/child_repeat/dob_day_2");
+    public static final TreeReference DOB_DAY_MONTH_TYPE_3_REF = getRef("/data/household/child_repeat/dob_day_3");
+    public static final TreeReference DOB_DAY_MONTH_TYPE_4_REF = getRef("/data/household/child_repeat/dob_day_4");
+    public static final TreeReference DOB_AGE_IN_MONTHS_REF = getRef("/data/household/child_repeat/age_months");
     public static final TreeReference VACCINATION_PENTA1_REF = getRef("/data/household/child_repeat/penta1");
     public static final TreeReference VACCINATION_PENTA3_REF = getRef("/data/household/child_repeat/penta3");
     public static final TreeReference VACCINATION_MEASLES_REF = getRef("/data/household/child_repeat/mcv1");
-    public static final TreeReference NOT_ELIG_NOTE_REF = getRef("/data/household/child_repeat/not_elig_note");
-    public static final LocalDate TODAY = LocalDate.now();
     public static final TreeReference CHILD_REPEAT_REF = getRef("/data/household/child_repeat");
+    public static final TreeReference NOT_ELIG_NOTE_REF = getRef("/data/household/child_repeat/not_elig_note");
+    public static final TreeReference NEXT_CHILD_REF = getRef("/data/household/child_repeat/nextChild");
+    public static final TreeReference NEXT_CHILD_NO_MOTHER_REF = getRef("/data/household/child_repeat/nextChild_no_mother");
+    public static final TreeReference FINAL_FLAT_REF = getRef("/data/household/finalflat");
+    public static final LocalDate TODAY = LocalDate.now();
 
     @Test
     public void smoke_test() {
         Scenario scenario = Scenario.init("child_vaccination_VOL_tool_v12.xml");
+
+        // region Answer questions about the building
 
         scenario.next();
         scenario.answer("multi");
@@ -63,29 +73,73 @@ public class ChildVaccionationTest {
         scenario.next();
         scenario.answer("Some address, some location");
 
-        // Start filling in households
+        // endregion
 
-        AtomicInteger householdSeq = new AtomicInteger(1);
-        List<List<Consumer<Integer>>> households = HealthRecord.all().stream().flatMap(healthRecord -> buildHouseholds(scenario, healthRecord).stream()).collect(Collectors.toList());
+        // region Answer all household repeats
+
+        // Create all possible permutations of children combining
+        // all health record types, meaningful ages, ways to define age,
+        // and vaccination sets, which amounts to 18 households and 108 children
+        List<List<Consumer<Integer>>> households = HealthRecord.all().stream()
+            .flatMap(healthRecord -> buildHouseholdChildren(scenario, healthRecord).stream())
+            .collect(toList());
+
         for (int i = 0; i < households.size(); i++) {
-            answerHousehold(scenario, householdSeq.getAndIncrement(), households.get(i));
+            List<Consumer<Integer>> children = households.get(i);
+            answerHousehold(scenario, i, children);
+            assertThat(scenario.refAtIndex().genericize(), anyOf(
+                // Either we stopped after filling the age with a decomposed date
+                is(DOB_DAY_MONTH_TYPE_1_REF),
+                is(DOB_DAY_MONTH_TYPE_2_REF),
+                is(DOB_DAY_MONTH_TYPE_3_REF),
+                is(DOB_DAY_MONTH_TYPE_4_REF),
+                // Or we stopped after filling the age in months
+                is(DOB_AGE_IN_MONTHS_REF),
+                // Or we stopped after answering any of the vaccination questions
+                is(VACCINATION_PENTA1_REF),
+                is(VACCINATION_PENTA3_REF),
+                is(VACCINATION_MEASLES_REF),
+                // Or we answered all questions
+                is(NEXT_CHILD_REF)
+            ));
             if (i + 1 < households.size()) {
                 scenario.next();
                 scenario.answer("no");
                 scenario.next();
+            } else {
+                scenario.next();
+                scenario.answer("yes");
             }
         }
 
         scenario.trace("END HOUSEHOLDS");
-        scenario.next();
-        scenario.answer("yes");
 
-        // Go to the end of the form
+        // endregion
+
+        // region Go to the end of the form
+
         scenario.next();
         scenario.next();
+
+        // endregion
     }
 
-    private List<List<Consumer<Integer>>> buildHouseholds(Scenario scenario, HealthRecord healthRecord) {
+    /**
+     * Builds every possible combination of:
+     * <ul>
+     *     <li>Meaningful ages: 23, 6 and 3 months</li>
+     *     <li>Supported ways of defining the age: a decomposed date of birth or
+     *     a number of months</li>
+     *     <li>Vaccination records: No vaccines, first and third diphteria shot, measles shot</li>
+     * </ul>
+     * <p>
+     * These combinations ensure that all critical paths for the provided health
+     * record type are taken while filling out a child group.
+     * <p>
+     * Since the form's limit to the number of children per household is 10, this method will return
+     * a list entry per every 6 children (the possible permutations of vaccine sets).
+     */
+    private List<List<Consumer<Integer>>> buildHouseholdChildren(Scenario scenario, HealthRecord healthRecord) {
         return Arrays.asList(
             buildChildrenWithLocalDates(scenario, 23, healthRecord),
             buildChildrenWithIntegers(scenario, 23, healthRecord),
@@ -120,6 +174,8 @@ public class ChildVaccionationTest {
     }
 
     private void answerHousehold(Scenario scenario, int number, List<Consumer<Integer>> children) {
+        // region Answer info about the household
+
         scenario.trace("HOUSEHOLD " + number);
         scenario.next();
         scenario.next();
@@ -142,11 +198,17 @@ public class ChildVaccionationTest {
         // Give consent
         scenario.next();
         scenario.answer("yes");
+
+        // endregion
+
         // How many children under 2?
         scenario.next();
         scenario.answer(children.size());
 
-        answerChildren(scenario, children);
+        for (int i = 0; i < children.size(); i++)
+            children.get(i).accept(i);
+
+        scenario.trace("END CHILDREN");
     }
 
 
@@ -227,12 +289,6 @@ public class ChildVaccionationTest {
         }
     }
 
-    private void answerChildren(Scenario scenario, List<Consumer<Integer>> children) {
-        for (int i = 0; i < children.size(); i++)
-            children.get(i).accept(i + 1);
-        scenario.trace("END CHILDREN");
-    }
-
     private Consumer<Integer> answerChild(Scenario scenario, HealthRecord healthRecord, LocalDate dob, Vaccines vaccines, Sex sex) {
         return i -> {
             int ageInMonths = dob.until(TODAY).getMonths();
@@ -252,6 +308,8 @@ public class ChildVaccionationTest {
 
             if (Arrays.asList(NEXT_CHILD_REF, NEXT_CHILD_NO_MOTHER_REF).contains(scenario.nextRef().genericize()))
                 scenario.next();
+            else if (!scenario.nextRef().genericize().equals(FINAL_FLAT_REF))
+                fail("Unexpected next ref " + scenario.nextRef().toString(true, true) + " at index");
         };
     }
 
@@ -266,13 +324,13 @@ public class ChildVaccionationTest {
             scenario.next();
             scenario.answer(sex.getName());
             answerAgeInMonths(scenario, ageInMonths);
-            if (scenario.nextRef().genericize().equals(NOT_ELIG_NOTE_REF))
-                scenario.next();
-            else if (scenario.nextRef().genericize().equals(VACCINATION_PENTA1_REF))
+            if (scenario.nextRef().genericize().equals(VACCINATION_PENTA1_REF))
                 vaccines.visit(scenario);
 
             if (Arrays.asList(NEXT_CHILD_REF, NEXT_CHILD_NO_MOTHER_REF).contains(scenario.nextRef().genericize()))
                 scenario.next();
+            else if (!scenario.nextRef().genericize().equals(FINAL_FLAT_REF))
+                fail("Unexpected next ref " + scenario.nextRef().toString(true, true) + " at index");
         };
     }
 
