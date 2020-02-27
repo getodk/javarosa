8bc3362cc640f1f6cc74abdfefaa4added1f5982
Guille <ggalmazor@gmail.com>
2020-02-27 18:18:33 +0100

    Ensure we won't set multiplicities in all steps of a tree ref
    If the ref being contextualized has a non-zero refLevel - it starts with one or more relative parent paths as in ../../.. - then we won't copy the corresponding context's multiplicity because it would overwrite any explicit multiplicity set by the user in the ref that's being contextualized.

--- src/main/java/org/javarosa/core/model/instance/TreeReference.java
+++ src/main/java/org/javarosa/core/model/instance/TreeReference.java
@@ -326,8 +326,13 @@ public class TreeReference implements Externalizable, Serializable {
                 //We can't actually merge nodes if the newRef has predicates or filters
                 //on this expression, since those reset any existing resolutions which
                 //may have been done.
-                if(newRef.getPredicate(i) == null) {
-                    newRef.setMultiplicity(i, contextRef.getMultiplicity(i));
+                if (newRef.getPredicate(i) == null) {
+                    List<XPathExpression> predicate = contextRef.getPredicate(i);
+                    newRef.addPredicate(i, predicate);
+                }
+                if (i + refLevel <= newRef.size()) {
+                    int multiplicity = contextRef.getMultiplicity(i);
+                    newRef.setMultiplicity(i, multiplicity);
                 }
             } else {
                 break;
