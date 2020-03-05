9c6afbb0b15f2b6722d34776b126d646b6faab8f
Guille <ggalmazor@gmail.com>
2020-03-05 11:47:31 +0100

    Create test class and form file from #449 to the project

---
+++ src/test/java/org/javarosa/regression/Issue449Test.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright 2020 Nafundi
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.javarosa.regression;
+
+import static org.javarosa.test.utils.ResourcePathHelper.r;
+
+import java.nio.file.Path;
+import org.javarosa.core.reference.ReferenceManagerTestUtils;
+import org.javarosa.core.test.Scenario;
+import org.junit.Test;
+
+public class Issue449Test {
+    @Test
+    public void name() {
+        Path formFile = r("issue_449.xml");
+        ReferenceManagerTestUtils.setUpSimpleReferenceManager(formFile.getParent(), "file");
+        Scenario.init(formFile);
+    }
+}
