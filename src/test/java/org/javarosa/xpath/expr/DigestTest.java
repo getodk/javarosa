cd2d2c8fe1948f8df25346bedf1e018b685d9dc8
Guille <ggalmazor@gmail.com>
2020-03-11 09:22:23 +0100

    Automatic reformatting and static import of some enum values
    Now we use Encoding values the same way we use DigestAlgorithm values

--- src/test/java/org/javarosa/xpath/expr/DigestTest.java
+++ src/test/java/org/javarosa/xpath/expr/DigestTest.java
@@ -31,12 +31,13 @@ import static org.javarosa.xpath.expr.DigestAlgorithm.SHA1;
 import static org.javarosa.xpath.expr.DigestAlgorithm.SHA256;
 import static org.javarosa.xpath.expr.DigestAlgorithm.SHA384;
 import static org.javarosa.xpath.expr.DigestAlgorithm.SHA512;
+import static org.javarosa.xpath.expr.Encoding.BASE64;
+import static org.javarosa.xpath.expr.Encoding.HEX;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 import java.util.Arrays;
-
 import org.javarosa.core.test.Scenario;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -61,25 +62,25 @@ public class DigestTest {
 
     @Parameterized.Parameters(name = "{0}")
     public static Iterable<Object[]> data() {
-    return Arrays.asList(new Object[][]{
-        {"MD5", MD5, "some text", Encoding.HEX, "552e21cd4cd9918678e3c1a0df491bc3"},
-        {"SHA-1", SHA1, "some text", Encoding.HEX, "37aa63c77398d954473262e1a0057c1e632eda77"},
-        {"SHA-256", SHA256, "some text", Encoding.HEX, "b94f6f125c79e3a5ffaa826f584c10d52ada669e6762051b826b55776d05aed2"},
-        {"SHA-384", SHA384, "some text", Encoding.HEX, "cc94ec3e9873c0b9a72486442958f671067cdf77b9427416d031440cc62041e2ee1344498447ec0ced9f7043461bd1f3"},
-        {"SHA-512", SHA512, "some text", Encoding.HEX, "e2732baedca3eac1407828637de1dbca702c3fc9ece16cf536ddb8d6139cd85dfe7464b8235b29826f608ccf4ac643e29b19c637858a3d8710a59111df42ddb5"},
+        return Arrays.asList(new Object[][]{
+            {"MD5", MD5, "some text", HEX, "552e21cd4cd9918678e3c1a0df491bc3"},
+            {"SHA-1", SHA1, "some text", HEX, "37aa63c77398d954473262e1a0057c1e632eda77"},
+            {"SHA-256", SHA256, "some text", HEX, "b94f6f125c79e3a5ffaa826f584c10d52ada669e6762051b826b55776d05aed2"},
+            {"SHA-384", SHA384, "some text", HEX, "cc94ec3e9873c0b9a72486442958f671067cdf77b9427416d031440cc62041e2ee1344498447ec0ced9f7043461bd1f3"},
+            {"SHA-512", SHA512, "some text", HEX, "e2732baedca3eac1407828637de1dbca702c3fc9ece16cf536ddb8d6139cd85dfe7464b8235b29826f608ccf4ac643e29b19c637858a3d8710a59111df42ddb5"},
 
-        {"MD5", MD5, "some text", Encoding.BASE64, "VS4hzUzZkYZ448Gg30kbww=="},
-        {"SHA-1", SHA1, "some text", Encoding.BASE64, "N6pjx3OY2VRHMmLhoAV8HmMu2nc="},
-        {"SHA-256", SHA256, "some text", Encoding.BASE64, "uU9vElx546X/qoJvWEwQ1SraZp5nYgUbgmtVd20FrtI="},
-        {"SHA-384", SHA384, "some text", Encoding.BASE64, "zJTsPphzwLmnJIZEKVj2cQZ833e5QnQW0DFEDMYgQeLuE0RJhEfsDO2fcENGG9Hz"},
-        {"SHA-512", SHA512, "some text", Encoding.BASE64, "4nMrrtyj6sFAeChjfeHbynAsP8ns4Wz1Nt241hOc2F3+dGS4I1spgm9gjM9KxkPimxnGN4WKPYcQpZER30LdtQ=="}
-    });
+            {"MD5", MD5, "some text", BASE64, "VS4hzUzZkYZ448Gg30kbww=="},
+            {"SHA-1", SHA1, "some text", BASE64, "N6pjx3OY2VRHMmLhoAV8HmMu2nc="},
+            {"SHA-256", SHA256, "some text", BASE64, "uU9vElx546X/qoJvWEwQ1SraZp5nYgUbgmtVd20FrtI="},
+            {"SHA-384", SHA384, "some text", BASE64, "zJTsPphzwLmnJIZEKVj2cQZ833e5QnQW0DFEDMYgQeLuE0RJhEfsDO2fcENGG9Hz"},
+            {"SHA-512", SHA512, "some text", BASE64, "4nMrrtyj6sFAeChjfeHbynAsP8ns4Wz1Nt241hOc2F3+dGS4I1spgm9gjM9KxkPimxnGN4WKPYcQpZER30LdtQ=="}
+        });
     }
 
     @Test
     public void generates_a_digest() {
-    assertEquals(expectedOutput, algorithm.digest(input, encoding));
-  }
+        assertEquals(expectedOutput, algorithm.digest(input, encoding));
+    }
 
     @Test
     public void digestFunction_acceptsDynamicParameters() throws IOException {
