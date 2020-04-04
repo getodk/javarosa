9daad11366269265ae90c8598df3d116e3177483
Hélène Martin <lognaturel@gmail.com>
2020-04-03 17:39:44 -0700

    Add failing test for predicate on relative ref

---
+++ src/test/java/org/javarosa/xpath/expr/XPathPathExprRelativeRefTest.java
@@ -0,0 +1,20 @@
+package org.javarosa.xpath.expr;
+
+import org.javarosa.xpath.XPathParseTool;
+import org.javarosa.xpath.parser.XPathSyntaxException;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.is;
+
+public class XPathPathExprRelativeRefTest {
+    @Test
+    public void predicateInRelativeRef_isAppliedToCorrectLevel() throws XPathSyntaxException {
+        XPathExpression predicate = XPathParseTool.parseXPath("position() = ../count");
+        XPathPathExpr parentRefWithPredicate = (XPathPathExpr) XPathParseTool.parseXPath("../repeat[position() = ../count]/choice");
+        assertThat(parentRefWithPredicate.getReference().getPredicate(0).get(0), is(predicate));
+
+        XPathPathExpr grandParentRefWithPredicate = (XPathPathExpr) XPathParseTool.parseXPath("../../repeat[position() = ../count]/choice");
+        assertThat(grandParentRefWithPredicate.getReference().getPredicate(0).get(0), is(predicate));
+    }
+}
