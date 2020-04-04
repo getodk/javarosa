af53cd9376ffae017e5877e194a288a14a825381
Hélène Martin <lognaturel@gmail.com>
2020-04-03 17:51:41 -0700

    Account for parenting steps when adding predicates
    The loop counter represents the steps in the XPath expression. The reference keeps track of parenting steps in the refLevel counter and other steps in a level list. The addPredicate method expects an index into the level list.

--- src/main/java/org/javarosa/xpath/expr/XPathPathExpr.java
+++ src/main/java/org/javarosa/xpath/expr/XPathPathExpr.java
@@ -200,8 +200,8 @@ public class XPathPathExpr extends XPathExpression {
             if (step.predicates.length > 0) {
                 List<XPathExpression> v = new ArrayList<XPathExpression>(step.predicates.length);
                 Collections.addAll(v, step.predicates);
-                //int level = ref.getRefLevel() > 0 ? i - ref.getRefLevel() : i; // refLevel represents parenting steps
-                ref.addPredicate(i, v);
+                int level = ref.getRefLevel() > 0 ? i - ref.getRefLevel() : i; // refLevel represents parenting steps
+                ref.addPredicate(level, v);
             }
         }
         return ref;
