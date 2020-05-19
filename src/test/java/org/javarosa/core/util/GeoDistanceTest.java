13b0abad68be26d4bf10b4f98fbee2c08fe37f5c
Hélène Martin <lognaturel@gmail.com>
2020-05-18 22:12:33 -0700

    Add distance test on concatenated point string

--- src/test/java/org/javarosa/core/util/GeoDistanceTest.java
+++ src/test/java/org/javarosa/core/util/GeoDistanceTest.java
@@ -31,6 +31,7 @@ import static org.javarosa.core.util.XFormsElement.title;
 import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
+import org.hamcrest.number.IsCloseTo;
 import org.javarosa.core.test.Scenario;
 import org.junit.Test;
 
@@ -111,5 +112,39 @@ public class GeoDistanceTest {
 
         assertThat(Double.parseDouble(scenario.answerOf("/data/distance").getDisplayText()),
             closeTo(NINETY_DEGREES_ON_EQUATOR_KM * 2, 1e-7));
+    }
+
+    @Test
+    public void distance_isComputedForString() throws IOException {
+        Scenario scenario = Scenario.init("string distance", html(
+            head(
+                title("String distance"),
+                model(
+                    mainInstance(t("data id=\"string-distance\"",
+                        t("point1", "38.253094215699576 21.756382658677467 0 0"),
+                        t("point2", "38.25021274773806 21.756382658677467 0 0"),
+                        t("point3", "38.25007793942195 21.763892843919166 0 0"),
+                        t("point4", "38.25290886154963 21.763935759263404 0 0"),
+                        t("point5", "38.25146813817506 21.758421137528785 0 0"),
+                        t("concat"),
+                        t("distance")
+                    )),
+                    bind("/data/point1").type("geopoint"),
+                    bind("/data/point2").type("geopoint"),
+                    bind("/data/point3").type("geopoint"),
+                    bind("/data/point4").type("geopoint"),
+                    bind("/data/point5").type("geopoint"),
+                    bind("/data/concat").type("string").calculate("concat(/data/point1, ';', /data/point2, ';', /data/point3, ';', /data/point4, ';', /data/point5)"),
+                    bind("/data/distance").type("decimal").calculate("distance(/data/concat)")
+                )
+            ),
+            body(
+                input("/data/point1")
+            )
+        ));
+
+        // http://www.mapdevelopers.com/area_finder.php?&points=%5B%5B38.253094215699576%2C21.756382658677467%5D%2C%5B38.25021274773806%2C21.756382658677467%5D%2C%5B38.25007793942195%2C21.763892843919166%5D%2C%5B38.25290886154963%2C21.763935759263404%5D%2C%5B38.25146813817506%2C21.758421137528785%5D%5D
+        assertThat(Double.parseDouble(scenario.answerOf("/data/distance").getDisplayText()),
+            IsCloseTo.closeTo(1801, 0.5));
     }
 }
