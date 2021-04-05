fae6c7c13d2deb0a935db653b74567495775af1b
Dave Briccetti <daveb@davebsoft.com>
2021-04-05 15:56:00 -0700

    Replace hand-written CSV parser with Apache Commons CSV

--- src/test/java/org/javarosa/core/model/instance/CsvExternalInstanceTest.java
+++ src/test/java/org/javarosa/core/model/instance/CsvExternalInstanceTest.java
@@ -1,24 +1,39 @@
 package org.javarosa.core.model.instance;
 
-import org.junit.Test;
-
-import java.io.IOException;
-
 import static org.javarosa.test.utils.ResourcePathHelper.r;
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
+import org.junit.Before;
+import org.junit.Test;
+
 public class CsvExternalInstanceTest {
+    private TreeElement root;
+
+    @Before
+    public void setUp() throws IOException {
+        root = CsvExternalInstance.parse("id", r("non_trivial.csv").toString());
+    }
 
     @Test
-    public void heading_has_no_extra_quotes() throws IOException {
-        TreeElement root = CsvExternalInstance.parse("id", r("non_trivial.csv").toString());
+    public void heading_has_no_extra_quotes() {
         assertEquals("label", root.getChildAt(0).getChildAt(0).getName());
     }
 
     @Test
-    public void value_has_no_extra_quotes() throws IOException {
-        TreeElement root = CsvExternalInstance.parse("id", r("non_trivial.csv").toString());
+    public void value_has_no_extra_quotes() {
         assertEquals("A", root.getChildAt(0).getChildAt(0).getValue().getValue());
     }
 
+    @Test
+    public void quoted_string_with_comma() {
+        assertEquals("121 Main St, NE", root.getChildAt(6).getChildAt(0).getValue().getValue());
+    }
+
+    @Test
+    public void missing_fields_replaced_with_spaces() {
+        for (int fieldIndex = 1; fieldIndex < 2; fieldIndex++) {
+            assertEquals("", root.getChildAt(5).getChildAt(fieldIndex).getValue().getValue());
+        }
+    }
 }
