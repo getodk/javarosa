ab7226f6e51e43db569b92d2da8e775d3d7df4bc
Callum Stott <callum@seadowg.com>
2023-04-13 16:31:48 +0200

    Add test for failing comp case

--- src/test/java/org/javarosa/core/model/test/PredicateCachingTest.java
+++ src/test/java/org/javarosa/core/model/test/PredicateCachingTest.java
@@ -135,7 +135,38 @@ public class PredicateCachingTest {
 
     @Test
     public void repeatedCompPredicatesAreOnlyEvaluatedOnce() throws Exception {
-        fail();
+        Scenario scenario = Scenario.init("Some form", html(
+            head(
+                title("Some form"),
+                model(
+                    mainInstance(t("data id=\"some-form\"",
+                        t("choice"),
+                        t("calculate1"),
+                        t("calculate2")
+                    )),
+                    instance("instance",
+                        item("1", "A"),
+                        item("2", "B")
+                    ),
+                    bind("/data/choice").type("string"),
+                    bind("/data/calculate1").type("string")
+                        .calculate("instance('instance')/root/item[value < /data/choice]/label"),
+                    bind("/data/calculate2").type("string")
+                        .calculate("instance('instance')/root/item[value < /data/choice]/value")
+                )
+            ),
+            body(
+                input("/data/choice")
+            )
+        ));
+
+        int evaluations = Measure.withMeasure("PredicateEvaluations", () -> {
+            scenario.answer("/data/choice", "2");
+            scenario.answer("/data/choice", "2");
+        });
+
+        // Check that we do less than size of secondary instance * number of times we answer
+        assertThat(evaluations, lessThan(4));
     }
 
     /**
