ae070b560b38df404c15c1474e37c71ab1d6974d
Callum Stott <callum@seadowg.com>
2023-04-17 09:19:58 +0200

    Add test for func case

--- src/test/java/org/javarosa/core/model/test/PredicateCachingTest.java
+++ src/test/java/org/javarosa/core/model/test/PredicateCachingTest.java
@@ -18,7 +18,6 @@ import static org.javarosa.core.util.XFormsElement.mainInstance;
 import static org.javarosa.core.util.XFormsElement.model;
 import static org.javarosa.core.util.XFormsElement.t;
 import static org.javarosa.core.util.XFormsElement.title;
-import static org.junit.Assert.fail;
 
 public class PredicateCachingTest {
 
@@ -94,7 +93,37 @@ public class PredicateCachingTest {
 
     @Test
     public void repeatedIdempotentFuncPredicatesAreOnlyEvaluatedOnceWhileAnswering() throws Exception {
-        fail();
+        Scenario scenario = Scenario.init("Some form", html(
+            head(
+                title("Some form"),
+                model(
+                    mainInstance(t("data id=\"some-form\"",
+                        t("choice"),
+                        t("calculate1"),
+                        t("calculate2")
+                    )),
+                    instance("instance",
+                        item("1", "A"),
+                        item("2", "B")
+                    ),
+                    bind("/data/choice").type("string"),
+                    bind("/data/calculate1").type("string")
+                        .calculate("instance('instance')/root/item[regex(value, /data/choice)]/label"),
+                    bind("/data/calculate2").type("string")
+                        .calculate("instance('instance')/root/item[regex(value, /data/choice)]/value")
+                )
+            ),
+            body(
+                input("/data/choice")
+            )
+        ));
+
+        int evaluations = Measure.withMeasure("PredicateEvaluations", () -> {
+            scenario.answer("/data/choice", "1");
+        });
+
+        // Check that we do less than (size of secondary instance) * (number of calculates with a filter)
+        assertThat(evaluations, lessThan(4));
     }
 
     @Test
