c44d8ac860bcff2ab54fb9aa1493aeef3548cc16
Jack Bolles <dev@jacksonbramwell.com>
2023-06-15 23:18:39 +0100

    tidy

--- src/main/java/org/javarosa/xpath/expr/XPathFuncExpr.java
+++ src/main/java/org/javarosa/xpath/expr/XPathFuncExpr.java
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-
 package org.javarosa.xpath.expr;
 
 import org.javarosa.core.model.condition.EvaluationContext;
