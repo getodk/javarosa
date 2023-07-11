24d9f30887640248e91b26be3748980aec9e3dcc
Callum Stott <callum@seadowg.com>
2023-07-11 10:49:54 +0100

    Add convenince wrapper abstract class for IXFormParserFactory

--- src/main/java/org/javarosa/xform/parse/IXFormParserFactory.java
+++ src/main/java/org/javarosa/xform/parse/IXFormParserFactory.java
@@ -1,8 +1,9 @@
 package org.javarosa.xform.parse;
 
-import java.io.Reader;
-
+import org.jetbrains.annotations.NotNull;
 import org.kxml2.kdom.Document;
+
+import java.io.Reader;
 
 /**
  * Interface for class factory for creating an XFormParser.
@@ -12,12 +13,42 @@ import org.kxml2.kdom.Document;
  *
  */
 public interface IXFormParserFactory {
-    public XFormParser getXFormParser(Reader reader);
+    XFormParser getXFormParser(Reader reader);
 
-    public XFormParser getXFormParser(Document doc);
+    XFormParser getXFormParser(Document doc);
 
-    public XFormParser getXFormParser(Reader form, Reader instance);
+    XFormParser getXFormParser(Reader form, Reader instance);
 
-    public XFormParser getXFormParser(Document form, Document instance);
+    XFormParser getXFormParser(Document form, Document instance);
 
+    abstract class Wrapper implements IXFormParserFactory {
+
+        private final IXFormParserFactory base;
+
+        public Wrapper(@NotNull IXFormParserFactory base) {
+            this.base = base;
+        }
+
+        public abstract XFormParser apply(@NotNull XFormParser xFormParser);
+
+        @Override
+        public XFormParser getXFormParser(Reader reader) {
+            return apply(base.getXFormParser(reader));
+        }
+
+        @Override
+        public XFormParser getXFormParser(Document doc) {
+            return apply(base.getXFormParser(doc));
+        }
+
+        @Override
+        public XFormParser getXFormParser(Reader form, Reader instance) {
+            return apply(base.getXFormParser(form, instance));
+        }
+
+        @Override
+        public XFormParser getXFormParser(Document form, Document instance) {
+            return apply(base.getXFormParser(form, instance));
+        }
+    }
 }
