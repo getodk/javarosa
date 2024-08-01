1d44ee03ace612658e7dacf8e25ec9577fc9992b
Grzegorz Orczykowski <grzesiek2010@gmail.com>
2024-08-01 21:21:33 +0200

    Improved tests

--- src/test/java/org/javarosa/core/model/data/helper/AnswerDataUtilTest.java
+++ src/test/java/org/javarosa/core/model/data/helper/AnswerDataUtilTest.java
@@ -16,27 +16,64 @@ import static org.junit.Assert.assertEquals;
 public class AnswerDataUtilTest {
 
     @Test
-    public void NumericalDataReturnsProperValueWhenConvertedToInt() {
+    public void answerDataToInt_returnsCorrectValueForIntegerData() {
+        assertEquals(1, AnswerDataUtil.answerDataToInt(new IntegerData(1)));
         assertEquals(5, AnswerDataUtil.answerDataToInt(new IntegerData(5)));
-
-        assertEquals(7, AnswerDataUtil.answerDataToInt(new DecimalData(7.35)));
-
-        assertEquals(3, AnswerDataUtil.answerDataToInt(new LongData(3L)));
+        assertEquals(20, AnswerDataUtil.answerDataToInt(new IntegerData(20)));
     }
 
     @Test
-    public void NonNumericalDataReturnsZeroWhenConvertedToInt() {
-        assertEquals(0, AnswerDataUtil.answerDataToInt(new BooleanData(true)));
-        assertEquals(0, AnswerDataUtil.answerDataToInt(new SelectOneData(new Selection("Selection1"))));
-        assertEquals(0, AnswerDataUtil.answerDataToInt(new DateData(new Date())));
+    public void answerDataToInt_returnsZeroForIntegerDataIfThereIsNoValue() {
+        assertEquals(0, AnswerDataUtil.answerDataToInt(new IntegerData()));
+    }
+
+    @Test
+    public void answerDataToInt_returnsCorrectValueForDecimalData() {
+        assertEquals(2, AnswerDataUtil.answerDataToInt(new DecimalData(2)));
+        assertEquals(7, AnswerDataUtil.answerDataToInt(new DecimalData(7.5)));
+        assertEquals(41, AnswerDataUtil.answerDataToInt(new DecimalData(41)));
+    }
+
+    @Test
+    public void answerDataToInt_returnsZeroForDecimalDataIfThereIsNoValue() {
+        assertEquals(0, AnswerDataUtil.answerDataToInt(new DecimalData()));
+    }
+
+    @Test
+    public void answerDataToInt_returnsCorrectValueForLongData() {
+        assertEquals(4, AnswerDataUtil.answerDataToInt(new LongData(4L)));
+        assertEquals(15, AnswerDataUtil.answerDataToInt(new LongData(15L)));
+        assertEquals(120, AnswerDataUtil.answerDataToInt(new LongData(120L)));
+    }
+
+    @Test
+    public void answerDataToInt_returnsZeroForLongDataIfThereIsNoValue() {
+        assertEquals(0, AnswerDataUtil.answerDataToInt(new LongData()));
+    }
+
+    @Test
+    public void answerDataToInt_returnsCorrectValueForStringData() {
+        assertEquals(6, AnswerDataUtil.answerDataToInt(new StringData("6")));
+        assertEquals(9, AnswerDataUtil.answerDataToInt(new StringData("9.0")));
+        assertEquals(17, AnswerDataUtil.answerDataToInt(new StringData("17")));
+        assertEquals(21, AnswerDataUtil.answerDataToInt(new StringData("21.5")));
+        assertEquals(53, AnswerDataUtil.answerDataToInt(new StringData("53")));
+    }
+
+    @Test
+    public void answerDataToInt_returnsZeroForStringDataIfThereIsNoValue() {
+        assertEquals(0, AnswerDataUtil.answerDataToInt(new StringData()));
+    }
+
+    @Test
+    public void answerDataToInt_returnsZeroForStringDataIfTheValueCanNotBeConvertedToInteger() {
         assertEquals(0, AnswerDataUtil.answerDataToInt(new StringData("blah")));
     }
 
     @Test
-    public void IfDataTypeHasNoAnswerReturnsZeroWhenConvertedToInt() {
-        assertEquals(0, AnswerDataUtil.answerDataToInt(new IntegerData()));
-        assertEquals(0, AnswerDataUtil.answerDataToInt(new DecimalData()));
-        assertEquals(0, AnswerDataUtil.answerDataToInt(new LongData()));
-        assertEquals(0, AnswerDataUtil.answerDataToInt(new BooleanData()));
+    public void answerDataToInt_returnsZeroForNotSupportedDataTypes() {
+        assertEquals(0, AnswerDataUtil.answerDataToInt(new BooleanData(true)));
+        assertEquals(0, AnswerDataUtil.answerDataToInt(new SelectOneData(new Selection("Selection1"))));
+        assertEquals(0, AnswerDataUtil.answerDataToInt(new DateData(new Date())));
     }
 }
